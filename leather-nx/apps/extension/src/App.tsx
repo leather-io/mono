7f5383103512efc243b6eb3b1dd92cc7aee30b5b
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-09-15 10:17:06 +0100

    Consume UI button from APP

--- leather-nx/apps/extension/src/App.tsx
+++ leather-nx/apps/extension/src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import { Button } from 'ui';
 
 function App() {
   return (
     <div className="App">
+      <Button onClick={() => console.log('consumed button')}>UI button</Button>
     </div>
   );
 }
