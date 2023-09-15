bb958bf85bac2ca7fe6ce800584fc3d7ec54363e
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2023-09-15 16:51:56 +0100

    Try use esbuild in generated APP

--- leather-nx/apps/leather-ext/src/app/app.tsx
+++ leather-nx/apps/leather-ext/src/app/app.tsx
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
 import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
