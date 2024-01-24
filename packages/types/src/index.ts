e42716d56005eaa47549b38771632c0e6fc4b990
kyranjamie <kyran.burraston@gmail.com>
2024-01-24 19:23:25 -0300

    feat(types): add types package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,49 @@
+import { DefineGetAddressesMethod } from './methods/get-addresses';
+import { DefineGetInfoMethod } from './methods/get-info';
+import { DefineSendTransferMethod } from './methods/send-transfer';
+import { DefineSignMessageMethod } from './methods/sign-message';
+import { DefineSignPsbtMethod } from './methods/sign-psbt';
+import { ValueOf } from './utils';
+
+export * from './rpc';
+export * from './methods/get-info';
+export * from './methods/sign-psbt';
+export * from './methods/get-addresses';
+export * from './methods/send-transfer';
+
+export type MethodMap = DefineGetInfoMethod &
+  DefineGetAddressesMethod &
+  DefineSignPsbtMethod &
+  DefineSignMessageMethod &
+  DefineSendTransferMethod;
+
+export type RpcRequests = ValueOf<MethodMap>['request'];
+
+export type RpcResponses = ValueOf<MethodMap>['response'];
+
+export type MethodNames = keyof MethodMap;
+
+export interface RequestFn {
+  <T extends MethodNames>(arg: T, params?: object | string[]): Promise<MethodMap[T]['response']>;
+}
+
+export interface ListenFn {
+  (method: string, callback: () => void): () => void;
+}
+
+declare global {
+  interface Window {
+    btc?: {
+      /**
+       * Request method. Takes a method name, and optional parameters
+       * @returns Typed response for corresponding method
+       */
+      request: RequestFn;
+      /**
+       * Listen method. Takes an event name to listen for, and a callback function.
+       * @returns An unsubscribe function
+       */
+      listen: ListenFn;
+    };
+  }
+}
