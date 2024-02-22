3ba1a242da03dc583b60f8586a83b50d177856e0
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-02-22 11:21:10 +0400

    fix: metro config tsconfig paths resolution

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -7,13 +7,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
 
-const path = require("path");
-const { getDefaultConfig } = require("expo/metro-config");
-const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
-
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
 
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
+const workspaceRoot = path.resolve(projectRoot, '../..');
 
 const config = getDefaultConfig(projectRoot);
 
@@ -23,36 +22,19 @@ config.watchFolders = [workspaceRoot];
 config.resolver.disableHierarchicalLookup = true;
 // #3 - Try resolving with project modules first, then workspace modules
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules", ".pnpm", "node_modules"),
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules', '.pnpm', 'node_modules'),
 ];
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName.startsWith("@/")) {
-    // Logic to resolve the module name to a file path...
-    // NOTE: Throw an error if there is no resolution.
-    const relativePath = moduleName.replace("@/", "src/");
-    const filePathWithoutExt = path.resolve(__dirname, relativePath);
-    const filePaths = config.resolver.sourceExts
-      .map(ext => {
-        const filePathWithExt = `${filePathWithoutExt}.${ext}`;
-        if (context.doesFileExist(filePathWithExt)) {
-          return filePathWithExt;
-        }
-        return;
-      })
-      .filter(filePathWithExt => filePathWithExt);
-
-    if (filePaths.length === 0) throw new Error("File cannot be found: " + filePathWithoutExt);
-
-    return {
-      filePath: filePaths[0],
-      type: "sourceFile",
-    };
+  // TODO: either read tsconfig automatically or figure out another way of resolving tsconfig aliases.
+  // If we add more aliases in tsconfig, we need to add those to this if statement.
+  if (moduleName.startsWith('@/')) {
+    return context.resolveRequest(context, moduleName, platform);
   }
-  const symlingResolver = MetroSymlinksResolver();
-  return symlingResolver(context, moduleName, platform);
+  const symlinkResolver = MetroSymlinksResolver();
+  return symlinkResolver(context, moduleName, platform);
 };
 
 config.transformer.getTransformOptions = async () => ({
