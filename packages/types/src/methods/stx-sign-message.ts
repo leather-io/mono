e9347cbe1f77efe0b988e5c86cf2ea29c45215bb
kyranjamie <kyran.burraston@gmail.com>
2024-02-27 14:23:16 +0100

    chore: add stx message signing types

---
+++ packages/types/src/methods/stx-sign-message.ts
@@ -0,0 +1,36 @@
+import { DefineRpcMethod, RpcRequest, RpcResponse } from '../rpc';
+
+export type StxSignMessageTypes = 'utf8' | 'structured';
+
+export interface StxSignMessageRequestParamsBase {
+  type: StxSignMessageTypes;
+  network?: 'mainnet' | 'testnet' | 'devnet' | 'mocknet';
+}
+
+export interface StxSignMessageRequestParamsUtf8 extends StxSignMessageRequestParamsBase {
+  type: 'utf8';
+  message: string;
+}
+
+export interface StxSignMessageRequestParamsStructured extends StxSignMessageRequestParamsBase {
+  type: 'structured';
+  domain: string;
+  message: string;
+}
+
+export type StxSignMessageRequestParams =
+  | StxSignMessageRequestParamsUtf8
+  | StxSignMessageRequestParamsStructured;
+
+export interface StxSignMessageResponseBody {
+  signature: string;
+}
+
+export type StxSignMessageRequest = RpcRequest<'StxSignMessage', StxSignMessageRequestParams>;
+
+export type StxSignMessageResponse = RpcResponse<StxSignMessageResponseBody>;
+
+export type DefineStxSignMessageMethod = DefineRpcMethod<
+  StxSignMessageRequest,
+  StxSignMessageResponse
+>;
