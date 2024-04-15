e9ae97db59a3d6ef2aeea887f6c1495c28d7cc9d
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-04-15 11:39:57 +0400

    fix: circular dependency

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,4 +1,4 @@
-import type { Blockchains } from '@leather-wallet/models';
+import type { Blockchains } from '@leather-wallet/models/src/blockchain.model';
 import { ChainID } from '@stacks/transactions';
 
 export const gaiaUrl = 'https://hub.blockstack.org';
