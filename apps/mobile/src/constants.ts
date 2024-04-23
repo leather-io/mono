9a7763f7c92b465d8508f5f8a5874abe7bbcd163
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-04-23 17:21:44 +0400

    feat: bootstrap release-please

--- apps/mobile/src/constants.ts
+++ apps/mobile/src/constants.ts
@@ -1,2 +1,3 @@
 export const TWITTER_LINK = 'https://twitter.com/LeatherBTC';
 export const BROWSER_EXTENSION_LINK = 'https://leather.io/install-extension';
+export const __NEW_SOME_KIND_OF_CONST = 'WHAT CONST?';
