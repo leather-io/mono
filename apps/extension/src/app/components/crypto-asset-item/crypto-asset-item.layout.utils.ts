sufatmawati/extension

refactor: assets and asset list

src/app/components/crypto-asset-item/crypto-asset-item.layout.utils.ts
@@ -1,0 +1,21 @@
+import type { CryptoAssetBalances } from '@leather-wallet/models';
+import { CryptoAssetSelectors } from '@tests/selectors/crypto-asset.selectors';
+
+import { formatBalance } from '@app/common/format-balance';
+import { ftDecimals } from '@app/common/stacks-utils';
+
+export function parseCryptoAssetBalance(balance: CryptoAssetBalances) {
+  const { availableBalance } = balance;
+
+  const amount = ftDecimals(availableBalance.amount, availableBalance.decimals);
+  const dataTestId = CryptoAssetSelectors.CryptoAssetListItem.replace(
+    '{symbol}',
+    availableBalance.symbol.toLowerCase()
+  );
+  const formattedBalance = formatBalance(amount);
+
+  return {
+    dataTestId,
+    formattedBalance,
+  };
+}

src/app/features/stacks-transaction-request/hooks/use-transaction-error.ts
@@ -11,7 +11,7 @@ import { initialSearchParams } from '@app/common/initial-search-params';
 import { stxToMicroStx } from '@app/common/money/unit-conversion';
 import { validateStacksAddress } from '@app/common/stacks-utils';
 import { TransactionErrorReason } from '@app/features/stacks-transaction-request/transaction-error/transaction-error';
-import { useCurrentStcAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
+import { useCurrentStxAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
 import { useContractInterface } from '@app/query/stacks/contract/contract.hooks';
 import { useCurrentStacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
 import { useTransactionRequestState } from '@app/store/transactions/requests.hooks';
@@ -27,7 +27,7 @@ export function useTransactionError() {
   const { values } = useFormikContext<StacksTransactionFormValues>();
 
   const currentAccount = useCurrentStacksAccount();
-  const availableUnlockedBalance = useCurrentStcAvailableUnlockedBalance();
+  const availableUnlockedBalance = useCurrentStxAvailableUnlockedBalance();
 
   return useMemo<TransactionErrorReason | void>(() => {
     if (!origin) return TransactionErrorReason.ExpiredRequest;

src/app/features/stacks-transaction-request/stacks-transaction-signer.tsx
@@ -26,7 +26,7 @@ import { PostConditionModeWarning } from '@app/features/stacks-transaction-reque
 import { PostConditions } from '@app/features/stacks-transaction-request/post-conditions/post-conditions';
 import { StxTransferDetails } from '@app/features/stacks-transaction-request/stx-transfer-details/stx-transfer-details';
 import { TransactionError } from '@app/features/stacks-transaction-request/transaction-error/transaction-error';
-import { useCurrentStcAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
+import { useCurrentStxAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
 import { useCalculateStacksTxFees } from '@app/query/stacks/fees/fees.hooks';
 import { useNextNonce } from '@app/query/stacks/nonce/account-nonces.hooks';
 import { useTransactionRequestState } from '@app/store/transactions/requests.hooks';
@@ -55,7 +55,7 @@ export function StacksTransactionSigner({
   const transactionRequest = useTransactionRequestState();
   const { data: stxFees } = useCalculateStacksTxFees(stacksTransaction);
   const analytics = useAnalytics();
-  const availableUnlockedBalance = useCurrentStcAvailableUnlockedBalance();
+  const availableUnlockedBalance = useCurrentStxAvailableUnlockedBalance();
   const navigate = useNavigate();
   const { data: nextNonce } = useNextNonce();
   const { search } = useLocation();

src/app/features/stacks-transaction-request/transaction-error/error-messages.tsx
@@ -12,7 +12,7 @@ import { useScrollLock } from '@app/common/hooks/use-scroll-lock';
 import { stacksValue } from '@app/common/stacks-utils';
 import { SwitchAccountDialog } from '@app/features/dialogs/switch-account-dialog/switch-account-dialog';
 import { ErrorMessage } from '@app/features/stacks-transaction-request/transaction-error/error-message';
-import { useCurrentStcAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
+import { useCurrentStxAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
 import { useCurrentNetworkState } from '@app/store/networks/networks.hooks';
 import { useTransactionRequestState } from '@app/store/transactions/requests.hooks';
 import { Button } from '@app/ui/components/button/button';
@@ -59,7 +59,7 @@ export const FeeInsufficientFundsErrorMessage = memo(props => {
 
 export const StxTransferInsufficientFundsErrorMessage = memo(props => {
   const pendingTransaction = useTransactionRequestState();
-  const availableUnlockedBalance = useCurrentStcAvailableUnlockedBalance();
+  const availableUnlockedBalance = useCurrentStxAvailableUnlockedBalance();
 
   return (
     <ErrorMessage

src/app/pages/send/send-crypto-asset-form/form/btc/btc-send-form.tsx
@@ -9,7 +9,7 @@ import { CryptoCurrencies } from '@shared/models/currencies.model';
 
 import { formatMoney } from '@app/common/money/format-money';
 import { HighFeeDialog } from '@app/features/dialogs/high-fee-dialog/high-fee-dialog';
-import { useBtcCryptoAssetBalanceNativeSegwit } from '@app/query/bitcoin/balance/btc-balance-native-segwit.hooks';
+import { useBtcCryptoAsset } from '@app/query/bitcoin/btc/btc-crypto-asset.hooks';
 import { useCryptoCurrencyMarketDataMeanAverage } from '@app/query/common/market-data/market-data.hooks';
 import { useCurrentAccountNativeSegwitIndexZeroSigner } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 import { BtcAvatarIcon } from '@app/ui/components/avatar/btc-avatar-icon';
@@ -36,11 +36,9 @@ export function BtcSendForm() {
   const routeState = useSendFormRouteState();
   const btcMarketData = useCryptoCurrencyMarketDataMeanAverage(symbol);
 
-  const nativeSegwitSigner = useCurrentAccountNativeSegwitIndexZeroSigner();
-  // TODO: Asset refactor: need asset here
-  const { btcCryptoAssetBalance } = useBtcCryptoAssetBalanceNativeSegwit(
-    nativeSegwitSigner.address
-  );
+  const { address } = useCurrentAccountNativeSegwitIndexZeroSigner();
+  const { asset } = useBtcCryptoAsset(address);
+  const { balance, info } = asset;
 
   const {
     calcMaxSpend,
@@ -83,19 +81,17 @@ export function BtcSendForm() {
                     >
                       Continue
                     </Button>
-                    <AvailableBalance
-                      balance={formatMoney(btcCryptoAssetBalance.availableBalance)}
-                    />
+                    <AvailableBalance balance={formatMoney(balance.availableBalance)} />
                   </Footer>
                 }
               >
                 <CardContent dataTestId={SendCryptoAssetSelectors.SendForm}>
                   <AmountField
                     autoComplete="off"
-                    balance={btcCryptoAssetBalance.availableBalance}
+                    balance={balance.availableBalance}
                     bottomInputOverlay={
                       <BitcoinSendMaxButton
-                        balance={btcCryptoAssetBalance.availableBalance}
+                        balance={balance.availableBalance}
                         isSendingMax={isSendingMax}
                         onSetIsSendingMax={onSetIsSendingMax}
                         sendMaxBalance={sendMaxCalculation.spendableBitcoin.toString()}
@@ -108,11 +104,7 @@ export function BtcSendForm() {
                       <SendFiatValue marketData={btcMarketData} assetSymbol={symbol} />
                     }
                   />
-                  <SelectedAssetField
-                    icon={<BtcAvatarIcon />}
-                    name={btcBalance.asset.name}
-                    symbol={symbol}
-                  />
+                  <SelectedAssetField icon={<BtcAvatarIcon />} name={info.name} symbol={symbol} />
                   <TransferRecipientField />
                   {currentNetwork.chain.bitcoin.bitcoinNetwork === 'testnet' && (
                     <Callout variant="warning" title="Funds have no value" mt="space.04">

leather-io/mono

apps/extension/src/app/components/crypto-asset-item/crypto-asset-item.layout.utils.ts
@@ -1,0 +1,21 @@
+import type { CryptoAssetBalance } from '@leather-wallet/models';
+import { CryptoAssetSelectors } from '@tests/selectors/crypto-asset.selectors';
+
+import { formatBalance } from '@app/common/format-balance';
+import { ftDecimals } from '@app/common/stacks-utils';
+
+export function parseCryptoAssetBalance(balance: CryptoAssetBalance) {
+  const { availableBalance } = balance;
+
+  const amount = ftDecimals(availableBalance.amount, availableBalance.decimals);
+  const dataTestId = CryptoAssetSelectors.CryptoAssetListItem.replace(
+    '{symbol}',
+    availableBalance.symbol.toLowerCase()
+  );
+  const formattedBalance = formatBalance(amount);
+
+  return {
+    dataTestId,
+    formattedBalance,
+  };
+}

apps/extension/src/app/features/stacks-transaction-request/hooks/use-transaction-error.ts
@@ -11,7 +11,7 @@ import { initialSearchParams } from '@app/common/initial-search-params';
 import { stxToMicroStx } from '@app/common/money/unit-conversion';
 import { validateStacksAddress } from '@app/common/stacks-utils';
 import { TransactionErrorReason } from '@app/features/stacks-transaction-request/transaction-error/transaction-error';
-import { useCurrentStcAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
+import { useCurrentStxAvailableUnlockedBalance } from '@app/query/stacks/balance/account-balance.hooks';
 import { useContractInterface } from '@app/query/stacks/contract/contract.hooks';
 import { useCurrentStacksAccount } from '@app/store/accounts/blockchain/stacks/stacks-account.hooks';
 import { useTransactionRequestState } from '@app/store/transactions/requests.hooks';
@@ -27,7 +27,7 @@ export function useTransactionError() {
   const { values } = useFormikContext<StacksTransactionFormValues>();
 
   const currentAccount = useCurrentStacksAccount();
-  const availableUnlockedBalance = useCurrentStcAvailableUnlockedBalance();
+  const availableUnlockedBalance = useCurrentStxAvailableUnlockedBalance();
 
   return useMemo<TransactionErrorReason | void>(() => {
     if (!origin) return TransactionErrorReason.ExpiredRequest;

apps/extension/src/app/features/stacks-transaction-request/stacks-transaction-signer.tsx
@@ -26,7 +26,7 @@ import { PostConditionModeWarning } from '@app/features/stacks-transaction-reque
 import { PostConditions } from '@app/features/stacks-transaction-request/post-conditions/post-conditions';
 import { StxTransferDetails } from '@app/features/stacks-transaction-request/stx-transfer-details/stx-transfer-details';
 import { TransactionError } from '@app/features/stacks-transaction-request/transaction-error/transaction-error';
-import { useCurrentStcAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
+import { useCurrentStxAvailableUnlockedBalance } from '@app/query/stacks/balance/account-balance.hooks';
 import { useCalculateStacksTxFees } from '@app/query/stacks/fees/fees.hooks';
 import { useNextNonce } from '@app/query/stacks/nonce/account-nonces.hooks';
 import { useTransactionRequestState } from '@app/store/transactions/requests.hooks';
@@ -55,7 +55,7 @@ export function StacksTransactionSigner({
   const transactionRequest = useTransactionRequestState();
   const { data: stxFees } = useCalculateStacksTxFees(stacksTransaction);
   const analytics = useAnalytics();
-  const availableUnlockedBalance = useCurrentStcAvailableUnlockedBalance();
+  const availableUnlockedBalance = useCurrentStxAvailableUnlockedBalance();
   const navigate = useNavigate();
   const { data: nextNonce } = useNextNonce();
   const { search } = useLocation();

apps/extension/src/app/features/stacks-transaction-request/transaction-error/error-messages.tsx
@@ -12,7 +12,7 @@ import { useScrollLock } from '@app/common/hooks/use-scroll-lock';
 import { stacksValue } from '@app/common/stacks-utils';
 import { SwitchAccountDialog } from '@app/features/dialogs/switch-account-dialog/switch-account-dialog';
 import { ErrorMessage } from '@app/features/stacks-transaction-request/transaction-error/error-message';
-import { useCurrentStcAvailableUnlockedBalance } from '@app/query/stacks/balance/stx-balance.hooks';
+import { useCurrentStxAvailableUnlockedBalance } from '@app/query/stacks/balance/account-balance.hooks';
 import { useCurrentNetworkState } from '@app/store/networks/networks.hooks';
 import { useTransactionRequestState } from '@app/store/transactions/requests.hooks';
 import { Button } from '@app/ui/components/button/button';
@@ -59,7 +59,7 @@ export const FeeInsufficientFundsErrorMessage = memo(props => {
 
 export const StxTransferInsufficientFundsErrorMessage = memo(props => {
   const pendingTransaction = useTransactionRequestState();
-  const availableUnlockedBalance = useCurrentStcAvailableUnlockedBalance();
+  const availableUnlockedBalance = useCurrentStxAvailableUnlockedBalance();
 
   return (
     <ErrorMessage

apps/extension/src/app/pages/send/send-crypto-asset-form/form/btc/btc-send-form.tsx
@@ -9,7 +9,7 @@ import { CryptoCurrencies } from '@shared/models/currencies.model';
 
 import { formatMoney } from '@app/common/money/format-money';
 import { HighFeeDialog } from '@app/features/dialogs/high-fee-dialog/high-fee-dialog';
-import { useBtcCryptoAssetBalanceNativeSegwit } from '@app/query/bitcoin/balance/btc-balance-native-segwit.hooks';
+import { useBtcAccountCryptoAssetWithDetails } from '@app/query/bitcoin/btc/btc-crypto-asset.hooks';
 import { useCryptoCurrencyMarketDataMeanAverage } from '@app/query/common/market-data/market-data.hooks';
 import { useCurrentAccountNativeSegwitIndexZeroSigner } from '@app/store/accounts/blockchain/bitcoin/native-segwit-account.hooks';
 import { BtcAvatarIcon } from '@app/ui/components/avatar/btc-avatar-icon';
@@ -36,11 +36,9 @@ export function BtcSendForm() {
   const routeState = useSendFormRouteState();
   const btcMarketData = useCryptoCurrencyMarketDataMeanAverage(symbol);
 
-  const nativeSegwitSigner = useCurrentAccountNativeSegwitIndexZeroSigner();
-  // TODO: Asset refactor: need asset here
-  const { btcCryptoAssetBalance } = useBtcCryptoAssetBalanceNativeSegwit(
-    nativeSegwitSigner.address
-  );
+  const { address } = useCurrentAccountNativeSegwitIndexZeroSigner();
+  const { asset } = useBtcAccountCryptoAssetWithDetails(address);
+  const { balance, info } = asset;
 
   const {
     calcMaxSpend,
@@ -83,19 +81,17 @@ export function BtcSendForm() {
                     >
                       Continue
                     </Button>
-                    <AvailableBalance
-                      balance={formatMoney(btcCryptoAssetBalance.availableBalance)}
-                    />
+                    <AvailableBalance balance={formatMoney(balance.availableBalance)} />
                   </Footer>
                 }
               >
                 <CardContent dataTestId={SendCryptoAssetSelectors.SendForm}>
                   <AmountField
                     autoComplete="off"
-                    balance={btcCryptoAssetBalance.availableBalance}
+                    balance={balance.availableBalance}
                     bottomInputOverlay={
                       <BitcoinSendMaxButton
-                        balance={btcCryptoAssetBalance.availableBalance}
+                        balance={balance.availableBalance}
                         isSendingMax={isSendingMax}
                         onSetIsSendingMax={onSetIsSendingMax}
                         sendMaxBalance={sendMaxCalculation.spendableBitcoin.toString()}
@@ -108,11 +104,7 @@ export function BtcSendForm() {
                       <SendFiatValue marketData={btcMarketData} assetSymbol={symbol} />
                     }
                   />
-                  <SelectedAssetField
-                    icon={<BtcAvatarIcon />}
-                    name={btcBalance.asset.name}
-                    symbol={symbol}
-                  />
+                  <SelectedAssetField icon={<BtcAvatarIcon />} name={info.name} symbol={symbol} />
                   <TransferRecipientField />
                   {currentNetwork.chain.bitcoin.bitcoinNetwork === 'testnet' && (
                     <Callout variant="warning" title="Funds have no value" mt="space.04">