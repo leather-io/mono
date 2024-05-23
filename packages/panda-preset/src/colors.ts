fe2d9693c9efeaab7f45c10703b9cdc87edc697b
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-05-23 10:03:25 +0100

    feat: panda preset package, ref leather-wallet/issues#62

---
+++ packages/panda-preset/src/colors.ts
@@ -0,0 +1,6 @@
+import { defineTokens } from '@pandacss/dev';
+
+export const colors = defineTokens.colors({
+  current: { value: 'currentColor' },
+  overlay: { value: 'rgba(0,0,0,0.4)' },
+});
---
+++ packages/panda-preset/src/preset.ts
@@ -0,0 +1,22 @@
+import { definePreset } from '@pandacss/dev';
+
+import { breakpoints } from './breakpoints';
+import { keyframes } from './keyframes';
+import { buttonRecipe } from './recipes/button-recipe';
+import { linkRecipe } from './recipes/link-recipe';
+import { semanticTokens } from './semantic-tokens';
+import { tokens } from './tokens';
+import { textStyles } from './typography';
+
+export default definePreset({
+  theme: {
+    extend: {
+      semanticTokens,
+      tokens,
+      keyframes,
+      textStyles,
+      breakpoints,
+      recipes: { button: buttonRecipe, link: linkRecipe },
+    },
+  },
+});
