0c33df2d1c92069e1e85df3e151d621ed68d3019
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-05-29 06:48:49 +0100

    feat: use panda preset, initial component migration, ref leather-wallet/issues#63

--- packages/ui/src/theme-web/colors.ts
+++
@@ -1,7 +0,0 @@
-import { colors as colorTokens } from '@leather-wallet/tokens';
-import { defineTokens } from '@pandacss/dev';
-
-// ts-unused-exports:disable-next-line
-export const colors = defineTokens.colors({
-  ...colorTokens,
-});
