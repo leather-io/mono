8c9c9a5578013e8b22f1f57ae0e1cd51f4ad5c36
Anastasios <anastas3993@gmail.com>
2024-05-30 17:47:08 +0400

    fix: i18nFormatCurrency fn

--- packages/utils/src/money/is-money.ts
+++ packages/utils/src/money/is-money.ts
@@ -1,8 +1,14 @@
 import { Money } from '@leather-wallet/models';
+import BigNumber from 'bignumber.js';
 
 import { isObject } from '../..';
 
 export function isMoney(val: unknown): val is Money {
   if (!isObject(val)) return false;
   return 'amount' in val && 'symbol' in val && 'decimals' in val;
+}
+
+export function isMoneyGreaterThanZero(money: Money) {
+  if (!BigNumber.isBigNumber(money.amount)) return;
+  return !(money.amount.isNaN() || money.amount.isZero());
 }
