74ba3b0a7a3c8e9be942f455f5aa81a4e20c5ee9
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-06-06 09:05:24 +0100

    chore: setup tsup to build web UI, ref leather-wallet/issues#64

---
+++ packages/ui/tsup.config.web.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['web.ts'],
+  format: ['esm'],
+  tsconfig: 'tsconfig.web.json',
+  outDir: 'dist-web/',
+  dts: true,
+  clean: true,
+  minify: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  minifyWhitespace: true,
+  splitting: true,
+  treeshake: true,
+});
