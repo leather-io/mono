2c80332e71ef3828e9f02874ab8512406177b498
Fara Woolf <fara@trustmachines.co>
2024-06-11 10:02:59 -0500

    fix: raw tx hook after relocation

--- packages/query/src/stacks/transactions/raw-transaction-by-id.hooks.ts
+++ packages/query/src/stacks/transactions/raw-transaction-by-id.hooks.ts
@@ -1,4 +1,8 @@
+import { useMemo } from 'react';
+
 import { deserializeTransaction } from '@stacks/transactions';
+
+import { isUndefined } from '@leather-wallet/utils';
 
 import { useRawTransactionById } from './raw-transaction-by-id.query';
 
@@ -7,9 +11,12 @@ const rawTxCache = new Map();
 export function useStacksRawTransaction(txid: string) {
   const { data: rawTxData, isLoading: isLoadingRawTx } = useRawTransactionById(txid);
 
-  const match = rawTxCache.get(txid);
-  // No need to fetch again
-  if (match) return match;
-  rawTxCache.set(txid, rawTxData?.raw_tx);
-  return { isLoadingRawTx, rawTx: deserializeTransaction(rawTxData?.raw_tx ?? '') };
+  return useMemo(() => {
+    const match = rawTxCache.get(txid);
+    // No need to fetch again
+    if (match) return match;
+    rawTxCache.set(txid, rawTxData?.raw_tx);
+    if (isUndefined(rawTxData)) return { isLoadingRawTx, rawTx: undefined };
+    return { isLoadingRawTx, rawTx: deserializeTransaction(rawTxData.raw_tx) };
+  }, [isLoadingRawTx, rawTxData, txid]);
 }
