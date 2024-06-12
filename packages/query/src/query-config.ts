39ecd4f2bfa7acc1430affc5821b615f608739f5
Fara Woolf <fara@trustmachines.co>
2024-06-12 12:46:18 -0500

    refactor: query for balance loaders

--- packages/query/src/query-config.ts
+++ packages/query/src/query-config.ts
@@ -1,4 +1,5 @@
 import { QueryObserverSuccessResult, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
 
 type AllowedReactQueryConfigOptions = keyof Pick<
   UseQueryOptions,
@@ -9,6 +10,19 @@ export type AppUseQueryConfig<QueryFnData, Response> = Pick<
   UseQueryOptions<QueryFnData, unknown, Response>,
   AllowedReactQueryConfigOptions
 >;
+
+export function isErrorTooManyRequests<TData, TError = unknown>(
+  query: UseQueryResult<TData, TError>
+) {
+  if (query.isError && isAxiosError(query.error)) {
+    return query.error.response?.status === 429;
+  }
+  return false;
+}
+
+export function isInitializingData<TData, TError = unknown>(query: UseQueryResult<TData, TError>) {
+  return query.isInitialLoading || query.isRefetching;
+}
 
 export function isFetchedWithSuccess<TData, TError = unknown>(
   query: UseQueryResult<TData, TError>
