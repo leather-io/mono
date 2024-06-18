683786843e3fa0cc25bb5bab05175c79542bd94d
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-06-18 15:38:57 +0100

    fix: export AddressDisplayer,  add avatarIcons ref leather-wallet/issues#64

---
+++ packages/ui/src/components/avatar/index.ts
@@ -0,0 +1,9 @@
+export * from './avatar.web';
+export { Brc20AvatarIcon } from './brc20-avatar-icon.web';
+export { BtcAvatarIcon } from './btc-avatar-icon.web';
+export { OrdinalAvatarIcon } from './ordinal-avatar-icon.web';
+export { RunesAvatarIcon } from './runes-avatar-icon.web';
+export { Src20AvatarIcon } from './src20-avatar-icon.web';
+export { StampsAvatarIcon } from './stamps-avatar-icon.web';
+export { StxAvatarIcon } from './stx-avatar-icon.web';
+export { Stx20AvatarIcon } from './stx20-avatar-icon.web';
