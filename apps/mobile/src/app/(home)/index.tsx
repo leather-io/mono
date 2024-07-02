572b62e2d560173d2be59bc98178a58dfd72ff9a
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-07-02 12:38:59 +0400

    feat: add header tabs

--- apps/mobile/src/app/(home)/index.tsx
+++ apps/mobile/src/app/(home)/index.tsx
@@ -1,9 +1,10 @@
+import { APP_ROUTES } from '@/constants';
 import { AppStartMode, whenAppStartMode } from '@/utils/when-app-start-mode';
 import { Redirect } from 'expo-router';
 
 export default function Home() {
   return whenAppStartMode(process.env.EXPO_PUBLIC_APP_START_MODE as AppStartMode)({
-    live: <Redirect href="/send" />,
-    prelaunch: <Redirect href="/waiting-list" />,
+    live: <Redirect href={APP_ROUTES.WalletAllAssets} />,
+    prelaunch: <Redirect href={APP_ROUTES.WaitingList} />,
   });
 }
---
+++ apps/mobile/src/app/wallet/_layout.tsx
@@ -0,0 +1,53 @@
+import { RefObject, createContext } from 'react';
+
+import ArrowLeft from '@/assets/arrow-left.svg';
+import EmojiSmile from '@/assets/emoji-smile.svg';
+import Menu from '@/assets/menu.svg';
+import { ActionBarMethods } from '@/components/action-bar';
+import { createBlurredHeader } from '@/components/blurred-header';
+import { Stack, useRouter } from 'expo-router';
+
+import { Box, Text, TouchableOpacity } from '@leather.io/ui/native';
+
+export const ActionBarContext = createContext<{ ref: RefObject<ActionBarMethods> | null }>({
+  ref: null,
+});
+
+export default function StackLayout() {
+  const router = useRouter();
+
+  const NavigationHeader = createBlurredHeader({
+    left: (
+      <TouchableOpacity
+        onPress={() => router.back()}
+        height={48}
+        width={48}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <ArrowLeft height={24} width={24} />
+      </TouchableOpacity>
+    ),
+    center: (
+      <TouchableOpacity height={48} px="3" flexDirection="row" alignItems="center" gap="2">
+        <Box borderRadius="round" p="1" bg="base.blue.background-secondary">
+          <EmojiSmile width={24} height={24} />
+        </Box>
+        <Text variant="heading05">Account 1</Text>
+      </TouchableOpacity>
+    ),
+    right: (
+      <TouchableOpacity height={48} width={48} justifyContent="center" alignItems="center">
+        <Menu height={24} width={24} />
+      </TouchableOpacity>
+    ),
+  });
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="send" options={{ header: NavigationHeader }} />
+      <Stack.Screen name="receive" options={{ header: NavigationHeader }} />
+      <Stack.Screen name="swap" options={{ header: NavigationHeader }} />
+    </Stack>
+  );
+}
