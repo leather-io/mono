bf7b42266d8530316719ab87e92c98e4256d6934
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-07-04 16:43:21 +0400

    feat: implement basic browser

--- apps/mobile/src/app/wallet/(tabs)/collectibles.tsx
+++ apps/mobile/src/app/wallet/(tabs)/collectibles.tsx
@@ -1,9 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+import { APP_ROUTES } from '@/constants';
+import { useRouter } from 'expo-router';
+
+import { TouchableOpacity } from '@leather.io/ui/native';
+
 export default function SwapScreen() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text>Collectibles 🖼️</Text>
+      <TouchableOpacity
+        onPress={() => router.navigate(APP_ROUTES.WalletBrowser)}
+        p="4"
+        bg="base.ink.background-primary"
+        borderRadius="sm"
+      >
+        <Text>navigate to browser</Text>
+      </TouchableOpacity>
     </View>
   );
 }
