7224d6203df0538960510b5c275ba44b3f390727
Anastasios <anastas3993@gmail.com>
2024-07-16 17:12:04 +0400

    feat: add rune market data

--- packages/query/src/bitcoin/runes/runes.utils.ts
+++ packages/query/src/bitcoin/runes/runes.utils.ts
@@ -1,4 +1,10 @@
-import { type RuneCryptoAssetInfo, createCryptoAssetBalance } from '@leather.io/models';
+import {
+  Money,
+  type RuneCryptoAssetInfo,
+  createCryptoAssetBalance,
+  createMarketData,
+  createMarketPair,
+} from '@leather.io/models';
 import { createMoney } from '@leather.io/utils';
 
 import { RuneBalance, RuneTickerInfo } from '../clients/best-in-slot';
@@ -16,11 +22,19 @@ function createRuneCryptoAssetInfo(tickerInfo: RuneTickerInfo): RuneCryptoAssetI
   };
 }
 
-export function createRuneCryptoAssetDetails(runeBalance: RuneBalance, tickerInfo: RuneTickerInfo) {
+export function createRuneCryptoAssetDetails(
+  runeBalance: RuneBalance,
+  tickerInfo: RuneTickerInfo,
+  fiatPrice: Money
+) {
   return {
     balance: createCryptoAssetBalance(
       createMoney(Number(runeBalance.total_balance), tickerInfo.rune_name, tickerInfo.decimals)
     ),
     info: createRuneCryptoAssetInfo(tickerInfo),
+    marketData: createMarketData(
+      createMarketPair(tickerInfo.rune_name, 'USD'),
+      createMoney(fiatPrice.amount, 'USD')
+    ),
   };
 }
