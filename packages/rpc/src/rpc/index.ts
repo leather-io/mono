518562e2abc55fd2f89c4e33b8824fabfd74e3e8
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-07-24 15:27:09 +0400

    WIP: add zod to rpc

--- packages/rpc/src/rpc/index.ts
+++ packages/rpc/src/rpc/index.ts
@@ -1,37 +1,90 @@
 // See JSON RPC specification
 // https://www.jsonrpc.org/specification
+import { z } from 'zod';
 
-export type RpcParameterByPosition = string[];
-export type RpcParameterByName = Record<string, string>;
+const rpcParameterByPositionSchema = z.string().array();
+export type RpcParameterByPosition = z.infer<typeof rpcParameterByPositionSchema>;
 
-export type RpcParameter = RpcParameterByPosition | RpcParameterByName;
-export interface RpcBaseProps {
-  jsonrpc: '2.0';
-  id: string;
+const rpcParameterByNameSchema = z.record(z.string(), z.unknown());
+export type RpcParameterByName = z.infer<typeof rpcParameterByNameSchema>;
+
+const rpcParameterSchema = z.union([rpcParameterByPositionSchema, rpcParameterByNameSchema]);
+export type RpcParameter = z.infer<typeof rpcParameterSchema>;
+
+const rpcBasePropsSchema = z.object({
+  jsonrpc: z.literal('2.0'),
+  id: z.string(),
+});
+export type RpcBaseProps = z.infer<typeof rpcBasePropsSchema>;
+
+function rpcRequestSchema<TMethod extends z.ZodTypeAny, TParam extends z.ZodTypeAny>(
+  methodSchema: TMethod,
+  paramsSchema: TParam
+) {
+  return rpcBasePropsSchema.extend({ method: methodSchema, params: paramsSchema });
 }
 
-export interface RpcRequest<TMethod, TParam = RpcParameter> extends RpcBaseProps {
-  method: TMethod;
-  params?: TParam;
+type RpcRequestSchema<TMethod, TParam extends RpcParameter = RpcParameter> = ReturnType<
+  typeof rpcRequestSchema<z.ZodType<TMethod>, z.ZodType<TParam>>
+>;
+export type RpcRequest<TMethod, TParam extends RpcParameter = RpcParameter> = z.infer<
+  RpcRequestSchema<TMethod, TParam>
+>;
+
+function rpcErrorSchema<TErrorData extends z.ZodTypeAny>(errorDataScheme: TErrorData) {
+  return z.object({
+    code: z.union([z.number(), rpcErrorCodeSchema]),
+    message: z.string(),
+    data: errorDataScheme.optional(),
+  });
+}
+type RpcErrorSchema<TErrorData extends RpcParameter = RpcParameter> = ReturnType<
+  typeof rpcErrorSchema<z.ZodType<TErrorData>>
+>;
+export type RpcError<TErrorData extends RpcParameter = RpcParameter> = z.infer<
+  RpcErrorSchema<TErrorData>
+>;
+
+function rpcSuccessResponseSchema<TResult extends z.ZodType<object>>(resultSchema: TResult) {
+  return rpcBasePropsSchema.extend({
+    result: resultSchema,
+  });
+}
+export type RpcSuccessResponseSchema<TResult extends object> = ReturnType<
+  typeof rpcSuccessResponseSchema<z.ZodType<TResult>>
+>;
+export type RpcSuccessResponse<TResult extends object> = z.infer<RpcSuccessResponseSchema<TResult>>;
+
+function rpcErrorResponseScheme<TError extends z.ZodType<RpcError> = z.ZodType<RpcError>>(
+  errorScheme: TError
+) {
+  return rpcBasePropsSchema.extend({
+    error: errorScheme,
+  });
 }
 
-export interface RpcError<TErrorData = RpcParameter> {
-  code: number | RpcErrorCode;
-  message: string;
-  data?: TErrorData;
-}
+type RpcErrorResponseSchema<TError extends RpcError = RpcError> = ReturnType<
+  typeof rpcErrorResponseScheme<z.ZodType<TError>>
+>;
+export type RpcErrorResponse<TError extends RpcError = RpcError> = z.infer<
+  RpcErrorResponseSchema<TError>
+>;
 
-export interface RpcSuccessResponse<TResult extends object> extends RpcBaseProps {
-  result: TResult;
+function rpcResponseSchema<
+  TResult extends z.ZodType<object>,
+  TError extends z.ZodType<RpcError> = z.ZodType<RpcError>,
+>(resultSchema: TResult, errorSchema: TError) {
+  return z.union([
+    rpcSuccessResponseSchema<TResult>(resultSchema),
+    rpcErrorResponseScheme<TError>(errorSchema),
+  ]);
 }
-
-export interface RpcErrorResponse<TError extends RpcError = RpcError> extends RpcBaseProps {
-  error: TError;
-}
-
-export type RpcResponse<TResult extends object, TError extends RpcError = RpcError> =
-  | RpcSuccessResponse<TResult>
-  | RpcErrorResponse<TError>;
+type RpcResponseSchema<TResult extends object, TError extends RpcError = RpcError> = ReturnType<
+  typeof rpcResponseSchema<z.ZodType<TResult>, z.ZodType<TError>>
+>;
+export type RpcResponse<TResult extends object, TError extends RpcError = RpcError> = z.infer<
+  RpcResponseSchema<TResult, TError>
+>;
 
 export enum RpcErrorCode {
   // Spec defined server errors
@@ -45,18 +98,45 @@ export enum RpcErrorCode {
   USER_REJECTION = 4001,
   METHOD_NOT_SUPPORTED = 4002,
 }
-
+const rpcErrorCodeSchema = z.nativeEnum(RpcErrorCode);
 export type ExtractSuccessResponse<T> = Extract<T, RpcSuccessResponse<any>>;
 
 export type ExtractErrorResponse<T> = Extract<T, RpcErrorResponse<any>>;
 
-export type DefineRpcMethod<
-  TRequest extends RpcRequest<string, unknown>,
+function defineRpcMethodSchema<
+  TMethod extends string,
+  TRequest extends z.ZodType<RpcRequest<string>>,
+  TResponse extends z.ZodType<RpcResponse<object>>,
+>(method: TMethod, requestSchema: TRequest, responseSchema: TResponse) {
+  return z.record(
+    z.literal(method),
+    z.object({
+      request: requestSchema,
+      response: responseSchema,
+    })
+  );
+}
+
+type DefineRpcMethodSchema<
+  TRequest extends RpcRequest<string>,
   TResponse extends RpcResponse<object>,
-> = Record<
-  TRequest['method'],
-  {
-    request: TRequest;
-    response: TResponse;
-  }
+> = ReturnType<
+  typeof defineRpcMethodSchema<TRequest['method'], z.ZodType<TRequest>, z.ZodType<TResponse>>
 >;
+
+// TODO: method should not be optional
+export type DefineRpcMethod<
+  TRequest extends RpcRequest<string>,
+  TResponse extends RpcResponse<object>,
+> = z.infer<DefineRpcMethodSchema<TRequest, TResponse>>;
+
+// export type DefineRpcMethod<
+//   TRequest extends RpcRequest<string, RpcParameter>,
+//   TResponse extends RpcResponse<object>,
+// > = Record<
+//   TRequest['method'],
+//   {
+//     request: TRequest;
+//     response: TResponse;
+//   }
+// >;
