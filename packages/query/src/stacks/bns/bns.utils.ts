7ca1ecc2068b8a1790011404ca871fc6d7c62fb8
Anastasios <anastas3993@gmail.com>
2024-08-05 16:46:33 +0400

    feat: remove bnsx name from lookup for account names, closes leather-io/extension#5679

--- packages/query/src/stacks/bns/bns.utils.ts
+++ packages/query/src/stacks/bns/bns.utils.ts
@@ -1,108 +1,12 @@
 import { parseZoneFile } from '@fungible-systems/zone-file';
-import { asciiToBytes, bytesToAscii } from '@stacks/common';
 import { BnsNamesOwnByAddressResponse } from '@stacks/stacks-blockchain-api-types';
-import {
-  BufferCV,
-  ClarityType,
-  OptionalCV,
-  PrincipalCV,
-  TupleCV,
-  UIntCV,
-  bufferCV,
-  cvToHex,
-  deserializeCV,
-  principalToString,
-  standardPrincipalCV,
-  tupleCV,
-} from '@stacks/transactions';
 
 import { isString, isUndefined } from '@leather.io/utils';
 
 import { StacksClient } from '../stacks-client';
 
-const bnsContractConsts = {
-  contractAddress: 'SP1JTCR202ECC6333N7ZXD7MK7E3ZTEEE1MJ73C60',
-  contractName: 'bnsx-registry',
-} as const;
-
-// Fetch an address's "primary name" from the BNSx contract
-async function fetchBnsxName(
-  client: StacksClient,
-  address: string,
-  signal?: AbortSignal
-): Promise<string | null> {
-  try {
-    const addressCV = standardPrincipalCV(address);
-    const addressHex = cvToHex(addressCV);
-    const res = await client.callReadOnlyFunction({
-      ...bnsContractConsts,
-      functionName: 'get-primary-name',
-      tip: 'latest',
-      readOnlyFunctionArgs: {
-        sender: address,
-        arguments: [addressHex],
-      },
-      signal,
-    });
-    if (!res.okay || !res.result) return null;
-    const { result } = res;
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-    const cv = deserializeCV(result) as OptionalCV<
-      TupleCV<{ name: BufferCV; namespace: BufferCV }>
-    >;
-    if (cv.type === ClarityType.OptionalNone) return null;
-    const { name, namespace } = cv.value.data;
-    const fullName = `${bytesToAscii(name.buffer)}.${bytesToAscii(namespace.buffer)}`;
-    return fullName;
-  } catch (error) {
-    return null;
-  }
-}
-
-/**
- * Fetch the owner of a BNSx name.
- * If the name is not registered in BNSx, returns null.
- *
- * Subdomains don't exist on-chain, so if the name looks like a subdomain,
- * the function exits early with `null`.
- */
-async function fetchBnsxOwner(client: StacksClient, fqn: string): Promise<string | null> {
-  const nameParts = fqn.split('.');
-
-  // If the name includes a subdomain, it's not on-chain, return null
-  if (nameParts.length !== 2) return null;
-
-  const [name, namespace] = nameParts;
-  const nameCV = tupleCV({
-    name: bufferCV(asciiToBytes(name)),
-    namespace: bufferCV(asciiToBytes(namespace)),
-  });
-
-  const res = await client.callReadOnlyFunction({
-    ...bnsContractConsts,
-    functionName: 'get-name-properties',
-    tip: 'latest',
-    readOnlyFunctionArgs: {
-      // Sender is irrelevant
-      sender: bnsContractConsts.contractAddress,
-      arguments: [cvToHex(nameCV)],
-    },
-  });
-
-  if (!res.okay || !res.result) return null;
-  const { result } = res;
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-  const cv = deserializeCV(result) as OptionalCV<TupleCV<{ owner: PrincipalCV; id: UIntCV }>>;
-  if (cv.type === ClarityType.OptionalNone) return null;
-  const ownerCV = cv.value.data.owner;
-  return principalToString(ownerCV);
-}
-
 /**
  * Fetch names owned by an address.
- *
- * If `isTestnet` is `false` (aka mainnet), names are fetched from the
- * BNSx contract directly.
  */
 interface FetchNamesForAddressArgs {
   client: StacksClient;
@@ -123,23 +27,17 @@ export async function fetchNamesForAddress({
     return fetchFromApi();
   }
 
-  // Return BNSx name if available, otherwise return names from API
-  const [bnsxName, bnsNames] = await Promise.all([
-    fetchBnsxName(client, address, signal),
-    fetchFromApi(),
-  ]);
+  const bnsNames = await fetchFromApi();
+
   const bnsName = 'names' in bnsNames ? bnsNames.names[0] : null;
   const names: string[] = [];
   if (bnsName) names.push(bnsName);
-  if (bnsxName) names.push(bnsxName);
+
   return { names };
 }
 
 /**
  * Fetch the owner of a name.
- *
- * If on mainnet, this function concurrently fetches a BNSx owner from the contract
- * and a BNS owner from the API.
  */
 export async function fetchNameOwner(client: StacksClient, name: string, isTestnet: boolean) {
   const fetchFromApi = async () => {
@@ -148,12 +46,12 @@ export async function fetchNameOwner(client: StacksClient, name: string, isTestn
     if (!isString(res.address) || res.address.length === 0) return null;
     return res.address;
   };
+
   if (isTestnet) {
     return fetchFromApi();
   }
 
-  const [bnsxOwner, apiOwner] = await Promise.all([fetchBnsxOwner(client, name), fetchFromApi()]);
-  return bnsxOwner ?? apiOwner;
+  return fetchFromApi();
 }
 
 /**
