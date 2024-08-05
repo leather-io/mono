5aa7764f77005199786a2e9b634a4c12d2f78a0d
Anastasios <anastas3993@gmail.com>
2024-08-05 16:13:06 +0400

    feat: rate limit all bis requests, closes leather-io/extension#5707

--- packages/query/src/bitcoin/runes/runes-ticker-info.query.ts
+++ packages/query/src/bitcoin/runes/runes-ticker-info.query.ts
@@ -1,10 +1,12 @@
 import { useQueries } from '@tanstack/react-query';
 import BigNumber from 'bignumber.js';
+import PQueue from 'p-queue';
 
 import { baseCurrencyAmountInQuote, createMoney, isDefined } from '@leather.io/utils';
 
 import { useCryptoCurrencyMarketDataMeanAverage } from '../../common/market-data/market-data.hooks';
 import { BitcoinQueryPrefixes } from '../../query-prefixes';
+import { useBestInSlotApiRateLimiter } from '../../rate-limiter/best-in-slot-limiter';
 import { RuneBalance, RuneTickerInfo } from '../clients/best-in-slot';
 import { BitcoinClient, useBitcoinClient } from '../clients/bitcoin-client';
 import { useRunesEnabled } from './runes.hooks';
@@ -16,16 +18,21 @@ interface CreateGetRunesTickerInfoQueryOptionsArgs {
   client: BitcoinClient;
   runeBalance: RuneBalance;
   runesEnabled: boolean;
+  limiter: PQueue;
 }
 export function createGetRunesTickerInfoQueryOptions({
   client,
   runeBalance,
   runesEnabled,
+  limiter,
 }: CreateGetRunesTickerInfoQueryOptionsArgs) {
   return {
     enabled: isDefined(runeBalance) && runesEnabled,
     queryKey: [BitcoinQueryPrefixes.GetRunesTickerInfo, runeBalance.rune_name],
-    queryFn: () => client.BestinSlotApi.getRunesTickerInfo(runeBalance.rune_name),
+    queryFn: () =>
+      limiter.add(() => client.BestinSlotApi.getRunesTickerInfo(runeBalance.rune_name), {
+        throwOnTimeout: true,
+      }),
     ...queryOptions,
   } as const;
 }
@@ -34,11 +41,12 @@ export function useGetRunesTickerInfoQuery(runesBalances: RuneBalance[]) {
   const client = useBitcoinClient();
   const btcMarketData = useCryptoCurrencyMarketDataMeanAverage('BTC');
   const runesEnabled = useRunesEnabled();
+  const limiter = useBestInSlotApiRateLimiter();
 
   return useQueries({
     queries: runesBalances.map(runeBalance => {
       return {
-        ...createGetRunesTickerInfoQueryOptions({ client, runeBalance, runesEnabled }),
+        ...createGetRunesTickerInfoQueryOptions({ client, runeBalance, runesEnabled, limiter }),
         select: (resp: RuneTickerInfo) => {
           const fiatPrice = baseCurrencyAmountInQuote(
             createMoney(new BigNumber(resp.min_listed_unit_price_in_sats ?? 0), 'BTC'),
--- packages/query/src/bitcoin/runes/runes-wallet-balances.query.ts
+++ packages/query/src/bitcoin/runes/runes-wallet-balances.query.ts
@@ -1,10 +1,12 @@
 import { useQueries } from '@tanstack/react-query';
+import PQueue from 'p-queue';
 
 import { NetworkConfiguration } from '@leather.io/models';
 
 import { useConfigRunesEnabled } from '../../common/remote-config/remote-config.query';
 import { useLeatherNetwork } from '../../leather-query-provider';
 import { BitcoinQueryPrefixes } from '../../query-prefixes';
+import { useBestInSlotApiRateLimiter } from '../../rate-limiter/best-in-slot-limiter';
 import { BitcoinClient, useBitcoinClient } from '../clients/bitcoin-client';
 
 const queryOptions = { staleTime: 5 * 60 * 1000 } as const;
@@ -14,17 +16,22 @@ interface CreateGetRunesWalletBalancesByAddressesQueryOptionsArgs {
   client: BitcoinClient;
   network: NetworkConfiguration;
   runesEnabled: boolean;
+  limiter: PQueue;
 }
 export function createGetRunesWalletBalancesByAddressesQueryOptions({
   address,
   client,
   network,
   runesEnabled,
+  limiter,
 }: CreateGetRunesWalletBalancesByAddressesQueryOptionsArgs) {
   return {
     enabled: !!address && (network.chain.bitcoin.bitcoinNetwork === 'testnet' || runesEnabled),
     queryKey: [BitcoinQueryPrefixes.GetRunesWalletBalances, address],
-    queryFn: () => client.BestinSlotApi.getRunesWalletBalances(address),
+    queryFn: () =>
+      limiter.add(() => client.BestinSlotApi.getRunesWalletBalances(address), {
+        throwOnTimeout: true,
+      }),
     ...queryOptions,
   } as const;
 }
@@ -33,6 +40,7 @@ export function useGetRunesWalletBalancesByAddressesQuery(addresses: string[]) {
   const client = useBitcoinClient();
   const network = useLeatherNetwork();
   const runesEnabled = useConfigRunesEnabled();
+  const limiter = useBestInSlotApiRateLimiter();
 
   return useQueries({
     queries: addresses.map(address => {
@@ -41,6 +49,7 @@ export function useGetRunesWalletBalancesByAddressesQuery(addresses: string[]) {
         client,
         network,
         runesEnabled,
+        limiter,
       });
     }),
   });
