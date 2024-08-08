d3d304b2fb490ee770cbe339f182969a9dddb674
kyranjamie <kyran.burraston@gmail.com>
2024-08-08 13:27:27 +0200

    fix: improve watch mode

--- packages/constants/tsup.config.ts
+++ packages/constants/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
-  clean: true,
+  clean: false,
   dts: true,
   format: 'esm',
 });
