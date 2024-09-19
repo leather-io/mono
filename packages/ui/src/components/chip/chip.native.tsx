9db9f27b388edce79edda024ed32bf238980b3d9
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-09-19 11:18:38 +0100

    fix: deprecate defaultProps usage, closes leather-io/issues#324

--- packages/ui/src/components/chip/chip.native.tsx
+++ packages/ui/src/components/chip/chip.native.tsx
@@ -1,28 +1,27 @@
-import { createBox } from '@shopify/restyle';
+import { BoxProps } from '@shopify/restyle';
 
-import { Text } from '../../../native';
-import { type Theme } from '../../theme-native';
+import { Box, Text, Theme } from '../../../native';
 
-interface ChipProps {
+interface ChipProps extends BoxProps<Theme> {
   label: string | number;
 }
 
-export const Chip = createBox<Theme, ChipProps>(({ label, ...rest }) => (
-  <Text variant="label02" width="5" height="4" {...rest}>
-    {label}
-  </Text>
-));
-
-//  FIXME double check padding and gap with design
-Chip.defaultProps = {
-  backgroundColor: 'ink.component-background-default',
-  borderRadius: 'xs',
-  paddingLeft: '1',
-  paddingRight: '1',
-  paddingTop: '0', // says 2px in figma but no token for that
-  paddingBottom: '0', // says 2px in figma but no token for that
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  gap: '3', // 12px - says 10px in figma but no token for that
-};
+export function Chip({ label, ...rest }: ChipProps) {
+  return (
+    <Box
+      backgroundColor="ink.component-background-default"
+      borderRadius="xs"
+      paddingLeft="1"
+      paddingRight="1"
+      paddingTop="0" // says 2px in figma but no token for that
+      paddingBottom="0" // says 2px in figma but no token for that
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap="3" // 12px - says 10px in figma but no token for that
+      {...rest}
+    >
+      <Text variant="label02">{label}</Text>
+    </Box>
+  );
+}
