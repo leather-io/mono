682b20921b299fe665bddcf9235dd72254f13472
Anastasios <anastas3993@gmail.com>
2024-10-11 15:27:15 +0400

    feat: add tokens enabled config hook

--- packages/query/src/common/remote-config/remote-config.query.ts
+++ packages/query/src/common/remote-config/remote-config.query.ts
@@ -120,3 +120,8 @@ export function useConfigSwapsEnabled() {
   const config = useRemoteConfig();
   return get(config, 'swapsEnabled', false);
 }
+
+export function useConfigTokensEnabledByDefault() {
+  const config = useRemoteConfig();
+  return get(config, 'tokensEnabledByDefault', false);
+}
