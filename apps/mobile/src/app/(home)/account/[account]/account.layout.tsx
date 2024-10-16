e3dab857adb34c0690e9ceb4b08cbd8b2cc5e919
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-10-16 15:27:57 +0100

    feat: load stx data in account page

--- apps/mobile/src/app/(home)/account/[account]/account.layout.tsx
+++ apps/mobile/src/app/(home)/account/[account]/account.layout.tsx
@@ -1,35 +1,39 @@
 import { getAvatarIcon } from '@/components/avatar-icon';
 import { Balance } from '@/components/balance/balance';
 import { PageLayout } from '@/components/page/page.layout';
-import {
-  CollectiblesWidget,
-  mockCollectibles,
-  serializeCollectibles,
-} from '@/components/widgets/collectibles';
-import { TokensWidget } from '@/components/widgets/tokens';
+// import {
+//   CollectiblesWidget,
+//   mockCollectibles,
+//   serializeCollectibles,
+// } from '@/components/widgets/collectibles';
+// import { TokensWidget } from '@/components/widgets/tokens';
 import { MockedAccount } from '@/mocks/account.mocks';
 import { mockTotalBalance } from '@/mocks/balance.mocks';
-import { getMockTokens } from '@/mocks/tokens.mocks';
 
+import { Money } from '@leather.io/models';
+
+// import { getMockTokens } from '@/mocks/tokens.mocks';
 import { AccountOverview } from './account-overview-card';
 
 interface AccountLayoutProps {
   account: MockedAccount;
+  balance: Money;
 }
 
-export function AccountLayout({ account }: AccountLayoutProps) {
+export function AccountLayout({ account, balance }: AccountLayoutProps) {
   return (
     <PageLayout>
       <AccountOverview
         Icon={getAvatarIcon(account.icon)}
-        heading={<Balance balance={mockTotalBalance} variant="heading02" />}
+        heading={<Balance balance={balance} variant="heading02" />}
         caption={account.name}
       />
-      <TokensWidget tokens={getMockTokens()} totalBalance={mockTotalBalance} />
+      {/* TODO - re-enable widgets when real data available */}
+      {/* <TokensWidget tokens={getMockTokens()} totalBalance={mockTotalBalance} />
       <CollectiblesWidget
         collectibles={serializeCollectibles(mockCollectibles)}
         totalBalance={mockTotalBalance}
-      />
+      /> */}
     </PageLayout>
   );
 }
--- apps/mobile/src/app/(home)/account/[account]/index.tsx
+++ apps/mobile/src/app/(home)/account/[account]/index.tsx
@@ -3,6 +3,12 @@ import { AccountLoader } from '@/store/accounts/accounts';
 import { useLocalSearchParams } from 'expo-router';
 import { z } from 'zod';
 
+import {
+  useCryptoCurrencyMarketDataMeanAverage,
+  useStxCryptoAssetBalance,
+} from '@leather.io/query';
+import { baseCurrencyAmountInQuote, createMoney } from '@leather.io/utils';
+
 import { AccountLayout } from './account.layout';
 
 const configureAccountParamsSchema = z.object({
@@ -12,11 +18,25 @@ const configureAccountParamsSchema = z.object({
 
 export default function AccountScreen() {
   const params = useLocalSearchParams();
+  // Pete - get this from the account
+  const stxAddress = 'SP2417H88DQFN7FNDMSKM9N0B3Q6GNGEM40W7ZAZW';
+
+  const { filteredBalanceQuery } = useStxCryptoAssetBalance(stxAddress);
   const { fingerprint, account: accountIndex } = configureAccountParamsSchema.parse(params);
 
+  // Logic is coming from useTotalBalance in extension which needs to be shared
+  // BUT when digging into BTC balances sends you down a loop of hooks using redux and the account store
+  const stxMarketData = useCryptoCurrencyMarketDataMeanAverage('STX');
+
+  const balance = filteredBalanceQuery.data;
+  const stxBalance = balance ? balance.totalBalance : createMoney(0, 'STX');
+
+  const stxUsdAmount = baseCurrencyAmountInQuote(stxBalance, stxMarketData);
+  // PETE show stxBalance in tokens widget!
+  // get STX address from account data
   return (
     <AccountLoader fingerprint={fingerprint} accountIndex={accountIndex}>
-      {account => <AccountLayout account={account as MockedAccount} />}
+      {account => <AccountLayout balance={stxUsdAmount} account={account as MockedAccount} />}
     </AccountLoader>
   );
 }
