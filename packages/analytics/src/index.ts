3b70b2c5740f30cbe595c0247898e5f412470e2a
Will Cameron <camerow@users.noreply.github.com>
2024-11-01 13:06:22 -0700

    refactor!: small change to trigger publish, closes LEA-1692

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -2,6 +2,7 @@ import { AnalyticsClient } from './client';
 import { AnalyticsClientInterface, DefaultProperties, JsonMap } from './types';
 
 export * from './types';
+
 export function configureAnalyticsClient<T extends AnalyticsClientInterface>({
   client,
   defaultProperties,
@@ -11,5 +12,5 @@ export function configureAnalyticsClient<T extends AnalyticsClientInterface>({
   defaultProperties: DefaultProperties;
   defaultTraits?: JsonMap;
 }) {
-  return AnalyticsClient<T>(client, { defaultProperties, defaultTraits }); // No need to pass EventProperties
+  return AnalyticsClient<T>(client, { defaultProperties, defaultTraits });
 }
