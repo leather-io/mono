0bfdb04fee0af1ed3db62be3616b1e6baa94f264
Will Cameron <camerow@users.noreply.github.com>
2024-11-26 12:29:07 -0800

    chore: use existing pattern

--- packages/ui/src/icons/arrow-down-icon.native.tsx
+++ packages/ui/src/icons/arrow-down-icon.native.tsx
@@ -1,20 +1,19 @@
 import { Component, forwardRef } from 'react';
 
-import { IconVariantMap } from 'src/icons/types.shared';
-
 import ArrowDownSmall from '../assets/icons/arrow-down-16-16.svg';
 import ArrowDown from '../assets/icons/arrow-down-24-24.svg';
 import { Icon, IconProps } from './icon/icon.native';
 
-const variants: IconVariantMap = {
-  small: <ArrowDownSmall />,
-  default: <ArrowDown />,
-};
-
 export const ArrowDownIcon = forwardRef<Component, IconProps>(({ variant, ...props }, ref) => {
+  if (variant === 'small')
+    return (
+      <Icon ref={ref} {...props}>
+        <ArrowDownSmall />
+      </Icon>
+    );
   return (
     <Icon ref={ref} {...props}>
-      {variants[variant ?? 'default']}
+      <ArrowDown />
     </Icon>
   );
 });
