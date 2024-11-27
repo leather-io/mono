60807702e32b5e7027a726a91ef683b6e2927f5f
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-11-27 10:33:49 +0000

    chore: add logging of utxo query keys

--- packages/query/src/bitcoin/balance/btc-blockbook-utxos.ts
+++ packages/query/src/bitcoin/balance/btc-blockbook-utxos.ts
@@ -37,6 +37,19 @@ export async function fetchAccountDescriptorUtxos(network: BitcoinNetwork, descr
 }
 
 export function createUtxoQueryOptions(network: BitcoinNetwork, descriptor: string) {
+  // maybe there is some issue with the cache key?
+  // this runs twice and adds two separate keys, once each for seqwit + tr
+  /**
+  LOG  createUtxoQueryOptions mainnet wpkh(xpub6CuWSJTu4HJMVB7Se7gLUF6n27GWyJt4vWHmS5fP82T5nAkUYcCEVXMvhnLniazkvCG7pCCh1N8vA8mz9uVim97d1LuBpwxfRvTDPVeJCht) ["btc-utxos", "mainnet", "wpkh(xpub6"]
+ LOG  createUtxoQueryOptions mainnet tr(xpub6CkDCXCcRuVxpyemADVdeVXgNNKAeRtvzByNoJUm7aehjDm2Gn3BV2cU4BnK72UpY2JyRVyj9NcXV39PZKcZcEQ3ixgtgsADXt1skMiRY41) ["btc-utxos", "mainnet", "tr(xpub6Ck"]
+
+   */
+  console.log(
+    'createUtxoQueryOptions',
+    network,
+    descriptor,
+    createUtxoAccountCacheKey(network, descriptor)
+  );
   return {
     refetchOnReconnect: false,
     refetchOnWindowFocus: false,
