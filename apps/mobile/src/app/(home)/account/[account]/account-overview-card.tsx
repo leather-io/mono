0c136290fb89046c86e658650f0c8f62aeda8b92
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-11-27 13:19:45 +0000

    chore: design updates to account page, ref LEA-1794

--- apps/mobile/src/app/(home)/account/[account]/account-overview-card.tsx
+++ apps/mobile/src/app/(home)/account/[account]/account-overview-card.tsx
@@ -12,26 +12,16 @@ export function AccountOverview({ caption, icon, heading }: AccountOverviewProps
       alignItems="flex-start"
       alignContent="flex-start"
       alignSelf="stretch"
-      m="5"
+      mx="5"
       flexWrap="wrap"
     >
-      <Box
-        p="5"
-        width={342}
-        height={180}
-        flexDirection="column"
-        alignItems="center"
-        gap="3"
-        flex={1}
-      >
+      <Box width={342} height={180} flexDirection="column" alignItems="center" gap="3" flex={1}>
         <Box p="2" borderRadius="round" backgroundColor="ink.text-primary">
           {icon}
         </Box>
         <Box gap="1" flexDirection="column" alignItems="center">
           {heading}
-          <Text variant="caption01" color="ink.text-subdued">
-            {caption}
-          </Text>
+          <Text variant="caption01">{caption}</Text>
         </Box>
       </Box>
     </Box>
