8faf4186f2363d6e93a90967085fa8f3ceb428ea
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-11-27 12:56:01 +0000

    fix: only show tokenlist if the user has wallets, ref #LEA-1787

--- apps/mobile/src/app/(home)/home/home.tsx
+++ apps/mobile/src/app/(home)/home/home.tsx
@@ -1,16 +1,20 @@
 import { PageLayout } from '@/components/page/page.layout';
 import { AccountsWidget } from '@/components/widgets/accounts/accounts-widget';
 import { AllAccountBalances, TokensWidget } from '@/components/widgets/tokens/tokens-widget';
+import { useWallets } from '@/store/wallets/wallets.read';
 import { useLingui } from '@lingui/react';
 
 export function Home() {
   useLingui();
+  const { hasWallets } = useWallets();
   return (
     <PageLayout>
       <AccountsWidget />
-      <TokensWidget>
-        <AllAccountBalances />
-      </TokensWidget>
+      {hasWallets && (
+        <TokensWidget>
+          <AllAccountBalances />
+        </TokensWidget>
+      )}
     </PageLayout>
   );
 }
