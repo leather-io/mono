6f6b6fda41ede79a834ffd15a79430d8d16dc68d
Tigran <3755268+tigranpetrossian@users.noreply.github.com>
2024-12-03 12:54:45 +0400

    chore: update to eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import pluginLingui from 'eslint-plugin-lingui';
+import tseslint from 'typescript-eslint';
+
+import baseConfig from '@leather.io/eslint-config';
+import reactConfig from '@leather.io/eslint-config/react';
+
+export default tseslint.config(
+  {
+    files: ['{packages,apps}/**/src/**/*.{ts,tsx}'],
+    extends: [baseConfig],
+  },
+  {
+    ignores: [
+      '**/*.{js,cjs,mjs}',
+      '**/node_modules/',
+      '**/leather-styles/',
+      '**/dist/',
+      '**/dist-web/',
+      '**/dist-native/',
+      '**/.expo/',
+      '**/.turbo/',
+      '**/.tsup/',
+      '**/*.d.ts',
+      '**/*.stories.{ts,tsx}',
+      '**/tsup.config*.ts',
+    ],
+  },
+  {
+    name: 'ui',
+    files: ['packages/ui/src/**/*.{ts,tsx}'],
+    extends: [reactConfig],
+  },
+  {
+    name: 'query',
+    files: ['packages/query/src/**/*.{ts,tsx}'],
+    extends: [reactConfig, pluginQuery.configs['flat/recommended']],
+  },
+  {
+    name: 'mobile',
+    files: ['apps/mobile/src/**/*.{ts,tsx}'],
+    extends: [reactConfig, pluginLingui.configs['flat/recommended']],
+    rules: {
+      'lingui/no-unlocalized-strings': [
+        'error',
+        {
+          ignoreFunction: [
+            'Error',
+            'console.log',
+            'console.warn',
+            'console.error',
+            'it',
+            'describe',
+            'test',
+          ],
+        },
+      ],
+    },
+  }
+);
