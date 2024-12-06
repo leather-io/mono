a8b8854e6ac1e0a495a93bf71b9d62f0d0ec3695
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-12-06 12:38:55 +0000

    fix: add wallet name and header to account selector, ref #LEA-1828

---
+++ apps/mobile/src/features/accounts/account-selector/account-selector-sheet-header.tsx
@@ -0,0 +1,39 @@
+import { RefObject } from 'react';
+
+import { AppRoutes } from '@/routes';
+import { TestId } from '@/shared/test-id';
+import { t } from '@lingui/macro';
+import { router } from 'expo-router';
+
+import { Box, SettingsGearIcon, SheetRef, Text, TouchableOpacity } from '@leather.io/ui/native';
+
+interface AccountSelectorHeaderProps {
+  sheetRef: RefObject<SheetRef>;
+}
+// TODO: refactor HeaderLayout so that we can use it here
+export function AccountSelectorHeader({ sheetRef }: AccountSelectorHeaderProps) {
+  return (
+    <Box alignItems="center" flexDirection="row" justifyContent="space-between">
+      <Box alignItems="center" justifyContent="center" left={0} position="absolute" right={0}>
+        <Text variant="heading05">
+          {t({
+            id: 'select_account.header_title',
+            message: 'Accounts',
+          })}
+        </Text>
+      </Box>
+      <Box alignItems="flex-end" flexGrow={1} justifyContent="center" zIndex="20">
+        <TouchableOpacity
+          p="2"
+          onPress={() => {
+            sheetRef.current?.close();
+            router.navigate(AppRoutes.SettingsWallet);
+          }}
+          testID={TestId.settingsWalletAndAccountsButton}
+        >
+          <SettingsGearIcon />
+        </TouchableOpacity>
+      </Box>
+    </Box>
+  );
+}
