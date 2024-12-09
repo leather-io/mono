cb7e5557b199d9b7f8933cdeb30ebbfbbb3473b8
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2024-12-09 23:32:37 +0400

    fix: localization strings

--- apps/mobile/src/features/settings/account-identifier-sheet.tsx
+++ apps/mobile/src/features/settings/account-identifier-sheet.tsx
@@ -1,20 +1,35 @@
 import { RefObject } from 'react';
 import { Linking } from 'react-native';
 
+import { getAccountDisplayPreferencesKeyedByType } from '@/common/display-preference';
 import { SettingsList } from '@/components/settings/settings-list';
 import { SettingsListItem } from '@/components/settings/settings-list-item';
 import { useToastContext } from '@/components/toast/toast-context';
 import { LEATHER_GUIDES_MOBILE_ACCOUNT_IDENTIFIER } from '@/shared/constants';
 import { useSettings } from '@/store/settings/settings';
 import { t } from '@lingui/macro';
-import { useLingui } from '@lingui/react';
 
-import { accountDisplayPreferencesKeyedByType } from '@leather.io/constants';
 import { AccountDisplayPreference } from '@leather.io/models';
 import { PackageSecurityIcon, SheetRef } from '@leather.io/ui/native';
-import { capitalize } from '@leather.io/utils';
+import { match } from '@leather.io/utils';
 
 import { SettingsSheetLayout } from './settings-sheet.layout';
+
+function getAccountDisplayPrefDescription(preference: AccountDisplayPreference) {
+  const matchPreference = match<AccountDisplayPreference>();
+  return matchPreference(preference, {
+    taproot: t({
+      id: 'account_display_preference.description.taproot',
+      message: 'Layer 1 • Bitcoin',
+    }),
+    'native-segwit': t({
+      id: 'account_display_preference.description.native_segwit',
+      message: 'Layer 1 • Bitcoin',
+    }),
+    bns: t({ id: 'account_display_preference.description.bns', message: 'Layer 2 • Stacks' }),
+    stacks: t({ id: 'account_display_preference.description.stacks', message: 'Layer 2 • Stacks' }),
+  });
+}
 
 interface AccountIdentifierSheetProps {
   sheetRef: RefObject<SheetRef>;
@@ -22,7 +37,6 @@ interface AccountIdentifierSheetProps {
 export function AccountIdentifierSheet({ sheetRef }: AccountIdentifierSheetProps) {
   const settings = useSettings();
   const { displayToast } = useToastContext();
-  const { i18n } = useLingui();
 
   function onUpdateAccountDisplayPreference(identifier: AccountDisplayPreference) {
     settings.changeAccountDisplayPreference(identifier);
@@ -44,22 +58,15 @@ export function AccountIdentifierSheet({ sheetRef }: AccountIdentifierSheetProps
         message: 'Account identifier',
       })}
       onPressSupport={() => {
-        Linking.openURL(LEATHER_GUIDES_MOBILE_ACCOUNT_IDENTIFIER);
+        void Linking.openURL(LEATHER_GUIDES_MOBILE_ACCOUNT_IDENTIFIER);
       }}
     >
       <SettingsList gap="0">
-        {Object.values(accountDisplayPreferencesKeyedByType).map(accountDisplayPref => (
+        {Object.values(getAccountDisplayPreferencesKeyedByType()).map(accountDisplayPref => (
           <SettingsListItem
             key={accountDisplayPref.name}
-            title={i18n._({
-              id: 'account_identifier.cell_title',
-              message: '{name}',
-              values: { name: accountDisplayPref.name },
-            })}
-            caption={t({
-              id: 'account_identifier.cell_caption',
-              message: `${capitalize(i18n._(accountDisplayPref.blockchain))} blockchain`,
-            })}
+            title={accountDisplayPref.name}
+            caption={getAccountDisplayPrefDescription(accountDisplayPref.type)}
             onPress={() => onUpdateAccountDisplayPreference(accountDisplayPref.type)}
             type="radio"
             isRadioSelected={settings.accountDisplayPreference.type === accountDisplayPref.type}
