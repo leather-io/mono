23c1544b1f9e71b56f5a7acf4741242681e94691
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2024-12-13 16:11:58 +0000

    fix: add empty states for wallet config and hidden accounts, ref LEA-1496

--- apps/mobile/src/app/(home)/settings/wallet/index.tsx
+++ apps/mobile/src/app/(home)/settings/wallet/index.tsx
@@ -6,9 +6,11 @@ import { AnimatedHeaderScreenLayout } from '@/components/headers/animated-header
 import { SettingsList } from '@/components/settings/settings-list';
 import { SettingsListItem } from '@/components/settings/settings-list-item';
 import { NetworkBadge } from '@/features/settings/network-badge';
+import { EmptyWalletsScreen } from '@/features/settings/wallet-and-accounts/components/empty-wallets-screen';
 import { WalletsList } from '@/features/settings/wallet-and-accounts/wallets-list';
 import { AppRoutes } from '@/routes';
 import { useAccounts } from '@/store/accounts/accounts.read';
+import { useWallets } from '@/store/wallets/wallets.read';
 import { t } from '@lingui/macro';
 import { useRouter } from 'expo-router';
 
@@ -19,6 +21,8 @@ export default function SettingsWalletScreen() {
   const addWalletSheetRef = useRef<SheetRef>(null);
   const hiddenAccounts = useAccounts('hidden');
   const hiddenAccountsLength = hiddenAccounts.list.length;
+  const { list: walletsList } = useWallets();
+  const hasWallets = walletsList.length > 0;
   return (
     <>
       <AnimatedHeaderScreenLayout
@@ -28,34 +32,44 @@ export default function SettingsWalletScreen() {
           message: 'Wallets',
         })}
       >
-        <WalletsList variant="active" />
-        <Divider />
-        <SettingsList paddingTop="3">
-          <SettingsListItem
-            title={t({
-              id: 'wallet.hidden_accounts.cell_title',
-              message: 'Hidden accounts',
-            })}
-            caption={t({
-              id: 'wallet.hidden_accounts.cell_caption',
-              message: `${hiddenAccountsLength} hidden accounts`,
-            })}
-            icon={<Eye1ClosedIcon />}
-            onPress={() => {
-              router.navigate(AppRoutes.SettingsWalletHiddenAccounts);
-            }}
-          />
-          <SettingsListItem
-            title={t({
-              id: 'wallet.add_wallet.cell_title',
-              message: 'Add wallet',
-            })}
-            icon={<PlusIcon />}
-            onPress={() => {
+        {hasWallets ? (
+          <>
+            <WalletsList variant="active" />
+            <Divider />
+            <SettingsList paddingTop="3">
+              <SettingsListItem
+                title={t({
+                  id: 'wallet.hidden_accounts.cell_title',
+                  message: 'Hidden accounts',
+                })}
+                caption={t({
+                  id: 'wallet.hidden_accounts.cell_caption',
+                  message: `${hiddenAccountsLength} hidden accounts`,
+                })}
+                icon={<Eye1ClosedIcon />}
+                onPress={() => {
+                  router.navigate(AppRoutes.SettingsWalletHiddenAccounts);
+                }}
+              />
+              <SettingsListItem
+                title={t({
+                  id: 'wallet.add_wallet.cell_title',
+                  message: 'Add wallet',
+                })}
+                icon={<PlusIcon />}
+                onPress={() => {
+                  addWalletSheetRef.current?.present();
+                }}
+              />
+            </SettingsList>
+          </>
+        ) : (
+          <EmptyWalletsScreen
+            onPressCreateWallet={() => {
               addWalletSheetRef.current?.present();
             }}
           />
-        </SettingsList>
+        )}
       </AnimatedHeaderScreenLayout>
       <AddWalletSheet addWalletSheetRef={addWalletSheetRef} />
     </>
