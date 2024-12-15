477e67363e775645cba96999cdfa72210f80c33e
Tigran <3755268+tigranpetrossian@users.noreply.github.com>
2024-12-15 14:41:11 +0400

    fix: adjust vertical centering of incorrectly rendered text

--- packages/ui/src/components/text/text.native.tsx
+++ packages/ui/src/components/text/text.native.tsx
@@ -10,6 +10,29 @@ const RestyleText = createText<Theme>();
 type TextElement = ElementRef<typeof RestyleText>;
 export type TextProps = RNTexProps & RestyleTextProps<Theme>;
 
-export const Text = forwardRef<TextElement, TextProps>((props, ref) => {
-  return <RestyleText ref={ref} color="ink.text-primary" {...props} />;
+// Manually adjust vertical centering of text affected by incorrect rendering of react-native:
+// https://github.com/facebook/react-native/issues/29507
+//
+// This is currently the only low-cost, better-than-nothing solution with effectively 0 runtime overhead.
+// It is intentionally overrideable: passing paddingTop/marginBottom will negate the effect of the adjustment,
+// reverting that particular usage from better to nothing.
+function getAdjustedTextStyle(variant: string | undefined) {
+  if (!variant) {
+    return;
+  }
+
+  return {
+    display02: { paddingTop: 14, marginBottom: -14 },
+    heading01: { paddingTop: 5, marginBottom: -5 },
+    heading02: { paddingTop: 6, marginBottom: -6 },
+    heading03: { paddingTop: 3, marginBottom: -3 },
+  }[variant];
+}
+
+export const Text = forwardRef<TextElement, TextProps>(({ style, ...rest }, ref) => {
+  const adjustmentStyle = getAdjustedTextStyle(rest.variant);
+
+  return (
+    <RestyleText ref={ref} color="ink.text-primary" style={[adjustmentStyle, style]} {...rest} />
+  );
 });
