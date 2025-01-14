8eaf4d9dca82671e07d85e3d39d630848d116a8c
kyranjamie <kyran.burraston@gmail.com>
2025-01-14 11:02:43 +0100

    fix(rpc): add zod clarity type schemas

--- packages/rpc/src/methods/stacks/_stacks-helpers.ts
+++ packages/rpc/src/methods/stacks/_stacks-helpers.ts
@@ -5,93 +5,22 @@ export const stacksTransactionDetailsSchema = z.object({
   transaction: z.string(),
 });
 
-// Clarity values
-export const cvIntSchema = z.object({
-  type: z.literal('int'),
-  value: z.string(), // `bigint` compatible
+export const baseStacksTransactionConfigSchema = z.object({
+  address: z.string().optional(),
+  network: z
+    .union([
+      z.literal('mainnet'),
+      z.literal('testnet'),
+      z.literal('regtest'),
+      z.literal('devnet'),
+      z.literal('mocknet'),
+      z.string(),
+    ])
+    .optional(),
+  fee: z.coerce.number().optional(),
+  nonce: z.coerce.number().optional(),
+  // add pc later when imported from stacks.js
+  postConditions: z.array(z.unknown()).optional(),
+  postConditionMode: z.union([z.literal('allow'), z.literal('deny')]).optional(),
+  sponsored: z.boolean().optional(),
 });
-
-export const cvUintSchema = z.object({
-  type: z.literal('uint'),
-  value: z.string(), // `bigint` compatible
-});
-
-export const cvBufferSchema = z.object({
-  type: z.literal('buffer'),
-  value: z.string(), // hex-encoded string
-});
-
-export const cvTrueSchema = z.object({
-  type: z.literal('true'),
-});
-
-export const cvFalseSchema = z.object({
-  type: z.literal('false'),
-});
-
-export const cvAddressSchema = z.object({
-  type: z.literal('address'),
-  value: z.string(), // Stacks c32-encoded
-});
-
-export const cvContractSchema = z.object({
-  type: z.literal('contract'),
-  value: z.string(), // Stacks c32-encoded, with contract name suffix
-});
-
-export const cvOkSchema = z.object({
-  type: z.literal('ok'),
-  value: z.unknown(), // Clarity value
-});
-
-export const cvErrSchema = z.object({
-  type: z.literal('err'),
-  value: z.unknown(), // Clarity value
-});
-
-export const cvNoneSchema = z.object({
-  type: z.literal('none'),
-});
-
-export const cvSomeSchema = z.object({
-  type: z.literal('some'),
-  value: z.unknown(), // Clarity value
-});
-
-export const cvListSchema = z.object({
-  type: z.literal('list'),
-  value: z.array(z.unknown()), // Array of Clarity values
-});
-
-export const cvTupleSchema = z.object({
-  type: z.literal('tuple'),
-  value: z.record(z.unknown()), // Record of Clarity values
-});
-
-export const cvAsciiSchema = z.object({
-  type: z.literal('ascii'),
-  value: z.string(), // ASCII-compatible string
-});
-
-export const cvUtf8Schema = z.object({
-  type: z.literal('utf8'),
-  value: z.string(),
-});
-
-export const clarityValueSchema = z.union([
-  cvIntSchema,
-  cvUintSchema,
-  cvBufferSchema,
-  cvTrueSchema,
-  cvFalseSchema,
-  cvAddressSchema,
-  cvContractSchema,
-  cvOkSchema,
-  cvErrSchema,
-  cvNoneSchema,
-  cvSomeSchema,
-  cvListSchema,
-  cvTupleSchema,
-  cvAsciiSchema,
-  cvUtf8Schema,
-]);
--- packages/rpc/src/methods/stacks/stx-deploy-contract.ts
+++ packages/rpc/src/methods/stacks/stx-deploy-contract.ts
@@ -1,18 +1,24 @@
 import { z } from 'zod';
 
 import { DefineRpcMethod, RpcRequest, RpcResponse } from '../../rpc/schemas';
-import { stacksTransactionDetailsSchema } from './_stacks-helpers';
+import {
+  baseStacksTransactionConfigSchema,
+  stacksTransactionDetailsSchema,
+} from './_stacks-helpers';
 
 export const stxDeployContractMethodName = 'stx_deployContract';
 
 type StxDeployContractRequestMethodName = typeof stxDeployContractMethodName;
 
 // Request
-export const stxDeployContractRequestParamsSchema = z.object({
-  name: z.string(),
-  clarityCode: z.string(),
-  clarityVersion: z.coerce.number().optional(),
-});
+export const stxDeployContractRequestParamsSchema = z.intersection(
+  z.object({
+    name: z.string(),
+    clarityCode: z.string(),
+    clarityVersion: z.coerce.number().optional(),
+  }),
+  baseStacksTransactionConfigSchema
+);
 
 export type StxDeployContractRequestParams = z.infer<typeof stxDeployContractRequestParamsSchema>;
 
