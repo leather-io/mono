aa7def08094c9897a7df7a9b5e4892e866946e99
Tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-01-14 10:04:34 +0400

    fix: limit account sheet max height

--- apps/mobile/src/features/accounts/account-selector/account-selector-sheet.layout.tsx
+++ apps/mobile/src/features/accounts/account-selector/account-selector-sheet.layout.tsx
@@ -1,8 +1,10 @@
 import { RefObject, useRef } from 'react';
-import { ScrollView } from 'react-native';
+import { Dimensions, ScrollView } from 'react-native';
 import { useSharedValue } from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Draggable } from '@/components/draggable';
+import { HEADER_HEIGHT } from '@/shared/constants';
 import { Account } from '@/store/accounts/accounts';
 import { useSettings } from '@/store/settings/settings';
 import { WalletLoader } from '@/store/wallets/wallets.read';
@@ -29,6 +31,7 @@ export function AccountSelectorSheetLayout({
   swapAccountIndexes,
   sheetRef,
 }: AccountSelectorSheetLayoutProps) {
+  const { top } = useSafeAreaInsets();
   const scrollViewRef = useRef<ScrollView>(null);
   const placeholderIdx = useSharedValue<null | number>(null);
   const direction = useSharedValue<'down' | 'up'>('down');
@@ -39,6 +42,7 @@ export function AccountSelectorSheetLayout({
       shouldHaveContainer={false}
       ref={sheetRef}
       themeVariant={themeDerivedFromThemePreference}
+      maxDynamicContentSize={Dimensions.get('screen').height - top - HEADER_HEIGHT}
     >
       <BottomSheetScrollView ref={scrollViewRef}>
         <Box px="5" pb="7">
