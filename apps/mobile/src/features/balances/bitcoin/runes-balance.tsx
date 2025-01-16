f9b335710d4921b5c930d9d74f9a8303f5820272
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-01-16 11:28:26 +0000

    feat: add boilerplate for Runes balances, ref LEA-1726

---
+++ apps/mobile/src/features/balances/bitcoin/runes-balance.tsx
@@ -0,0 +1,77 @@
+import { TokenIcon } from '@/components/widgets/tokens/token-icon';
+import {
+  useRunesAccountBalance,
+  useRunesTotalBalance,
+} from '@/queries/balance/runes-balance.query';
+
+import { Money } from '@leather.io/models';
+import { BitcoinAccountIdentifier } from '@leather.io/services';
+import { PressableProps } from '@leather.io/ui/native';
+
+import { TokenBalance } from '../token-balance';
+
+interface RunesTokenBalanceProps extends PressableProps {
+  availableBalance: Money;
+  fiatBalance: Money;
+  symbol: string;
+  name: string;
+}
+export function RunesTokenBalance({
+  availableBalance,
+  fiatBalance,
+  name,
+  symbol,
+  ...rest
+}: RunesTokenBalanceProps) {
+  return (
+    <TokenBalance
+      ticker={symbol}
+      icon={<TokenIcon ticker={symbol} />} // TODO LEA-1909: add images from uri
+      tokenName={name}
+      protocol="sip10"
+      fiatBalance={fiatBalance}
+      availableBalance={availableBalance}
+      {...rest}
+    />
+  );
+}
+export function RunesBalance() {
+  const data = useRunesTotalBalance();
+
+  // TODO: handle balance loading & error states
+  if (data.state !== 'success') return;
+
+  return data.value?.accountBalances.map((balances: any) =>
+    balances.runes.map((balance: any, index: any) => (
+      <RunesTokenBalance
+        key={`${balance.asset.symbol}-${index}`}
+        symbol={balance.asset.symbol}
+        name={balance.asset.name}
+        availableBalance={balance.availableBalance}
+        fiatBalance={balance.totalBalance}
+        px="5"
+        py="3"
+      />
+    ))
+  );
+}
+interface RunesBalanceByAccountProps {
+  account: BitcoinAccountIdentifier;
+}
+export function RunesBalanceByAccount({ account }: RunesBalanceByAccountProps) {
+  const data = useRunesAccountBalance(account);
+
+  // TODO: handle balance loading & error states
+  if (data.state !== 'success') return;
+  return data.value?.runes.map((balance: any, index: any) => (
+    <RunesTokenBalance
+      key={`${balance.asset.symbol}-${index}`}
+      symbol={balance.asset.symbol}
+      name={balance.asset.name}
+      availableBalance={balance.availableBalance}
+      fiatBalance={balance.totalBalance}
+      px="5"
+      py="3"
+    />
+  ));
+}
