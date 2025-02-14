2a0c22357a8ac225f128b7fb11369736aa3da55b
Will Cameron <camerow@users.noreply.github.com>
2025-02-14 12:54:31 -0800

    chore: update expo config

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -28,10 +28,10 @@ export default () => {
     expo: {
       name: 'Leather',
       slug: 'leather-wallet-mobile',
-      version: '2.0.0',
+      version: '2.1.0',
       orientation: 'portrait',
       icon: './src/assets/icon.png',
-      scheme: 'myapp',
+      scheme: 'leather',
       userInterfaceStyle: 'automatic',
       updates: {
         fallbackToCacheTimeout: 0,
