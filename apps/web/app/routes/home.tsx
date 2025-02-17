4ee14acff53f4be96d3da9f494056b96ea9bbad3
kyranjamie <kyran.burraston@gmail.com>
2025-02-17 17:44:29 +0100

    fix: upgrade default pkgs

--- apps/web/app/routes/home.tsx
+++ apps/web/app/routes/home.tsx
@@ -1,10 +1,32 @@
-import { Welcome } from '../welcome/welcome';
+import { delay } from '@leather.io/utils';
+
 import type { Route } from './+types/home';
 
+let name: null | string = null;
+
+export async function loader() {
+  if (name) {
+    return { name };
+  }
+  await delay(3000);
+
+  name = 'cached name';
+  return { name: 'lkjsdflksjd' };
+}
+
+// eslint-disable-next-line no-empty-pattern
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Leather' }, { name: 'description', content: 'Bitcoin for the rest of us' }];
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function HomeRoute({ loaderData }: Route.ComponentProps) {
+  // return <Home params={{}} loaderData={undefined} matches={[]} />;
+
+  return (
+    <main>
+      <div>
+        <header>Leather Earn {loaderData.name}</header>
+      </div>
+    </main>
+  );
 }
