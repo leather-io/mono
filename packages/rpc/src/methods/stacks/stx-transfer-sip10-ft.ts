6dd01c34f39c62f233420a4899f2a801b71c582b
Fara Woolf <fara@trustmachines.co>
2025-02-17 13:38:44 -0500

    fix: stx transfer sip10 ft types

--- packages/rpc/src/methods/stacks/stx-transfer-sip10-ft.ts
+++ packages/rpc/src/methods/stacks/stx-transfer-sip10-ft.ts
@@ -1,16 +1,20 @@
 import { z } from 'zod';
 
 import { defineRpcEndpoint } from '../../rpc/schemas';
-import { stacksTransactionDetailsSchema } from './_stacks-helpers';
-
-// Result
+import {
+  baseStacksTransactionConfigSchema,
+  stacksTransactionDetailsSchema,
+} from './_stacks-helpers';
 
 export const stxTransferSip10Ft = defineRpcEndpoint({
   method: 'stx_transferSip10Ft',
-  params: z.object({
-    recipient: z.string(),
-    asset: z.string(),
-    amount: z.coerce.number(),
-  }),
+  params: z.object(
+    {
+      recipient: z.string(),
+      asset: z.string(),
+      amount: z.coerce.number(),
+    },
+    baseStacksTransactionConfigSchema
+  ),
   result: stacksTransactionDetailsSchema,
 });
