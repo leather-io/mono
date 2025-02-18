4dd48c334d0f10709bc4d6e078c11b89545d9606
Will Cameron <camerow@users.noreply.github.com>
2025-02-18 14:29:58 -0800

    fix: try to not ignore service files when running eas build

--- apps/mobile/eas-build-pre-install.sh
+++
@@ -1,2 +0,0 @@
-echo $GOOGLE_SERVICES_JSON_B64 | base64 --decode > ./android/app/google-services.json
-echo $GOOGLE_SERVICES_INFO_PLIST_B64 | base64 --decode > ./ios/leatherwalletmobile/GoogleService-Info.plist
