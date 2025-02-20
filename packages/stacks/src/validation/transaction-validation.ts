db0602bf1dcc1756ad897ef3493ee2db6146f08f
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-02-20 10:16:15 +0000

    feat: add address validation to stacks pacakge, ref LEA-1647

---
+++ packages/stacks/src/validation/transaction-validation.ts
@@ -0,0 +1,27 @@
+import { ChainId } from '@leather.io/models';
+
+import {
+  isValidAddressChain,
+  isValidStacksAddress,
+  validatePayerNotRecipient,
+} from './address-validation';
+import { StacksError } from './stacks-error';
+
+export function isValidStacksTransaction(
+  senderAddress: string,
+  recipientAddress: string,
+  chainId: ChainId
+) {
+  if (!isValidStacksAddress(senderAddress) || !isValidStacksAddress(recipientAddress)) {
+    throw new StacksError('InvalidAddress');
+  }
+  if (
+    !isValidAddressChain(senderAddress, chainId) ||
+    !isValidAddressChain(recipientAddress, chainId)
+  ) {
+    throw new StacksError('InvalidNetworkAddress');
+  }
+  if (!validatePayerNotRecipient(senderAddress, recipientAddress)) {
+    throw new StacksError('InvalidSameAddress');
+  }
+}
