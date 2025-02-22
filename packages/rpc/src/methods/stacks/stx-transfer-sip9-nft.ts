cc431df7b2a7ffd3c59ee225ad1406ace5ce2e5d
Fara Woolf <fara@trustmachines.co>
2025-02-22 16:32:44 -0500

    fix: add rpc types for sip9 transfers

---
+++ packages/rpc/src/methods/stacks/stx-transfer-sip9-nft.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+import { defineRpcEndpoint } from '../../rpc/schemas';
+import {
+  baseStacksTransactionConfigSchema,
+  stacksTransactionDetailsSchema,
+} from './_stacks-helpers';
+
+export const stxTransferSip9Nft = defineRpcEndpoint({
+  method: 'stx_transferSip9Nft',
+  params: z.intersection(
+    z.object({
+      recipient: z.string(),
+      asset: z.string(),
+      assetId: z.string(),
+    }),
+    baseStacksTransactionConfigSchema
+  ),
+  result: stacksTransactionDetailsSchema,
+});
