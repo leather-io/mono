leather-io/extension

refactor: rpc send transfer

src/app/features/fee-editor/fee-editor.context.ts
@@ -1,0 +1,49 @@
+import { createContext, useContext } from 'react';
+
+import type { MarketData, Money } from '@leather.io/models';
+
+export type FeeType = 'slow' | 'standard' | 'fast' | 'custom';
+
+export interface RawFee {
+  type: FeeType;
+  baseUnitFeeValue: Money | null;
+  feeRate: number | null;
+  time: string;
+}
+
+export type RawFees = Record<Exclude<FeeType, 'custom'>, RawFee>;
+
+export interface FeeDisplayInfo {
+  feeType: FeeType;
+  baseUnitsValue: number;
+  feeRate: number;
+  titleLeft: string;
+  captionLeft: string;
+  titleRight?: string;
+  captionRight?: string;
+}
+
+export interface FeeEditorContext {
+  availableBalance: Money;
+  customFeeData: RawFee | null;
+  customFeeRate: string;
+  editFeeSelected: FeeType;
+  marketData: MarketData;
+  rawFees: RawFees;
+  selectedFeeData: FeeDisplayInfo;
+  selectedFeeType: FeeType;
+  getCustomFeeData(rate: number): RawFee;
+  onSetCustomFeeRate(value: string | null): void;
+  onSetEditFeeSelected(value: FeeType): void;
+  onSetSelectedFeeType(value: FeeType | null): void;
+}
+
+const feeEditorContext = createContext<FeeEditorContext | null>(null);
+
+export function useFeeEditorContext() {
+  const context = useContext(feeEditorContext);
+  if (!context) throw new Error('`useFeeEditorContext` must be used within a `FeeEditorProvider`');
+  return context;
+}
+
+export const FeeEditorProvider = feeEditorContext.Provider;

src/app/features/fee-editor/fee-editor.tsx
@@ -1,0 +1,100 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Center, styled } from 'leather-styles/jsx';
+
+import { Approver, Button } from '@leather.io/ui';
+
+import { Fees } from '@app/features/fee-editor/components/fees';
+import { formatFeeForDisplay } from '@app/features/fee-editor/fee-editor.utils';
+
+import { type RawFee, useFeeEditorContext } from './fee-editor.context';
+
+export function FeeEditor() {
+  const {
+    availableBalance,
+    customFeeRate,
+    editFeeSelected,
+    marketData,
+    rawFees,
+    selectedFeeData,
+    selectedFeeType,
+    getCustomFeeData,
+    onSetCustomFeeRate,
+    onSetEditFeeSelected,
+    onSetSelectedFeeType,
+  } = useFeeEditorContext();
+  const navigate = useNavigate();
+
+  const insufficientBalance = availableBalance.amount.isLessThan(
+    selectedFeeData?.baseUnitsValue ?? 0
+  );
+
+  function onCancel() {
+    navigate(-1);
+    onSetCustomFeeRate(selectedFeeData?.feeRate.toString() || '');
+    onSetEditFeeSelected(selectedFeeType);
+  }
+
+  function onSave() {
+    onSetSelectedFeeType(editFeeSelected);
+    if (editFeeSelected !== 'custom') {
+      onSetCustomFeeRate(selectedFeeData?.feeRate.toString() || '');
+    }
+    navigate(-1);
+  }
+
+  function getFeeItemProps(rawFee: RawFee) {
+    const { type } = rawFee;
+
+    const { feeType, titleLeft, captionLeft, titleRight, captionRight } = formatFeeForDisplay({
+      rawFee,
+      marketData,
+    });
+
+    return {
+      feeType,
+      isSelected: editFeeSelected === type,
+      isInsufficientBalance: insufficientBalance,
+      titleLeft,
+      captionLeft,
+      titleRight,
+      captionRight,
+      onSelect: () => onSetEditFeeSelected(type),
+    };
+  }
+
+  return (
+    <Approver height="100%" width="100%" requester={origin}>
+      <Approver.Section mt="0" py="space.05">
+        <Center>
+          <styled.span textStyle="heading.05">Edit fee</styled.span>
+        </Center>
+      </Approver.Section>
+
+      <Approver.Section>
+        <Fees>
+          <Fees.Item {...getFeeItemProps(rawFees.slow)} />
+          <Fees.Item {...getFeeItemProps(rawFees.standard)} />
+          <Fees.Item {...getFeeItemProps(rawFees.fast)} />
+
+          <Fees.CustomItem
+            {...getFeeItemProps(getCustomFeeData(Number(customFeeRate)))}
+            fee={customFeeRate}
+            setFee={onSetCustomFeeRate}
+          />
+        </Fees>
+      </Approver.Section>
+
+      <Approver.Actions
+        actions={[
+          <Button key="cancel" onClick={onCancel} fullWidth variant="outline">
+            Back
+          </Button>,
+          <Button key="save" onClick={onSave} fullWidth>
+            Save
+          </Button>,
+        ]}
+      />
+    </Approver>
+  );
+}

leather-io/mono

apps/extension/src/app/features/fee-editor/fee-editor.context.ts
@@ -1,0 +1,50 @@
+import { createContext, useContext } from 'react';
+
+import type { MarketData, Money } from '@leather.io/models';
+
+export type FeeType = 'slow' | 'standard' | 'fast' | 'custom';
+
+export interface RawFee {
+  type: FeeType;
+  baseUnitFeeValue: Money | null;
+  feeRate: number | null;
+  time: string;
+}
+
+export type RawFees = Record<Exclude<FeeType, 'custom'>, RawFee> | undefined;
+
+export interface FeeDisplayInfo {
+  feeType: FeeType;
+  baseUnitsValue: number;
+  feeRate: number;
+  titleLeft: string;
+  captionLeft: string;
+  titleRight?: string;
+  captionRight?: string;
+}
+
+export interface FeeEditorContext {
+  availableBalance: Money;
+  customFeeData: RawFee | null;
+  customFeeRate: string;
+  currentFeeType: FeeType;
+  isLoadingFees: boolean;
+  marketData: MarketData;
+  rawFees: RawFees;
+  selectedFeeData: FeeDisplayInfo | null;
+  selectedFeeType: FeeType;
+  getCustomFeeData(rate: number): RawFee;
+  onSetCustomFeeRate(value: string | null): void;
+  onSetSelectedFeeType(value: FeeType): void;
+  onSetCurrentFeeType(value: FeeType | null): void;
+}
+
+const feeEditorContext = createContext<FeeEditorContext | null>(null);
+
+export function useFeeEditorContext() {
+  const context = useContext(feeEditorContext);
+  if (!context) throw new Error('`useFeeEditorContext` must be used within a `FeeEditorProvider`');
+  return context;
+}
+
+export const FeeEditorProvider = feeEditorContext.Provider;

apps/extension/src/app/features/fee-editor/fee-editor.tsx
@@ -1,0 +1,99 @@
+import { useNavigate } from 'react-router-dom';
+
+import { Center, styled } from 'leather-styles/jsx';
+
+import { Approver, Button } from '@leather.io/ui';
+
+import { Fees } from '@app/features/fee-editor/components/fees';
+import { formatFeeForDisplay } from '@app/features/fee-editor/fee-editor.utils';
+
+import { type RawFee, useFeeEditorContext } from './fee-editor.context';
+
+export function FeeEditor() {
+  const {
+    availableBalance,
+    currentFeeType,
+    customFeeRate,
+    isLoadingFees,
+    marketData,
+    rawFees,
+    selectedFeeData,
+    selectedFeeType,
+    getCustomFeeData,
+    onSetCustomFeeRate,
+    onSetSelectedFeeType,
+    onSetCurrentFeeType,
+  } = useFeeEditorContext();
+  const navigate = useNavigate();
+
+  if (isLoadingFees || !rawFees) return null;
+
+  const insufficientBalance = availableBalance.amount.isLessThan(
+    selectedFeeData?.baseUnitsValue ?? 0
+  );
+
+  function onCancel() {
+    navigate(-1);
+    onSetCustomFeeRate(selectedFeeData?.feeRate.toString() || '');
+    onSetSelectedFeeType(currentFeeType);
+  }
+
+  function onSave() {
+    onSetCurrentFeeType(selectedFeeType);
+    if (selectedFeeType !== 'custom') {
+      onSetCustomFeeRate(selectedFeeData?.feeRate.toString() || '');
+    }
+    navigate(-1);
+  }
+
+  function getFeeItemProps(rawFee: RawFee) {
+    const { type } = rawFee;
+    const { feeType, titleLeft, captionLeft, titleRight, captionRight } = formatFeeForDisplay({
+      rawFee,
+      marketData,
+    });
+
+    return {
+      feeType,
+      isSelected: selectedFeeType === type,
+      isInsufficientBalance: insufficientBalance,
+      titleLeft,
+      captionLeft,
+      titleRight,
+      captionRight,
+      onSelect: () => onSetSelectedFeeType(type),
+    };
+  }
+
+  return (
+    <Approver height="100%" width="100%" requester={origin}>
+      <Approver.Section mt="0" py="space.05">
+        <Center>
+          <styled.span textStyle="heading.05">Edit fee</styled.span>
+        </Center>
+      </Approver.Section>
+      <Approver.Section>
+        <Fees>
+          <Fees.Item {...getFeeItemProps(rawFees.slow)} />
+          <Fees.Item {...getFeeItemProps(rawFees.standard)} />
+          <Fees.Item {...getFeeItemProps(rawFees.fast)} />
+          <Fees.CustomItem
+            {...getFeeItemProps(getCustomFeeData(Number(customFeeRate)))}
+            fee={customFeeRate}
+            setFee={onSetCustomFeeRate}
+          />
+        </Fees>
+      </Approver.Section>
+      <Approver.Actions
+        actions={[
+          <Button key="cancel" onClick={onCancel} fullWidth variant="outline">
+            Back
+          </Button>,
+          <Button key="save" onClick={onSave} fullWidth>
+            Save
+          </Button>,
+        ]}
+      />
+    </Approver>
+  );
+}