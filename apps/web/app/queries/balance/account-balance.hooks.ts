5cc645a042514d6d60433fa9181fd607b5e57d92
Anastasios <anastas3993@gmail.com>
2025-04-02 20:25:41 +0400

    fix: stx balance hooks

--- apps/web/app/queries/balance/account-balance.hooks.ts
+++ apps/web/app/queries/balance/account-balance.hooks.ts
@@ -1,13 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
+import { BigNumber } from 'bignumber.js';
 import { useStacksClient } from '~/queries/stacks/stacks-client';
 import { useStacksNetwork } from '~/store/stacks-network';
 
-import {
-  createGetStacksAccountBalanceQueryOptions,
-  createStxCryptoAssetBalance,
-  createStxMoney,
-} from '@leather.io/query';
-import { createMoney } from '@leather.io/utils';
+import { createGetStxAddressBalanceQueryOptions } from '@leather.io/query';
+import { createMoney, createStxCryptoAssetBalance } from '@leather.io/utils';
 
 import { useMempoolTxsBalance } from './use-mempool-txs-balance';
 
@@ -16,12 +13,11 @@ function useStxBalanceQuery(address: string) {
   const { networkPreference } = useStacksNetwork();
 
   return useQuery({
-    ...createGetStacksAccountBalanceQueryOptions({
+    ...createGetStxAddressBalanceQueryOptions({
       address,
       client,
       network: networkPreference.chain.stacks.url,
     }),
-    select: resp => createStxMoney(resp),
   });
 }
 
@@ -34,13 +30,13 @@ export function useStxCryptoAssetBalance(address: string) {
   const { inboundBalance, outboundBalance, query } = useMempoolTxsBalance([address]);
 
   const filteredBalanceQuery = useQuery({
-    ...createGetStacksAccountBalanceQueryOptions({
+    ...createGetStxAddressBalanceQueryOptions({
       address,
       client,
       network: networkPreference.chain.stacks.url,
     }),
     select: resp => {
-      const initialBalance = createStxMoney(resp);
+      const initialBalance = createMoney(new BigNumber(resp.balance), 'STX');
       return createStxCryptoAssetBalance(initialBalance, inboundBalance, outboundBalance);
     },
     enabled: !!initialBalanceQuery.data,
@@ -63,18 +59,4 @@ export function useStxAvailableUnlockedBalance(address: string) {
   const stxBalance = useStxCryptoAssetBalance(address);
 
   return stxBalance.filteredBalanceQuery.data?.unlockedBalance ?? createMoney(0, 'STX');
-}
-
-export function useStacksAccountBalanceFungibleTokens(address: string) {
-  const client = useStacksClient();
-  const { networkPreference } = useStacksNetwork();
-
-  return useQuery({
-    ...createGetStacksAccountBalanceQueryOptions({
-      address,
-      client,
-      network: networkPreference.chain.stacks.url,
-    }),
-    select: resp => resp.fungible_tokens,
-  });
 }
