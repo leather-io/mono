177a9c54f7a0e1f52a1ef026c6aab0edd90dd30f
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-04-10 11:19:15 +0100

    fix(mobile): add new key for Stacks layer 2, ref PRO-23

--- apps/mobile/src/features/balances/utils/get-chain-layer-from-protocol.ts
+++ apps/mobile/src/features/balances/utils/get-chain-layer-from-protocol.ts
@@ -5,8 +5,9 @@ import { CryptoAssetProtocol } from '@leather.io/models';
 export function getChainLayerFromAssetProtocol(protocol: CryptoAssetProtocol) {
   switch (protocol) {
     case 'nativeBtc':
-    case 'nativeStx':
       return t({ id: 'account_balance.caption_left.native', message: 'Layer 1' });
+    case 'nativeStx':
+      return t({ id: 'account_balance.caption_left.stacks', message: 'Layer 2' });
     case 'sip10':
       return t({ id: 'account_balance.caption_left.sip10', message: 'Layer 2 · Stacks' });
     case 'rune':
