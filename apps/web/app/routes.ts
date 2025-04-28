7d8918573389656b875f5540751663594e1d13a8
kyranjamie <kyran.burraston@gmail.com>
2025-04-28 16:31:32 +0200

    refactor(web): use .page.tsx naming

--- apps/web/app/routes.ts
+++ apps/web/app/routes.ts
@@ -1,15 +1,15 @@
 import { type RouteConfig, index, prefix, route } from '@react-router/dev/routes';
 
 export default [
-  index('routes/stacking/index.tsx'),
+  index('routes/stacking/stacking.page.tsx'),
   ...prefix('pooled-stacking/:slug', [
-    index('routes/stacking/pooled-stacking.tsx'),
-    route('active', 'routes/stacking/pooled-stacking-active.tsx'),
+    index('routes/stacking/pooled-stacking.page.tsx'),
+    route('active', 'routes/stacking/pooled-stacking-active.page.tsx'),
   ]),
   ...prefix('liquid-stacking/:slug', [
-    index('routes/stacking/liquid-stacking.tsx'),
-    route('active', 'routes/stacking/liquid-stacking-active.tsx'),
+    index('routes/stacking/liquid-stacking.page.tsx'),
+    route('active', 'routes/stacking/liquid-stacking-active.page.tsx'),
   ]),
-  route('sbtc', 'routes/sbtc.tsx'),
-  route('*', 'routes/error-not-found.tsx'),
+  route('sbtc', 'routes/sbtc.page.tsx'),
+  route('*', 'routes/error-not-found.page.tsx'),
 ] satisfies RouteConfig;
