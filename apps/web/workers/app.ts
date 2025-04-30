69c2ceb686356b3d6e025737c85a40345a7e883a
kyranjamie <kyran.burraston@gmail.com>
2025-04-30 14:29:17 +0200

    refactor(web): package upgrades

--- apps/web/workers/app.ts
+++ apps/web/workers/app.ts
@@ -1,5 +1,7 @@
 import { createRequestHandler } from 'react-router';
 
+import { csp } from './csp';
+
 declare module 'react-router' {
   export interface AppLoadContext {
     cloudflare: {
@@ -21,17 +23,11 @@ export default {
     });
 
     const headers = new Headers(resp.headers);
-    headers.set(
-      'Content-Security-Policy',
-      "default-src 'self'; script-src 'self'; object-src 'none'; base-uri 'self'; frame-ancestors 'none';"
-    );
+    headers.set('Content-Security-Policy', csp);
     headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
     headers.set('Referrer-Policy', 'no-referrer');
     headers.set('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
 
-    return new Response(resp.body, {
-      ...resp,
-      headers,
-    });
+    return new Response(resp.body, { ...resp, headers });
   },
 } satisfies ExportedHandler<Env>;
---
+++ apps/web/workers/csp.ts
@@ -0,0 +1,16 @@
+import builder from 'content-security-policy-builder';
+
+export const csp = builder({
+  directives: {
+    defaultSrc: [`'self'`],
+    // Ideally we remove unsafe-inline, however this too involved to add with
+    // RR7 for leather web launch
+    scriptSrc: [`'self'`, `'unsafe-inline'`],
+    styleSrc: [`'self'`, "'unsafe-inline'"],
+    objectSrc: [`'none'`],
+    baseUri: [`'self'`],
+    frameAncestors: [`'none'`],
+    workerSrc: [`'self'`, 'blob:'],
+    connectSrc: [`'self'`, '*.hiro.so', '*.ingest.us.sentry.io'],
+  },
+});
