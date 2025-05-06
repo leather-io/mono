78251181296a94c6d83e8d27e730e3f059547533
Anastasios <anastas3993@gmail.com>
2025-05-06 13:50:39 +0400

    feat(web): remove unnecessary icons

--- apps/web/app/pages/stacking/components/stacking-explainer.tsx
+++ apps/web/app/pages/stacking/components/stacking-explainer.tsx
@@ -1,5 +1,4 @@
 import { type HTMLStyledProps } from 'leather-styles/jsx';
-import { DummyIcon } from '~/components/dummy';
 import { Explainer } from '~/components/explainer';
 
 export function StackingExplainer(props: HTMLStyledProps<'section'>) {
@@ -9,25 +8,21 @@ export function StackingExplainer(props: HTMLStyledProps<'section'>) {
         index={0}
         title="Own STX"
         description="Get or hold STX in your wallet, ready to stack"
-        img={<DummyIcon />}
       />
       <Explainer.Step
         index={1}
         title="Choose a provider"
         description="Pick a protocol from the table below"
-        img={<DummyIcon />}
       />
       <Explainer.Step
         index={2}
         title="Lock STX"
         description="Delegate and lock your STX into the chosen pool"
-        img={<DummyIcon />}
       />
       <Explainer.Step
         index={3}
         title="Begin earning"
         description="Receive regular rewards without lifting a finger"
-        img={<DummyIcon />}
       />
     </Explainer>
   );
