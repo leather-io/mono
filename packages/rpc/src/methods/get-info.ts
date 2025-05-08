3302a7e59c1698b111895007bfee90eacec7dcac
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-05-08 17:04:14 +0400

    feat: add platform to getInfo response

--- packages/rpc/src/methods/get-info.ts
+++ packages/rpc/src/methods/get-info.ts
@@ -5,6 +5,7 @@ import { defineRpcEndpoint } from '../rpc/schemas';
 const getInfoResponseBodySchema = z.object({
   version: z.string(),
   supportedMethods: z.array(z.string()).optional(),
+  platform: z.union([z.literal('mobile'), z.literal('extension')]),
 });
 
 export const getInfo = defineRpcEndpoint({
