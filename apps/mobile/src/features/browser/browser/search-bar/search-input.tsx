1d8ab42b7dcf1b611acca4d6cf6a6f238b71c096
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-05-08 15:59:35 +0400

    feat: delete search text button

--- apps/mobile/src/features/browser/browser/search-bar/search-input.tsx
+++ apps/mobile/src/features/browser/browser/search-bar/search-input.tsx
@@ -6,7 +6,14 @@ import { SharedValue } from 'react-native-reanimated';
 import { t } from '@lingui/macro';
 import { useTheme } from '@shopify/restyle';
 
-import { TextInput, Theme } from '@leather.io/ui/native';
+import {
+  Box,
+  CloseIcon,
+  Pressable,
+  TextInput,
+  Theme,
+  legacyTouchablePressEffect,
+} from '@leather.io/ui/native';
 
 interface SearchInputProps {
   textInputRef: RefObject<RNTextInput>;
@@ -25,34 +32,51 @@ export function SearchInput({
 }: SearchInputProps) {
   const theme = useTheme<Theme>();
   return (
-    <TextInput
-      ref={textInputRef}
-      onFocus={() => {
-        isUrlFocused.value = true;
-      }}
-      onBlur={() => {
-        KeyboardController.dismiss();
-        isUrlFocused.value = false;
-      }}
-      keyboardType="web-search"
-      textContentType="URL"
-      autoCapitalize="none"
-      onChangeText={setTextUrl}
-      value={textUrl}
-      autoComplete="url"
-      onSubmitEditing={onSubmit}
-      width="100%"
-      py="5"
-      px="4"
-      borderRadius="sm"
-      borderColor="ink.border-default"
-      borderWidth={1}
-      placeholderTextColor={theme.colors['ink.text-subdued']}
-      color="ink.text-primary"
-      placeholder={t({
-        id: 'browser.input_placeholder',
-        message: 'Type URL or search',
-      })}
-    />
+    <Box>
+      <TextInput
+        ref={textInputRef}
+        onFocus={() => {
+          isUrlFocused.value = true;
+        }}
+        onBlur={() => {
+          KeyboardController.dismiss();
+          isUrlFocused.value = false;
+        }}
+        keyboardType="web-search"
+        textContentType="URL"
+        autoCapitalize="none"
+        onChangeText={setTextUrl}
+        value={textUrl}
+        autoComplete="url"
+        onSubmitEditing={onSubmit}
+        width="100%"
+        py="5"
+        px="4"
+        borderRadius="sm"
+        borderColor="ink.border-default"
+        borderWidth={1}
+        placeholderTextColor={theme.colors['ink.text-subdued']}
+        color="ink.text-primary"
+        placeholder={t({
+          id: 'browser.input_placeholder',
+          message: 'Type URL or search',
+        })}
+      />
+      <Pressable
+        onPress={() => setTextUrl('')}
+        position="absolute"
+        right={2}
+        top={2}
+        bottom={2}
+        py="4"
+        px="4"
+        justifyContent="center"
+        alignItems="center"
+        pressEffects={legacyTouchablePressEffect}
+        bg="ink.background-primary"
+      >
+        <CloseIcon width={16} height={16} />
+      </Pressable>
+    </Box>
   );
 }
