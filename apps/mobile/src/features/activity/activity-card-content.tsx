1eacb4751fbf62cec3bcba4ca7e409ae6136699e
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-05-09 14:52:25 +0100

    fix(mobile): activity ui updates, ref ENG-37

--- apps/mobile/src/features/activity/activity-card-content.tsx
+++ apps/mobile/src/features/activity/activity-card-content.tsx
@@ -3,7 +3,7 @@ import { Balance } from '@/components/balance/balance';
 import { Activity } from '@leather.io/models';
 import { Box, Text } from '@leather.io/ui/native';
 
-import { formatActivityType } from './utils/format-activity';
+import { formatActivityStatus } from './utils/format-activity';
 
 interface ActivityCardContentProps {
   activity: Activity;
@@ -12,7 +12,12 @@ export function ActivityCardContent({ activity }: ActivityCardContentProps) {
   return (
     <Box flexDirection="column" alignItems="flex-start" flexShrink={0} alignSelf="stretch">
       <Box alignItems="flex-start" gap="1" alignSelf="stretch" paddingTop="2">
-        <Text variant="label02">{formatActivityType(activity.type)}</Text>
+        <Text variant="label02">
+          {formatActivityStatus({
+            activityType: activity.type,
+            status: activity.status,
+          })}
+        </Text>
         {(activity.type === 'sendAsset' || activity.type === 'receiveAsset') && activity.value && (
           <Box alignItems="flex-start" gap="1" alignSelf="stretch">
             <Balance balance={activity.value.crypto} />
