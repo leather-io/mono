0db9b24cfc161c1d563c0a8afab698bb39b86378
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-05-16 17:07:42 +0100

    fix(mobile): sort tokens, add FlashList for performance, ref LEA-2310

---
+++ apps/mobile/src/features/balances/utils/sort-sip10-balances.ts
@@ -0,0 +1,21 @@
+import { Sip10AggregateBalance } from '@leather.io/services';
+
+export function sortSip10Balances(
+  a: Sip10AggregateBalance['sip10s'][number],
+  b: Sip10AggregateBalance['sip10s'][number]
+) {
+  // sBTC should always be first
+  if (a.asset.symbol === 'sBTC') return -1;
+  if (b.asset.symbol === 'sBTC') return 1;
+
+  // Sort by fiat total balance
+  const fiatDiff = Number(b.fiat.totalBalance.amount) - Number(a.fiat.totalBalance.amount);
+  if (fiatDiff !== 0) return fiatDiff;
+
+  // Sort by crypto total balance
+  const cryptoDiff = Number(b.crypto.totalBalance.amount) - Number(a.crypto.totalBalance.amount);
+  if (cryptoDiff !== 0) return cryptoDiff;
+
+  // Finally sort alphabetically by symbol
+  return a.asset.symbol.localeCompare(b.asset.symbol);
+}
