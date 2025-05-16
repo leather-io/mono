48487940d93c30565750c85fef18129ba0330eed
Will Cameron <camerow@users.noreply.github.com>
2025-05-16 12:28:46 -0700

    ci: revert file changes

--- apps/mobile/fingerprint.config.ts
+++
@@ -1,9 +0,0 @@
-// import { SourceSkips } from '@expo/fingerprint';
-
-export default {
-  // sourceSkips: [
-  // SourceSkips.ExpoConfigRuntimeVersionIfString,
-  // SourceSkips.ExpoConfigVersions,
-  // SourceSkips.PackageJsonAndroidAndIosScriptsIfNotContainRun,
-  // ],
-};
