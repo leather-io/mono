5aa152d027a1813def4f40f727eb9ec1cdff8c0d
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-05-19 06:44:03 +0100

    fix(mobile): make explorer links adapt to network, ref LEA-2582

--- apps/mobile/src/features/activity/activity-card.tsx
+++ apps/mobile/src/features/activity/activity-card.tsx
@@ -5,7 +5,7 @@ import { OnChainActivity } from '@leather.io/models';
 import { ActivityAvatarIcon, Box } from '@leather.io/ui/native';
 
 import { ActivityCardContent } from './activity-card-content';
-import { goToActivityExplorer, onPressActivity } from './utils/go-to-activity-explorer';
+import { onPressActivity } from './utils/go-to-activity-explorer';
 
 interface ActivityCardProps {
   activity: OnChainActivity;
@@ -18,12 +18,7 @@ export function ActivityCard({ activity }: ActivityCardProps) {
   const activityHasAsset = 'asset' in activity;
   const asset = activityHasAsset && 'symbol' in activity.asset ? activity.asset : undefined;
   return (
-    <Card
-      onPress={
-        txid && asset ? () => onPressActivity({ asset, txid, networkPreference }) : undefined
-      }
-      width={200}
-    >
+    <Card onPress={() => onPressActivity({ asset, txid, networkPreference })} width={200}>
       <Box flexDirection="row" justifyContent="space-between">
         <ActivityAvatarIcon type={type} asset={asset} status={status} />
       </Box>
