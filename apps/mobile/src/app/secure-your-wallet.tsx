c0888c12640e42d2d8e812a0db15071f166ee1b6
Tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-05-21 15:20:39 +0400

    fix(mobile): match "secure your wallet" page to design

--- apps/mobile/src/app/secure-your-wallet.tsx
+++ apps/mobile/src/app/secure-your-wallet.tsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Svg, { Path } from 'react-native-svg';
 
 import { AnimatedHeaderScreenLayout } from '@/components/headers/animated-header/animated-header-screen.layout';
 import { SkipSecureWalletSheet } from '@/features/wallet-manager/secure-your-wallet/skip-secure-wallet-sheet';
@@ -8,7 +9,7 @@ import { useCreateWallet } from '@/hooks/use-create-wallet';
 import { t } from '@lingui/macro';
 import { useTheme } from '@shopify/restyle';
 
-import { Box, Button, LockIcon, SheetRef, Text, Theme } from '@leather.io/ui/native';
+import { Box, Button, SheetRef, Text, Theme } from '@leather.io/ui/native';
 
 export default function SecureYourWalletScreen() {
   const { bottom } = useSafeAreaInsets();
@@ -43,8 +44,8 @@ export default function SecureYourWalletScreen() {
               })}
             </Text>
           </Box>
-          <Box justifyContent="center" alignItems="center">
-            <LockIcon height={204} width={204} />
+          <Box justifyContent="center" alignItems="center" aspectRatio={1}>
+            <CustomLockIcon />
           </Box>
         </AnimatedHeaderScreenLayout>
         <Box px="5">
@@ -79,5 +80,19 @@ export default function SecureYourWalletScreen() {
         sheetRef={sheetRef}
       />
     </>
+  );
+}
+
+function CustomLockIcon() {
+  return (
+    <Svg width="204" height="204" viewBox="0 0 204 204" fill="none">
+      <Path
+        d="M136 85.5V60C136 41.2223 120.778 26 102 26C83.2223 26 68 41.2223 68 60V85.5M102 119.5V145M51 179H153C157.694 179 161.5 175.194 161.5 170.5V94C161.5 89.3056 157.694 85.5 153 85.5H51C46.3056 85.5 42.5 89.3056 42.5 94V170.5C42.5 175.194 46.3056 179 51 179Z"
+        stroke="currentColor"
+        strokeWidth="7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
   );
 }
