4e2eb5c5e3690667587c202f932f615db0c0138b
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-05-24 05:20:37 +0100

    fix(mobile): add new collectible designs, ref ENG-33

--- apps/mobile/src/app/collectibles/index.tsx
+++ apps/mobile/src/app/collectibles/index.tsx
@@ -1,4 +1,5 @@
 import { AnimatedHeaderScreenLayout } from '@/components/headers/animated-header/animated-header-screen.layout';
+import { PageLayout } from '@/components/page/page.layout';
 import { Collectibles } from '@/features/collectibles';
 import { NetworkBadge } from '@/features/settings/network-badge';
 import { useTotalCollectibles } from '@/queries/collectibles/account-collectibles.query';
@@ -12,13 +13,16 @@ export default function CollectiblesScreen() {
   });
 
   return (
-    <AnimatedHeaderScreenLayout
-      contentContainerStyles={{ paddingHorizontal: 0 }}
-      rightHeaderElement={<NetworkBadge />}
-      title={pageTitle}
-      contentTitle={pageTitle}
-    >
-      <Collectibles collectibles={collectibles} />
-    </AnimatedHeaderScreenLayout>
+    <PageLayout>
+      <AnimatedHeaderScreenLayout
+        contentContainerStyles={{ paddingHorizontal: 0 }}
+        rightHeaderElement={<NetworkBadge />}
+        title={pageTitle}
+        contentTitle={pageTitle}
+        contentTitleStyles={{ paddingLeft: '5' }}
+      >
+        <Collectibles collectibles={collectibles} />
+      </AnimatedHeaderScreenLayout>
+    </PageLayout>
   );
 }
