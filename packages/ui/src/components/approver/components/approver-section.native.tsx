437ecb8d78ae81741167621e50a53a5bc8ca2b60
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-05-24 07:00:55 +0100

    fix(mobile): revert change approver BG and add border to some sections, ref LEA-2546
    This reverts commit ad8e040f79a93184b408ae754c23be59b46848e8.

--- packages/ui/src/components/approver/components/approver-section.native.tsx
+++ packages/ui/src/components/approver/components/approver-section.native.tsx
@@ -6,12 +6,9 @@ import { Theme } from '../../../theme-native';
 import { Box } from '../../box/box.native';
 import { useRegisterApproverChild } from '../approver-context.shared';
 
-interface ApproverSectionProps extends BoxProps<Theme> {
-  children: ReactNode;
-  noTopPadding?: boolean;
-  borderTop?: boolean;
-}
-export function ApproverSection(props: ApproverSectionProps) {
+export function ApproverSection(
+  props: BoxProps<Theme> & { children: ReactNode; noTopPadding?: boolean }
+) {
   useRegisterApproverChild('section');
   return (
     <Box
@@ -20,8 +17,6 @@ export function ApproverSection(props: ApproverSectionProps) {
       mt={props.noTopPadding ? '0' : '3'}
       gap="2"
       backgroundColor="ink.background-primary"
-      borderTopColor="ink.border-transparent"
-      borderTopWidth={props.borderTop ? 1 : 0}
       {...props}
     />
   );
