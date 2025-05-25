c788e622df02bdfd7395c2900ea3df185e748831
Tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-05-25 11:36:20 +0400

    fix(mobile): fix account initialization in send

--- apps/mobile/src/features/send/send-sheet.tsx
+++ apps/mobile/src/features/send/send-sheet.tsx
@@ -1,12 +1,15 @@
 import { FullHeightSheet } from '@/components/sheets/full-height-sheet/full-height-sheet';
 import { useGlobalSheets } from '@/core/global-sheet-provider';
 import { Send } from '@/features/send/send';
+import { useGlobalSearchParams, useSegments } from 'expo-router';
+import { isString } from 'remeda';
 
 import { useHaptics } from '@leather.io/ui/native';
 
 export function SendSheet() {
   const { sendSheetRef } = useGlobalSheets();
   const triggerHaptics = useHaptics();
+  const { accountId } = useInitialSendParams();
 
   function handleAnimatedPositionChange(fromIndex: number, toIndex: number) {
     if (fromIndex === 0 && toIndex === -1) {
@@ -16,7 +19,16 @@ export function SendSheet() {
 
   return (
     <FullHeightSheet sheetRef={sendSheetRef} onAnimate={handleAnimatedPositionChange}>
-      <Send />
+      <Send accountId={accountId} />
     </FullHeightSheet>
   );
+}
+
+function useInitialSendParams() {
+  const [rootSegment] = useSegments();
+  const params = useGlobalSearchParams();
+  const isAccountRoute = rootSegment === 'account';
+  const accountId = isAccountRoute && isString(params.accountId) ? params.accountId : undefined;
+
+  return { accountId };
 }
