dbfc29c1fdf54209b6ff642d287f8e8befa822ba
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-05-26 11:43:55 +0400

    fix: collectibles width

--- apps/mobile/src/features/collectibles/collectibles-list.tsx
+++ apps/mobile/src/features/collectibles/collectibles-list.tsx
@@ -1,18 +1,26 @@
 import { useCallback } from 'react';
+import { Dimensions } from 'react-native';
 
 import { FetchState } from '@/components/loading';
 import { serializeCollectibles } from '@/features/collectibles';
+import { useTheme } from '@shopify/restyle';
 
 import { NonFungibleCryptoAssetInfo } from '@leather.io/models';
-import { CollectibleCard } from '@leather.io/ui/native';
+import { CollectibleCard, Theme } from '@leather.io/ui/native';
 
+const { width } = Dimensions.get('window');
 interface CollectiblesListProps {
   collectibles: FetchState<NonFungibleCryptoAssetInfo[]>;
   mode: 'widget' | 'gallery';
 }
 export function CollectiblesList({ collectibles, mode }: CollectiblesListProps) {
   const displayLimit = mode === 'widget' ? 9 : undefined;
-  const thumbnailSize = mode === 'widget' ? 200 : 164;
+  const theme = useTheme<Theme>();
+  // TODO: not a good fix, it should be automatically adjusted without this mathematics on our side.
+  // Will open a ticket for this one in the future
+  const edgeToEdgeThumbnailWidth = (width - 2 * theme.spacing[5] - theme.spacing[4]) / 2;
+  const thumbnailSize = mode === 'widget' ? 200 : edgeToEdgeThumbnailWidth;
+
   const collectiblesList = useCallback(
     (collectibles: FetchState<NonFungibleCryptoAssetInfo[]>) => {
       if (collectibles.state !== 'success') return [];
