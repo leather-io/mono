a5dc41717f717ecfdfc03d6e349a38de5d7040fe
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-05-26 09:25:41 +0400

    fix: android dapp crash

--- apps/mobile/src/features/browser/browser/search-bar/use-search-bar-animated-styles.ts
+++ apps/mobile/src/features/browser/browser/search-bar/use-search-bar-animated-styles.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import { useReanimatedKeyboardAnimation } from 'react-native-keyboard-controller';
 import {
   Extrapolation,
@@ -13,16 +14,36 @@ export function useSearchBarAnimatedStyles() {
     bottom: isUrlFocused.value ? -keyboardHeight.value : 0,
   }));
   const browserNavigationBarStyle = useAnimatedStyle(() => {
+    const displayStyles =
+      Platform.OS === 'android'
+        ? {
+            // use transform instead of zIndex as zIndex doesn't work well with webviews on android
+            transform: progress.value >= 0.5 ? [{ translateY: 200 }] : [{ translateY: 0 }],
+            zIndex: 100,
+          }
+        : {
+            zIndex: interpolate(progress.value, [0, 1], [100, -5], Extrapolation.CLAMP),
+          };
     return {
       opacity: interpolate(progress.value, [0, 1], [1, 0], Extrapolation.CLAMP),
-      zIndex: interpolate(progress.value, [0, 1], [100, -5], Extrapolation.CLAMP),
+      ...displayStyles,
     };
   });
   const searchBarStyle = useAnimatedStyle(() => {
+    const displayStyles =
+      Platform.OS === 'android'
+        ? {
+            // use transform instead of zIndex as zIndex doesn't work well with webviews on android
+            transform: progress.value >= 0.5 ? [{ translateY: 0 }] : [{ translateY: 200 }],
+            zIndex: 100,
+          }
+        : {
+            zIndex: interpolate(progress.value, [0, 1], [-5, 100], Extrapolation.CLAMP),
+          };
     return {
       // no need to interpolate as progress.value is a continuous value between 0 and 1
       opacity: progress.value,
-      zIndex: interpolate(progress.value, [0, 1], [-5, 100], Extrapolation.CLAMP),
+      ...displayStyles,
     };
   });
   return { searchBarStyle, browserNavigationBarStyle, keyboardAvoidingStyle, isUrlFocused };
