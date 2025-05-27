ee6db12ab3a7335e786a18e1c4005b27d2b64e45
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-05-27 17:59:58 +0400

    fix: dapp feature flag

--- apps/mobile/src/features/browser/browser/browser-header.tsx
+++ apps/mobile/src/features/browser/browser/browser-header.tsx
@@ -1,5 +1,6 @@
-import { Box, ClockIcon, ConnectionIcon } from '@leather.io/ui/native';
+import { Box, ClockIcon, ConnectionIcon, SparkleIcon } from '@leather.io/ui/native';
 
+import { useDappSuggestions } from '../../feature-flags';
 import { Tab } from './tab';
 import { BrowserSheetTab } from './utils';
 
@@ -9,16 +10,19 @@ interface BrowserHeaderProps {
 }
 
 export function BrowserHeader({ currentTab, setCurrentTab }: BrowserHeaderProps) {
+  const dappSuggestions = useDappSuggestions();
   return (
     <Box flexDirection="row" height={74}>
-      {/* <Tab */}
-      {/*   isActive={currentTab === 'suggested'} */}
-      {/*   onPress={() => { */}
-      {/*     setCurrentTab('suggested'); */}
-      {/*   }} */}
-      {/* > */}
-      {/*   <SparkleIcon /> */}
-      {/* </Tab> */}
+      {dappSuggestions && (
+        <Tab
+          isActive={currentTab === 'suggested'}
+          onPress={() => {
+            setCurrentTab('suggested');
+          }}
+        >
+          <SparkleIcon />
+        </Tab>
+      )}
       <Tab
         isActive={currentTab === 'connected'}
         onPress={() => {
