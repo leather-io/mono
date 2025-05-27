0572e95972a121312022c01e4c869fd90f0d952a
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-05-27 17:20:46 +0400

    fix(mobile): update max amount in Send form on recipient change

--- apps/mobile/src/features/send/hooks/use-send-max.ts
+++ apps/mobile/src/features/send/hooks/use-send-max.ts
@@ -1,16 +1,24 @@
+import { useCallback, useEffect } from 'react';
+import { useWatch } from 'react-hook-form';
+
+import { usePrevious } from '@/hooks/use-previous';
 import BigNumber from 'bignumber.js';
 
 export function useSendMax(maxSpend: BigNumber, form: any) {
-  function setIsSendingMax() {
-    form.setValue('isSendingMax', true, {
+  const isSendingMax = useWatch({ control: form.control, name: 'isSendingMax' });
+  const recipient = useWatch({ control: form.control, name: 'recipient' });
+  const previousRecipient = usePrevious(recipient);
+
+  const setAmountToMax = useCallback(() => {
+    form.setValue('amount', maxSpend.toString(), {
       shouldTouch: true,
       shouldDirty: true,
       shouldValidate: true,
     });
-  }
+  }, [form, maxSpend]);
 
-  function setAmountToMax() {
-    form.setValue('amount', maxSpend.toString(), {
+  function setIsSendingMax() {
+    form.setValue('isSendingMax', true, {
       shouldTouch: true,
       shouldDirty: true,
       shouldValidate: true,
@@ -25,6 +33,14 @@ export function useSendMax(maxSpend: BigNumber, form: any) {
       form.setValue('isSendingMax', false);
     }
   }
+
+  useEffect(() => {
+    if (isSendingMax) {
+      if (recipient !== previousRecipient) {
+        setAmountToMax();
+      }
+    }
+  }, [recipient, isSendingMax, previousRecipient, setAmountToMax]);
 
   return {
     onSetMax: handleSetMax,
