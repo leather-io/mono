7333d636d05276509f65482a790aac0cad03a0f0
fbwoolf <fara@trustmachines.co>
2025-05-29 13:58:11 -0400

    fix: rpc network type errors

--- packages/rpc/src/methods/stacks/stx-transfer-stx.ts
+++ packages/rpc/src/methods/stacks/stx-transfer-stx.ts
@@ -1,16 +1,22 @@
 import { z } from 'zod';
 
 import { defineRpcEndpoint } from '../../rpc/schemas';
-import { stacksTransactionDetailsSchema } from './_stacks-helpers';
+import {
+  baseStacksTransactionConfigSchema,
+  stacksTransactionDetailsSchema,
+} from './_stacks-helpers';
 
 export const stxTransferStx = defineRpcEndpoint({
   method: 'stx_transferStx',
-  params: z
-    .object({
-      recipient: z.string(),
-      amount: z.coerce.number().int('Amount must be an integer describing µSTX'),
-      memo: z.string().optional(),
-    })
-    .passthrough(),
+  params: z.intersection(
+    z
+      .object({
+        recipient: z.string(),
+        amount: z.coerce.number().int('Amount must be an integer describing µSTX'),
+        memo: z.string().optional(),
+      })
+      .passthrough(),
+    baseStacksTransactionConfigSchema
+  ),
   result: stacksTransactionDetailsSchema,
 });
