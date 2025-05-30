bb4872d0fa2e5eb66eb834107598d6c81f444d6d
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-05-30 13:21:07 +0400

    fix(mobile): remove text measurement logic from amount field

--- apps/mobile/src/components/amount-field/amount-field-primary-value.tsx
+++ apps/mobile/src/components/amount-field/amount-field-primary-value.tsx
@@ -4,16 +4,10 @@ import { NativeSyntheticEvent, TextLayoutEventData } from 'react-native';
 import { InputCurrencyMode } from '@/utils/types';
 
 import { CryptoCurrency, QuoteCurrency } from '@leather.io/models';
-import { Box, Text, TextProps, Theme } from '@leather.io/ui/native';
+import { Box, Text, Theme } from '@leather.io/ui/native';
 
-const maxFontSize = 44;
-const lineHeightRatio = 1;
-
-const commonTextProps: TextProps = {
-  variant: 'heading02',
-  fontVariant: ['tabular-nums'],
-  letterSpacing: 1,
-};
+const initialLineHeight = 44;
+const topOffsetRatio = 0.27;
 
 interface AmountFieldPrimaryValueProps {
   color: keyof Theme['colors'];
@@ -25,68 +19,31 @@ interface AmountFieldPrimaryValueProps {
 }
 
 export function AmountFieldPrimaryValue({ value, color }: AmountFieldPrimaryValueProps) {
-  const [dynamicFontSize, setDynamicFontSize] = useState(maxFontSize);
-  // Maintain the relative lineHeight to prevent text shifting down as font size decreases
-  const staticLineHeight = maxFontSize * lineHeightRatio;
-  const dynamicLineHeight = dynamicFontSize * lineHeightRatio;
+  const [lineHeight, setLineHeight] = useState(initialLineHeight);
 
-  return (
-    <Box height={staticLineHeight} flexShrink={1}>
-      <Box flexDirection="row" position="absolute" top={3}>
-        {value.split('').map((character, index) => (
-          <Text
-            color={color}
-            key={index}
-            style={{ fontSize: dynamicFontSize, lineHeight: dynamicLineHeight }}
-            {...commonTextProps}
-          >
-            {character}
-          </Text>
-        ))}
-      </Box>
-      <TextMeasurementProxy
-        value={value}
-        onFontSizeChange={setDynamicFontSize}
-        textProps={commonTextProps}
-      />
-    </Box>
-  );
-}
-
-interface TextMeasurementProxyProps {
-  value: string;
-  textProps: TextProps;
-  onFontSizeChange(fontSize: number): void;
-}
-
-// The Text component's `adjustFontSizeTofit` is not animatable. It also causes text to shift
-// due to dynamic positioning issue: https://github.com/facebook/react-native/issues/29507
-// Use an invisible placeholder to extract the dynamic font size set by `adjustFontSizeToFit`
-// as the text changes during typing.
-function TextMeasurementProxy({ value, textProps, onFontSizeChange }: TextMeasurementProxyProps) {
-  function handleLayout(event: NativeSyntheticEvent<TextLayoutEventData>) {
+  function handleTextLayout(event: NativeSyntheticEvent<TextLayoutEventData>) {
     const line = event.nativeEvent.lines[0];
-    if (line) {
-      onFontSizeChange(line.ascender);
-    }
+    if (line) setLineHeight(line.ascender);
   }
 
   return (
-    <Text
-      variant="heading02"
-      fontVariant={['tabular-nums']}
-      letterSpacing={1}
-      onTextLayout={handleLayout}
-      adjustsFontSizeToFit
-      numberOfLines={1}
-      aria-hidden
-      style={{
-        flexGrow: 1,
-        opacity: 0,
-      }}
-      {...textProps}
-    >
-      {value}
-    </Text>
+    <Box flexGrow={1} flexShrink={1} overflow="hidden" height={initialLineHeight}>
+      <Text
+        color={color}
+        allowFontScaling={false}
+        variant="heading02"
+        numberOfLines={1}
+        adjustsFontSizeToFit
+        onTextLayout={handleTextLayout}
+        style={{
+          marginTop: -Math.round(lineHeight * topOffsetRatio),
+          paddingTop: 0,
+          // Unset fixed line height to prevent onTextLayout from misreporting measurements.
+          lineHeight: undefined,
+        }}
+      >
+        {value}
+      </Text>
+    </Box>
   );
 }
