4ca8ef4cf0cc15df6b450c1c1e0c7a9fb07d93ee
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-06-04 13:02:55 +0400

    fix: browser input

--- apps/mobile/src/features/browser/browser/search-bar/use-search-bar-animated-styles.ts
+++ apps/mobile/src/features/browser/browser/search-bar/use-search-bar-animated-styles.ts
@@ -1,4 +1,3 @@
-import { Platform } from 'react-native';
 import { useReanimatedKeyboardAnimation } from 'react-native-keyboard-controller';
 import {
   Extrapolation,
@@ -13,37 +12,20 @@ export function useSearchBarAnimatedStyles() {
   const keyboardAvoidingStyle = useAnimatedStyle(() => ({
     bottom: isUrlFocused.value ? -keyboardHeight.value : 0,
   }));
+
   const browserNavigationBarStyle = useAnimatedStyle(() => {
-    const displayStyles =
-      Platform.OS === 'android'
-        ? {
-            // use transform instead of zIndex as zIndex doesn't work well with webviews on android
-            transform: progress.value >= 0.5 ? [{ translateY: 200 }] : [{ translateY: 0 }],
-            zIndex: 100,
-          }
-        : {
-            zIndex: interpolate(progress.value, [0, 1], [100, -5], Extrapolation.CLAMP),
-          };
     return {
       opacity: interpolate(progress.value, [0, 1], [1, 0], Extrapolation.CLAMP),
-      ...displayStyles,
+      transform: progress.value >= 0.5 ? [{ translateY: 200 }] : [{ translateY: 0 }],
+      zIndex: 100,
     };
   });
   const searchBarStyle = useAnimatedStyle(() => {
-    const displayStyles =
-      Platform.OS === 'android'
-        ? {
-            // use transform instead of zIndex as zIndex doesn't work well with webviews on android
-            transform: progress.value >= 0.5 ? [{ translateY: 0 }] : [{ translateY: 200 }],
-            zIndex: 100,
-          }
-        : {
-            zIndex: interpolate(progress.value, [0, 1], [-5, 100], Extrapolation.CLAMP),
-          };
     return {
       // no need to interpolate as progress.value is a continuous value between 0 and 1
       opacity: progress.value,
-      ...displayStyles,
+      transform: progress.value >= 0.5 ? [{ translateY: 0 }] : [{ translateY: 200 }],
+      zIndex: 100,
     };
   });
   return { searchBarStyle, browserNavigationBarStyle, keyboardAvoidingStyle, isUrlFocused };
