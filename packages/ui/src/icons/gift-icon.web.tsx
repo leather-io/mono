11c7394b727548574d978b33d8336865605f7bc2
fbwoolf <fara@trustmachines.co>
2025-06-05 20:09:47 -0400

    fix: gift icon web

---
+++ packages/ui/src/icons/gift-icon.web.tsx
@@ -0,0 +1,9 @@
+import Gift24 from '../assets/icons/account-avatars/gift-24-24.svg';
+import { createWebIcon } from './icon/create-icon.web';
+
+export const GiftIcon = createWebIcon({
+  icon: {
+    medium: Gift24,
+  },
+  displayName: 'Gift',
+});
