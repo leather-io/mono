72814577cb037baef75fc93eacf17401c1ba4553
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-06-13 09:06:29 +0400

    feat(ui): add bitcoin and stacks filled circle icons

---
+++ packages/ui/src/icons/bitcoin-filled-circle-icon.web.tsx
@@ -0,0 +1,9 @@
+import BitcoinFilledCircle from '../assets/icons/bitcoin.svg';
+import { createWebIcon } from './icon/create-icon.web';
+
+export const BitcoinFilledCircleIcon = createWebIcon({
+  icon: {
+    medium: BitcoinFilledCircle,
+  },
+  displayName: 'BitcoinCircle',
+});
