3512fc9465d36c93ae29f3bbb8da77f8d2faf990
Alex Perry <alexp3y@gmail.com>
2025-06-23 21:50:51 +0200

    feat(web): complete zealy task on wallet connect

--- apps/web/app/store/addresses.ts
+++ apps/web/app/store/addresses.ts
@@ -1,12 +1,17 @@
 import { useMemo } from 'react';
 
+import { StacksNetwork } from '@stacks/network';
 import { atom, useAtom, useAtomValue } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
+import { v4 as uuidv4 } from 'uuid';
 import { analytics } from '~/features/analytics/analytics';
 import { leather } from '~/helpers/leather-sdk';
 import { type ExtensionState, isLeatherInstalled, whenExtensionState } from '~/helpers/utils';
 
+import { ChainId } from '@leather.io/models';
 import { delay } from '@leather.io/utils';
+
+import { useStacksNetwork } from './stacks-network';
 
 type GetAddressesResult = Awaited<ReturnType<typeof leather.getAddresses>>['addresses'];
 
@@ -38,6 +43,7 @@ async function waitForExtensionConnectAnimationToFinish() {
 
 export function useLeatherConnect() {
   const [addresses, setAddresses] = useAtom(addressesAtom);
+  const stacksNetwork = useStacksNetwork();
   const extensionState = useAtomValue(extensionStateAtom);
   const [showMissingStacksKeysDialog, setShowMissingStacksKeysDialog] = useAtom(
     showMissingStacksKeysDialogAtom
@@ -97,6 +103,10 @@ export function useLeatherConnect() {
           duration: performance.now() - startTime,
         });
         setAddresses(result.addresses);
+        completeZealyConnectTask(
+          stacksNetwork.network,
+          result.addresses.find(address => address.symbol === 'STX')?.address
+        );
       } catch {
         void analytics.untypedTrack('sign_in_clicked', {
           status: 'error',
@@ -110,4 +120,21 @@ export function useLeatherConnect() {
       setAddresses([]);
     },
   };
+}
+
+function completeZealyConnectTask(network: StacksNetwork, address?: string) {
+  if (network.chainId === ChainId.Mainnet && address) {
+    fetch('https://api.leather.io/v1/quests/connect-earn/complete', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-client-id': uuidv4(),
+      },
+      body: JSON.stringify({
+        address,
+      }),
+    })
+      // eslint-disable-next-line no-console
+      .catch(() => console.error('Unable to complete quest task'));
+  }
 }
