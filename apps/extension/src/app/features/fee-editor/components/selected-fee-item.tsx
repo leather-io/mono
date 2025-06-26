leather-io/extension

fix: rpc flow sbtc sponsorship, closes ENG-97

src/app/features/fee-editor/components/selected-fee-item.tsx
@@ -1,5 +1,7 @@
+import { Flex, HStack, styled } from 'leather-styles/jsx';
+
 import type { MarketData } from '@leather.io/models';
-import { Approver, Pressable } from '@leather.io/ui';
+import { Approver, Avatar, Badge, Flag, PlaceholderIcon, Pressable } from '@leather.io/ui';
 
 import { CryptoAssetItemPlaceholder } from '@app/components/crypto-asset-item/crypto-asset-item-placeholder';
 import type { Fee, FeeType } from '@app/features/fee-editor/fee-editor.context';
@@ -10,6 +12,7 @@ import { FeeValueItemLayout } from './fee-value-item.layout';
 interface SelectedFeeItemProps {
   feeType: FeeType;
   isLoading: boolean;
+  isSponsored: boolean;
   marketData: MarketData;
   onEditFee(): void;
   selectedFee: Fee | null;
@@ -17,15 +20,29 @@ interface SelectedFeeItemProps {
 export function SelectedFeeItem({
   feeType,
   isLoading,
+  isSponsored,
   marketData,
   onEditFee,
   selectedFee,
 }: SelectedFeeItemProps) {
+  if (isSponsored)
+    return (
+      <Approver.Section>
+        <Flag img={<Avatar icon={<PlaceholderIcon />} />} my="space.02">
+          <HStack alignItems="center" justifyContent="space-between">
+            <styled.span textStyle="label.02">Sponsored fee</styled.span>
+            <Flex alignItems="center" gap="space.03">
+              <Badge label="FREE" variant="success" />
+            </Flex>
+          </HStack>
+        </Flag>
+      </Approver.Section>
+    );
+
   if (isLoading || !selectedFee)
     return (
       <Approver.Section>
-        <Approver.Subheader>Fee</Approver.Subheader>
-        <CryptoAssetItemPlaceholder my="0" />
+        <CryptoAssetItemPlaceholder />
       </Approver.Section>
     );
 

leather-io/mono

apps/extension/src/app/features/fee-editor/components/selected-fee-item.tsx
@@ -1,5 +1,7 @@
+import { Flex, HStack, styled } from 'leather-styles/jsx';
+
 import type { MarketData } from '@leather.io/models';
-import { Approver, Pressable } from '@leather.io/ui';
+import { Approver, Avatar, Badge, Flag, GiftIcon, Pressable } from '@leather.io/ui';
 
 import { CryptoAssetItemPlaceholder } from '@app/components/crypto-asset-item/crypto-asset-item-placeholder';
 import type { Fee, FeeType } from '@app/features/fee-editor/fee-editor.context';
@@ -10,6 +12,7 @@ import { FeeValueItemLayout } from './fee-value-item.layout';
 interface SelectedFeeItemProps {
   feeType: FeeType;
   isLoading: boolean;
+  isSponsored: boolean;
   marketData: MarketData;
   onEditFee(): void;
   selectedFee: Fee | null;
@@ -17,15 +20,29 @@ interface SelectedFeeItemProps {
 export function SelectedFeeItem({
   feeType,
   isLoading,
+  isSponsored,
   marketData,
   onEditFee,
   selectedFee,
 }: SelectedFeeItemProps) {
+  if (isSponsored)
+    return (
+      <Approver.Section>
+        <Flag img={<Avatar icon={<GiftIcon />} />} my="space.02">
+          <HStack alignItems="center" justifyContent="space-between">
+            <styled.span textStyle="label.02">Sponsored fee</styled.span>
+            <Flex alignItems="center" gap="space.03">
+              <Badge label="FREE" variant="success" />
+            </Flex>
+          </HStack>
+        </Flag>
+      </Approver.Section>
+    );
+
   if (isLoading || !selectedFee)
     return (
       <Approver.Section>
-        <Approver.Subheader>Fee</Approver.Subheader>
-        <CryptoAssetItemPlaceholder my="0" />
+        <CryptoAssetItemPlaceholder />
       </Approver.Section>
     );
 