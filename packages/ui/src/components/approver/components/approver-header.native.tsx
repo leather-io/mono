a7d5ae299768b14e7b42b2ba650253920a715ce1
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-07-04 17:44:50 +0400

    feat(mobile): add requester address and icon to approver header

--- packages/ui/src/components/approver/components/approver-header.native.tsx
+++ packages/ui/src/components/approver/components/approver-header.native.tsx
@@ -1,13 +1,27 @@
 import { Box } from '../../box/box.native';
+import { Favicon } from '../../favicon/favicon.native';
 import { Text } from '../../text/text.native';
-import { useRegisterApproverChild } from '../approver-context.shared';
+import { useApproverContext, useRegisterApproverChild } from '../approver-context.shared';
 
-export function ApproverHeader({ title }: { title: string }) {
+interface ApproverHeaderProps {
+  title: string;
+}
+
+export function ApproverHeader({ title }: ApproverHeaderProps) {
   useRegisterApproverChild('header');
+  const { requester, hostname } = useApproverContext();
 
   return (
     <Box px="5" pt="6" pb="5" backgroundColor="ink.background-primary" style={{ marginBottom: -1 }}>
-      <Text variant="heading03">{title}</Text>
+      <Box gap="2">
+        <Text variant="heading03">{title}</Text>
+        {requester && (
+          <Box flexDirection="row" gap="1">
+            <Favicon origin={requester} />
+            <Text variant="label03">Requested by {hostname}</Text>
+          </Box>
+        )}
+      </Box>
     </Box>
   );
 }
