317b4dceb91418e04516cc0c6fdab9c1d7edcd4d
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-07-10 10:08:54 +0100

    feat(mobile): add indicator icon

---
+++ packages/ui/src/icons/arrow-triangle-top-icon.native.tsx
@@ -0,0 +1,10 @@
+import ArrowTriangleTop from '../assets/icons/arrow-triangle-top.svg';
+import { createNativeIcon } from './icon/create-icon.native';
+
+export const ArrowTriangleTopIcon = createNativeIcon({
+  icon: {
+    small: ArrowTriangleTop,
+    medium: ArrowTriangleTop,
+  },
+  displayName: 'ArrowTriangleTop',
+});
