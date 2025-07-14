6a2e4b357d7f4e1f0cc82b922850752e19b0ecc5
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-07-14 13:23:36 +0400

    refactor: delete unused routes

--- apps/mobile/src/features/wallet-manager/add-wallet/add-wallet-sheet.tsx
+++ apps/mobile/src/features/wallet-manager/add-wallet/add-wallet-sheet.tsx
@@ -1,23 +1,15 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback } from 'react';
 
-import {
-  NotifyUserSheetData,
-  NotifyUserSheetLayout,
-} from '@/components/sheets/notify-user-sheet.layout';
 import { useGlobalSheets } from '@/core/global-sheet-provider';
 import { useSettings } from '@/store/settings/settings';
 import { useRouter } from 'expo-router';
 
-import { SheetRef } from '@leather.io/ui/native';
-
 import { AddWalletSheetLayout } from './add-wallet-sheet.layout';
 
 export function AddWalletSheet() {
-  const sheetRef = useRef<SheetRef>(null);
   const { themeDerivedFromThemePreference } = useSettings();
   const router = useRouter();
   const { addWalletSheetRef } = useGlobalSheets();
-  const [sheetData, setSheetData] = useState<NotifyUserSheetData | null>(null);
   const createWallet = useCallback(() => {
     router.navigate('/create-new-wallet');
     addWalletSheetRef.current?.close();
@@ -28,30 +20,15 @@ export function AddWalletSheet() {
     addWalletSheetRef.current?.close();
   }, [addWalletSheetRef, router]);
 
-  function onOpenSheet(option: NotifyUserSheetData) {
-    setSheetData(option);
-    sheetRef.current?.present();
-  }
-
-  function onCloseSheet() {
-    setSheetData(null);
-  }
-
   return (
     <>
       <AddWalletSheetLayout
-        onOpenSheet={onOpenSheet}
         createWallet={createWallet}
         restoreWallet={restoreWallet}
         addWalletSheetRef={addWalletSheetRef}
         themeVariant={themeDerivedFromThemePreference}
         // TODO: this should be set when we call ref.current.present
         opensFully={false}
-      />
-      <NotifyUserSheetLayout
-        onCloseSheet={onCloseSheet}
-        sheetData={sheetData}
-        sheetRef={sheetRef}
       />
     </>
   );
