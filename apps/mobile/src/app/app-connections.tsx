ba708670a26675fc31a93049bfa965714f810181
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-07-17 19:26:29 +0400

    fix: update design for connections tab

--- apps/mobile/src/app/app-connections.tsx
+++ apps/mobile/src/app/app-connections.tsx
@@ -1,16 +1,50 @@
-import { AppListing } from '@/features/browser/browser/app-listing';
+import { Screen } from '@/components/screen/screen';
+import { HeaderTitle } from '@/components/screen/screen-header/components/header-title';
+import { LinkCard } from '@/features/browser/browser/link-card';
+import { useApps } from '@/store/apps/apps.read';
 import { t } from '@lingui/macro';
+import { Image } from 'expo-image';
+
+import { Box, Text } from '@leather.io/ui/native';
 
 export default function ConnectionsScreen() {
-  return (
-    <AppListing
-      appStatus="connected"
-      title={t({ id: 'browser.connections.title', message: 'Connections' })}
-      emptyStateTitle={t({
-        id: 'browser-sheet.connected.empty.caption',
-        message: 'You will find the apps you are connected to here',
-      })}
-      imageSrc={require('@/assets/stickers/ufo.png')}
-    />
+  const { list: appList } = useApps('connected');
+  const title = t({ id: 'browser.connections.title', message: 'Connections' });
+
+  const emptyState = (
+    <>
+      <Box position="absolute" style={{ width: '100%' }}>
+        <Screen.Header centerElement={<HeaderTitle title={title} />} />
+      </Box>
+      <Screen.Body width={270} alignSelf="center" justifyContent="center" alignItems="center">
+        <Image
+          style={{ height: 270, width: 270 }}
+          contentFit="cover"
+          source={require('@/assets/stickers/ufo.png')}
+        />
+        <Text textAlign="center" variant="label01">
+          {t({
+            id: 'browser-sheet.connected.empty.caption',
+            message: 'You will find the apps you are connected to here',
+          })}
+        </Text>
+      </Screen.Body>
+    </>
   );
+  const withApps = (
+    <>
+      <Screen.Header centerElement={<HeaderTitle title={title} />} />
+      <Screen.ScrollView>
+        <Box flexDirection="row" flexWrap="wrap" gap="5" px="5">
+          {appList.reverse().map(app => (
+            <LinkCard app={app} key={app.origin} />
+          ))}
+        </Box>
+      </Screen.ScrollView>
+    </>
+  );
+
+  const innerContent = appList.length === 0 ? emptyState : withApps;
+
+  return <Screen backgroundColor="ink.background-primary">{innerContent}</Screen>;
 }
--- apps/mobile/src/app/app-recently-viewed.tsx
+++ apps/mobile/src/app/app-recently-viewed.tsx
@@ -1,16 +1,59 @@
-import { AppListing } from '@/features/browser/browser/app-listing';
+import { Screen } from '@/components/screen/screen';
+import { HeaderTitle } from '@/components/screen/screen-header/components/header-title';
+import { ScreenshotCard } from '@/features/browser/browser/screenshot-card';
+import { useOpenURL } from '@/features/browser/browser/use-open-url';
+import { useApps } from '@/store/apps/apps.read';
 import { t } from '@lingui/macro';
+import { Image } from 'expo-image';
+
+import { Box, Text } from '@leather.io/ui/native';
 
 export default function RecentlyViewedScreen() {
-  return (
-    <AppListing
-      appStatus="recently_visited"
-      title={t({ id: 'browser.recently-viewed.title', message: 'Recently viewed' })}
-      emptyStateTitle={t({
-        id: 'browser-sheet.recent.empty.caption',
-        message: 'You will find the apps you recently viewed here',
-      })}
-      imageSrc={require('@/assets/stickers/flower.png')}
-    />
+  const { list: appList } = useApps('recently_visited');
+  const { openURL } = useOpenURL();
+  const title = t({ id: 'browser.recently-viewed.title', message: 'Recently viewed' });
+
+  const emptyState = (
+    <>
+      <Box position="absolute" style={{ width: '100%' }}>
+        <Screen.Header centerElement={<HeaderTitle title={title} />} />
+      </Box>
+      <Screen.Body width={270} alignSelf="center" justifyContent="center" alignItems="center">
+        <Image
+          style={{ height: 270, width: 270 }}
+          contentFit="cover"
+          source={require('@/assets/stickers/flower.png')}
+        />
+
+        <Text textAlign="center" variant="label01">
+          {t({
+            id: 'browser-sheet.recent.empty.caption',
+            message: 'You will find the apps you recently viewed here',
+          })}
+        </Text>
+      </Screen.Body>
+    </>
   );
+  const withApps = (
+    <>
+      <Screen.Header centerElement={<HeaderTitle title={title} />} />
+      <Screen.ScrollView>
+        <Box flexDirection="row" flexWrap="wrap" gap="5" px="5">
+          {appList.reverse().map(app => (
+            <ScreenshotCard
+              key={app.origin}
+              app={app}
+              onPress={() => {
+                openURL(app.origin);
+              }}
+            />
+          ))}
+        </Box>
+      </Screen.ScrollView>
+    </>
+  );
+
+  const innerContent = appList.length === 0 ? emptyState : withApps;
+
+  return <Screen backgroundColor="ink.background-primary">{innerContent}</Screen>;
 }
