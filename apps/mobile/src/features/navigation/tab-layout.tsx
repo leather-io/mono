5c2ec973a2aa2a221b47f96f07319d09f8c4ca2a
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-07-18 12:03:30 +0400

    fix: tab bar navigation height

--- apps/mobile/src/features/navigation/tab-layout.tsx
+++ apps/mobile/src/features/navigation/tab-layout.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { t } from '@lingui/macro';
@@ -8,51 +9,58 @@ import { TabList, TabSlot, TabTrigger, Tabs } from 'expo-router/ui';
 import { BrowserIcon, HomeIcon, PulseIcon, Theme } from '@leather.io/ui/native';
 
 import { TabButton } from './tab-button';
+import { TabLayoutContext } from './tab-layout-context';
 
 export function TabLayout() {
   const { bottom } = useSafeAreaInsets();
   const { colors } = useTheme<Theme>();
+  const [tabBarHeight, setTabBarHeight] = useState(0);
   return (
-    <Tabs>
-      <TabSlot />
-      <LinearGradient
-        colors={[
-          colors['ink.background-primary'],
-          colors['ink.border-default'],
-          colors['ink.border-default'],
-          colors['ink.background-primary'],
-        ]}
-        locations={[0, 0.4, 0.6, 1]}
-        style={{ width: '100%', height: 1 }}
-        start={{ x: 0, y: 0.5 }}
-        end={{ x: 1, y: 0.5 }}
-      />
-      <TabList
-        style={{
-          paddingHorizontal: 64,
-          paddingBottom: bottom,
-          backgroundColor: colors['ink.background-primary'],
-        }}
-      >
-        <TabTrigger style={{ flex: 1 }} name="(index)" href="/(tabs)/(index)">
-          <TabButton
-            icon={<HomeIcon />}
-            title={t({ id: 'tabs.button.home.title', message: 'Home' })}
-          />
-        </TabTrigger>
-        <TabTrigger style={{ flex: 1 }} name="activity" href="/(tabs)/activity">
-          <TabButton
-            icon={<PulseIcon />}
-            title={t({ id: 'tabs.button.activity.title', message: 'Activity' })}
-          />
-        </TabTrigger>
-        <TabTrigger style={{ flex: 1 }} name="browser" href="/(tabs)/browser">
-          <TabButton
-            icon={<BrowserIcon />}
-            title={t({ id: 'tabs.button.browser.title', message: 'Browser' })}
-          />
-        </TabTrigger>
-      </TabList>
-    </Tabs>
+    <TabLayoutContext.Provider value={{ tabBarHeight }}>
+      <Tabs>
+        <TabSlot />
+        <LinearGradient
+          colors={[
+            colors['ink.background-primary'],
+            colors['ink.border-default'],
+            colors['ink.border-default'],
+            colors['ink.background-primary'],
+          ]}
+          locations={[0, 0.4, 0.6, 1]}
+          style={{ width: '100%', height: 1 }}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 1, y: 0.5 }}
+        />
+        <TabList
+          onLayout={e => {
+            setTabBarHeight(e.nativeEvent.layout.height);
+          }}
+          style={{
+            paddingHorizontal: 64,
+            paddingBottom: bottom,
+            backgroundColor: colors['ink.background-primary'],
+          }}
+        >
+          <TabTrigger style={{ flex: 1 }} name="(index)" href="/(tabs)/(index)">
+            <TabButton
+              icon={<HomeIcon />}
+              title={t({ id: 'tabs.button.home.title', message: 'Home' })}
+            />
+          </TabTrigger>
+          <TabTrigger style={{ flex: 1 }} name="activity" href="/(tabs)/activity">
+            <TabButton
+              icon={<PulseIcon />}
+              title={t({ id: 'tabs.button.activity.title', message: 'Activity' })}
+            />
+          </TabTrigger>
+          <TabTrigger style={{ flex: 1 }} name="browser" href="/(tabs)/browser">
+            <TabButton
+              icon={<BrowserIcon />}
+              title={t({ id: 'tabs.button.browser.title', message: 'Browser' })}
+            />
+          </TabTrigger>
+        </TabList>
+      </Tabs>
+    </TabLayoutContext.Provider>
   );
 }
