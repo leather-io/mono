2adebe751d75808b43198ce3f54383701e1f5a41
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2025-07-18 19:37:30 +0400

    fix: update tab bar nav buttons

---
+++ packages/ui/src/icons/activity-active-icon.native.tsx
@@ -0,0 +1,9 @@
+import ActivityActive24 from '../assets/icons/activity-active-24-24.svg';
+import { createNativeIcon } from './icon/create-icon.native';
+
+export const ActivityActiveIcon = createNativeIcon({
+  icon: {
+    medium: ActivityActive24,
+  },
+  displayName: 'ActivityActive',
+});
