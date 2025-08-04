344a186231b2259cfeb9ef50a3fccc25c29c0006
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-08-04 17:06:26 +0400

    refactor(mobile): update Lingui to v5

--- apps/mobile/lingui.config.ts
+++ apps/mobile/lingui.config.ts
@@ -2,19 +2,15 @@ import { LinguiConfig } from '@lingui/conf';
 import { formatter } from '@lingui/format-po';
 
 const config: LinguiConfig = {
-  locales: ['en', 'pseudo-locale'],
-  pseudoLocale: 'pseudo-locale',
+  locales: ['en'],
   sourceLocale: 'en',
-  fallbackLocales: {
-    'pseudo-locale': 'en',
-  },
   catalogs: [
     {
-      path: '<rootDir>/src/locales/{locale}/messages',
+      path: '<rootDir>/src/i18n/locales/{locale}/messages',
       include: ['src'],
     },
   ],
-  format: formatter({ explicitIdAsDefault: true }),
+  format: formatter(),
 };
 
 export default config;
--- apps/mobile/src/i18n/index.ts
+++ apps/mobile/src/i18n/index.ts
@@ -1,14 +1,10 @@
 import { i18n } from '@lingui/core';
 
 const DEFAULT_LOCALE = 'en';
-const LOCALES: string[] = [];
-export function getAvailableLocales() {
-  return LOCALES;
-}
 
 export async function initiateI18n() {
   // run load and activate so the I18nProvider doesn't block the render
-  const { messages } = await import(`./en/messages`);
+  const { messages } = await import(`./locales/en/messages`);
   i18n.load(DEFAULT_LOCALE, messages);
   i18n.activate(DEFAULT_LOCALE);
 }
