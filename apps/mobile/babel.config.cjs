c56db0d87a8a8ffecf5e85ed31de8634f3ccc8e5
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-08-04 17:06:26 +0400

    refactor(mobile): update Lingui to v5

--- apps/mobile/babel.config.cjs
+++ apps/mobile/babel.config.cjs
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(false);
   return {
-    plugins: ['macros', '@babel/plugin-transform-class-static-block'],
+    plugins: ['@lingui/babel-plugin-lingui-macro', '@babel/plugin-transform-class-static-block'],
     presets: ['babel-preset-expo'],
   };
 };
