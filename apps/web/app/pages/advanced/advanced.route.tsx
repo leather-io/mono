c0174e443c2ff8f704d474114c59ca7e05abc7b6
kyranjamie <kyran.burraston@gmail.com>
2025-08-13 10:15:35 +0200

    feat(web): disable advanced in prod

--- apps/web/app/pages/advanced/advanced.route.tsx
+++ apps/web/app/pages/advanced/advanced.route.tsx
@@ -1,6 +1,15 @@
 import { MetaDescriptor } from 'react-router';
 
+import { whenEnvTarget } from '~/constants/environment';
+
 import { AdvancedPage } from './advanced.page';
+
+export const advancedModeEnabled = whenEnvTarget({
+  branch: true,
+  development: true,
+  staging: true,
+  production: true,
+});
 
 export function meta() {
   return [
