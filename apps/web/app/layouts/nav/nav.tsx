8c98576d408ab0961e256add43b4423593ddcdeb
kyranjamie <kyran.burraston@gmail.com>
2025-08-13 10:15:35 +0200

    feat(web): disable advanced in prod

--- apps/web/app/layouts/nav/nav.tsx
+++ apps/web/app/layouts/nav/nav.tsx
@@ -7,6 +7,7 @@ import { Drawer } from 'vaul';
 import { LeatherLogo } from '~/components/icons/leather-logo';
 import { SbtcMonogramIcon } from '~/components/icons/sbtc-monogram-icon';
 import { StackingIcon } from '~/components/icons/stacking-icon';
+import { advancedModeEnabled } from '~/pages/advanced/advanced.route';
 import { externalLeatherNavigator } from '~/utils/external-leather-navigator';
 
 import { BarsTwoIcon, GridIcon, IconButton, SupportIcon, WalletIcon } from '@leather.io/ui';
@@ -38,29 +39,31 @@ export function NavContents() {
       <NavItem href={externalLeatherNavigator.apps} icon={<GridIcon variant="small" />}>
         Apps
       </NavItem>
-      <NavItem
-        href="/advanced"
-        icon={
-          // TODO: add this to UI library @fabric-8
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.50016 7.16683V2.8335C8.50016 2.28121 8.94788 1.8335 9.50016 1.8335H11.8335C12.3858 1.8335 12.8335 2.28121 12.8335 2.8335V7.16683M8.50016 4.50016H10.1668M3.16683 7.16683V4.25228C3.16683 4.08679 3.2079 3.92389 3.28636 3.77818L4.17546 2.12699C4.2728 1.94622 4.46152 1.8335 4.66683 1.8335V1.8335C4.87213 1.8335 5.06086 1.94622 5.1582 2.12699L6.0473 3.77818C6.12576 3.92389 6.16683 4.08679 6.16683 4.25228V7.16683M1.8335 7.16683H14.1668V12.5002C14.1668 13.0524 13.7191 13.5002 13.1668 13.5002H2.8335C2.28121 13.5002 1.8335 13.0524 1.8335 12.5002V7.16683Z"
-              stroke="#12100F"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        }
-      >
-        Advanced
-      </NavItem>
+      {advancedModeEnabled && (
+        <NavItem
+          href="/advanced"
+          icon={
+            // TODO: add this to UI library @fabric-8
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.50016 7.16683V2.8335C8.50016 2.28121 8.94788 1.8335 9.50016 1.8335H11.8335C12.3858 1.8335 12.8335 2.28121 12.8335 2.8335V7.16683M8.50016 4.50016H10.1668M3.16683 7.16683V4.25228C3.16683 4.08679 3.2079 3.92389 3.28636 3.77818L4.17546 2.12699C4.2728 1.94622 4.46152 1.8335 4.66683 1.8335V1.8335C4.87213 1.8335 5.06086 1.94622 5.1582 2.12699L6.0473 3.77818C6.12576 3.92389 6.16683 4.08679 6.16683 4.25228V7.16683M1.8335 7.16683H14.1668V12.5002C14.1668 13.0524 13.7191 13.5002 13.1668 13.5002H2.8335C2.28121 13.5002 1.8335 13.0524 1.8335 12.5002V7.16683Z"
+                stroke="#12100F"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          }
+        >
+          Advanced
+        </NavItem>
+      )}
 
       <styled.div mt="auto" mb={[null, null, 'space.06']}>
         <NavItem href={externalLeatherNavigator.support} icon={<SupportIcon variant="small" />}>
