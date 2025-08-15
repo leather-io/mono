94491b205ab1e155ebed07dd83b50aa4c981fc48
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-08-15 13:30:27 +0400

    fix(mobile): fix cell spacings in token details

--- apps/mobile/src/features/token/stacks/sip10-address-list.tsx
+++ apps/mobile/src/features/token/stacks/sip10-address-list.tsx
@@ -3,8 +3,6 @@ import { useSip10AccountBalance } from '@/queries/balance/sip10-balance.query';
 import { Account } from '@/store/accounts/accounts';
 import { useStacksSignerAddressFromAccountIndex } from '@/store/keychains/stacks/stacks-keychains.read';
 
-import { Box } from '@leather.io/ui/native';
-
 import { AddressList, AddressListItem } from '../components/address-list';
 
 interface Sip10AddressListProps {
@@ -21,15 +19,13 @@ export function Sip10AddressList({ account, tokenId }: Sip10AddressListProps) {
   const data = sip10Balance.value?.sip10s.find(sip10 => sip10.asset.symbol === tokenId);
   return (
     <AddressList account={account}>
-      <Box>
-        <AddressListItem
-          assetType={AssetType.Stacks}
-          address={stxAddress ?? ''}
-          name={data?.asset.name ?? ''}
-          availableBalance={data?.crypto.availableBalance}
-          quoteBalance={data?.quote.availableBalance}
-        />
-      </Box>
+      <AddressListItem
+        assetType={AssetType.Stacks}
+        address={stxAddress ?? ''}
+        name={data?.asset.name ?? ''}
+        availableBalance={data?.crypto.availableBalance}
+        quoteBalance={data?.quote.availableBalance}
+      />
     </AddressList>
   );
 }
