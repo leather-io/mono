75bff7a5d0b3e83edf77b5672e060eb787ce63b6
Will Cameron <camerow@users.noreply.github.com>
2025-09-03 13:24:06 -0700

    feat(cms): add sbtc schema and fix exports

--- packages/cms/src/studio/schema-types/index.ts
+++ packages/cms/src/studio/schema-types/index.ts
@@ -1,3 +1,4 @@
 import { postType } from './post-type';
+import { sbtcPoolType } from './sbtc-pool-information-type';
 
-export const schemaTypes = [postType];
+export const schemaTypes = [postType, sbtcPoolType];
