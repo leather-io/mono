2f4997a2a0b3e2d947f8c9849ee75c3bf3f818b2
Will Cameron <camerow@users.noreply.github.com>
2025-09-04 09:06:09 -0700

    feat(cms): add faq type

--- packages/cms/src/studio/schema-types/index.ts
+++ packages/cms/src/studio/schema-types/index.ts
@@ -1,4 +1,5 @@
+import { faqType } from './faq-type';
 import { postType } from './post-type';
 import { sbtcPoolType } from './sbtc-pool-information-type';
 
-export const schemaTypes = [postType, sbtcPoolType];
+export const schemaTypes = [postType, sbtcPoolType, faqType];
