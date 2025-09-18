3e0da05abc753130d0e9f9f2aba27d9417a2e731
bilalanees98 <bilalanees1998@gmail.com>
2025-09-18 11:25:48 +0500

    feat(mobile): deep linking via app links and universal links

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -27,6 +27,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       bundleIdentifier: 'io.leather.mobilewallet',
       googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST ?? './GoogleService-Info.plist',
       supportsTablet: false,
+      associatedDomains: ['applinks:connect.leather.io'],
       entitlements: {
         'aps-environment': 'production',
       },
@@ -69,6 +70,17 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       package: 'io.leather.mobilewallet',
       googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
       edgeToEdgeEnabled: true,
+      intentFilters: [
+        {
+          action: 'VIEW',
+          category: ['DEFAULT', 'BROWSABLE'],
+          data: {
+            scheme: 'https',
+            host: 'connect.leather.io',
+          },
+          autoVerify: true,
+        },
+      ],
       adaptiveIcon: {
         foregroundImage: './src/assets/adaptive-icon.png',
         backgroundColor: '#12100F',
