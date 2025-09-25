23aef943f35db1928bab3184df5bda8b7a754a79
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-09-25 13:40:29 +0100

    feat(mobile): add better support for ordinal gltf format

--- packages/ui/src/components/collectibles/native/inscription.native.tsx
+++ packages/ui/src/components/collectibles/native/inscription.native.tsx
@@ -2,6 +2,7 @@ import { InscriptionMimeType } from '@leather.io/models';
 import { assertUnreachable } from '@leather.io/utils';
 
 import { CollectibleAudio } from './collectible-audio.native';
+import { CollectibleGltf } from './collectible-gltf.native';
 import { CollectibleHtml } from './collectible-html.native';
 import { CollectibleImage } from './collectible-image.native';
 import { CollectibleText } from './collectible-text.native';
@@ -22,10 +23,11 @@ export function Inscription({ mimeType, name, height = 200, src, onPress }: Insc
       return <CollectibleText src={src} height={height} onPress={onPress} />;
     case 'html':
     case 'gltf':
-    case 'svg':
+      return <CollectibleGltf src={src} height={height} onPress={onPress} />;
     case 'video':
       return <CollectibleHtml src={src} height={height} onPress={onPress} />;
     case 'other':
+    case 'svg':
     case 'image':
       return <CollectibleImage source={src} alt={name} height={height} onPress={onPress} />;
     default:
