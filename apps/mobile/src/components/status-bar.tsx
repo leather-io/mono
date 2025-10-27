71f7b4550749043e5efda0b0cf3635be753c718a
Will Cameron <camerow@users.noreply.github.com>
2025-10-27 09:04:52 -0700

    feat(mobile): use consistent background color for app bar, support edge-to-edge appropriate bg color

--- apps/mobile/src/components/status-bar.tsx
+++ apps/mobile/src/components/status-bar.tsx
@@ -2,8 +2,11 @@ import { StatusBar as NativeStatusBar } from 'react-native';
 
 import { useSettings } from '@/store/settings/settings';
 
+import { useTheme } from '@leather.io/ui/native';
+
 export function StatusBar() {
   const { whenTheme } = useSettings();
+  const theme = useTheme();
 
   return (
     <NativeStatusBar
@@ -11,6 +14,7 @@ export function StatusBar() {
         dark: 'light-content',
         light: 'dark-content',
       } as const)}
+      backgroundColor={theme.colors['ink.background-primary']}
     />
   );
 }
