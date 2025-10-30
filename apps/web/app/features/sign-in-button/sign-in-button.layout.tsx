a7e19498f62b560a97f2effaf60ac5ee39794f93
kyranjamie <kyran.burraston@gmail.com>
2025-10-30 10:55:55 +0100

    refactor(web): fix dropdown, address feedback

--- apps/web/app/features/sign-in-button/sign-in-button.layout.tsx
+++ apps/web/app/features/sign-in-button/sign-in-button.layout.tsx
@@ -12,7 +12,7 @@ import {
 import { truncateMiddle } from '@leather.io/utils';
 
 export function SignInButtonLayout(props: ButtonProps) {
-  return <Button alignSelf="center" {...props} />;
+  return <Button alignSelf="center" size="md" {...props} />;
 }
 
 interface ActiveAccountButtonLayoutProps {
@@ -30,7 +30,7 @@ export function ActiveAccountButtonLayout({
   return (
     <DropdownMenu.Root modal={false}>
       <DropdownMenu.Trigger asChild>
-        <Button textAlign="left" variant="outline" width="138px" alignSelf="center">
+        <Button textAlign="left" variant="outline" size="md" width="138px" alignSelf="center">
           <Flag reverse spacing="space.01" img={<ChevronDownIcon variant="small" />}>
             {truncateMiddle(address, 4)}
           </Flag>
@@ -38,8 +38,8 @@ export function ActiveAccountButtonLayout({
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content sideOffset={1}>
-          <styled.div mx="space.02" py="space.02">
+        <DropdownMenu.Content sideOffset={8} alignOffset={0} align="end">
+          <styled.div mx="space.02" py="space.02" width="200px">
             <DropdownMenu.Item onSelect={onSwitchAccount}>
               <Flag textStyle="label.03" img={<ArrowsRepeatLeftRightIcon variant="small" />}>
                 Switch account
