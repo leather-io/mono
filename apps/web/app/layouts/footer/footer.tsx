065a6c13f3d229908ffe0d4c1cb1f60adc82ef06
Fabricio Rosa Marques <hello@fabric8.de>
2025-10-31 12:15:46 +0100

    fix(web): replace blog with changelog in footer

--- apps/web/app/layouts/footer/footer.tsx
+++ apps/web/app/layouts/footer/footer.tsx
@@ -8,7 +8,7 @@ function AppFooter() {
       <Footer.Grid>
         <Footer.Column title="Dive deeper">
           <Footer.Link href="/help-center">Help Center</Footer.Link>
-          <Footer.Link href={externalLeatherNavigator.blog}>Blog</Footer.Link>
+          <Footer.Link href="/changelog">Changelog</Footer.Link>
           <Footer.Link withIcon href={externalLeatherNavigator.docs}>
             Developer docs
           </Footer.Link>
