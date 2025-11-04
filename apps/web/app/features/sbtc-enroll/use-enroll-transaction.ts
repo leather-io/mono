c1479a7d23ee252ee24d6b23e7099bb8b4a18020
Werner <33360391+314159265359879@users.noreply.github.com>
2025-11-04 11:32:43 +0100

    chore: update use-enroll-transaction.ts
    updated the contract for enrollment for sBTC rewards. 
    
    Signed-off-by: Werner <33360391+314159265359879@users.noreply.github.com>

--- apps/web/app/features/sbtc-enroll/use-enroll-transaction.ts
+++ apps/web/app/features/sbtc-enroll/use-enroll-transaction.ts
@@ -15,9 +15,9 @@ interface EnrollContractIdentifier {
 }
 const sbtcEnrollContractMap = {
   testnet: {
-    contractAddress: 'ST1SY0NMZMBSA28MH31T09KCQWPZ4H5HRMYRX4XW7',
-    contractName: 'yield-rewards-testnet',
-    contract: 'ST1SY0NMZMBSA28MH31T09KCQWPZ4H5HRMYRX4XW7.yield-rewards-testnet',
+    contractAddress: 'ST25SZ23P3FDTNAEAQTXG0PD34FC51CX33K3SKPXC',
+    contractName: 'yield-dual-stacking-v4',
+    contract: 'ST25SZ23P3FDTNAEAQTXG0PD34FC51CX33K3SKPXC.yield-dual-stacking-v4',
   },
   devnet: {
     contractAddress: 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6',
@@ -25,9 +25,9 @@ const sbtcEnrollContractMap = {
     contract: 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6.yield',
   },
   mainnet: {
-    contractAddress: 'SP804CDG3KBN9M6E00AD744K8DC697G7HBCG520Q',
-    contractName: 'sbtc-yield-rewards-v3',
-    contract: 'SP804CDG3KBN9M6E00AD744K8DC697G7HBCG520Q.sbtc-yield-rewards-v3',
+    contractAddress: 'SP1HFCRKEJ8BYW4D0E3FAWHFDX8A25PPAA83HWWZ9',
+    contractName: 'dual-stacking-v1',
+    contract: 'SP804CDG3KBN9M6E00AD744K8DC697G7HBCG520Q.dual-stacking-v1',
   },
   mocknet: {} as EnrollContractIdentifier,
 } as const satisfies Record<StacksNetworkName, EnrollContractIdentifier>;
