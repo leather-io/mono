05200fc3aa94c9db1807a5741b179e8f90edde24
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-11-20 11:46:22 +0400

    feat(mobile): add refetching to swap queries

--- apps/mobile/src/features/swap/swap-state/swap.queries.ts
+++ apps/mobile/src/features/swap/swap-state/swap.queries.ts
@@ -1,51 +1,38 @@
-import {
-  SwapQuotePolicy,
-  SwapQuoteSelectionResult,
-} from '@/features/swap/swap-state/swap-state.types';
+import { SwapQuotePolicy } from '@/features/swap/swap-state/swap-state.types';
 import { createSwapAssetsSelector } from '@/features/swap/swap-state/utils/asset-selection';
 import { swapQuoteSelector } from '@/features/swap/swap-state/utils/swap-quote-selection';
 import { useDebouncedValue } from '@/hooks/use-debounced-value';
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import BigNumber from 'bignumber.js';
 import { isDefined, isNonNullish } from 'remeda';
 
-import {
-  CryptoAssetId,
-  MarketData,
-  Money,
-  SwapAsset,
-  SwapQuote,
-  SwappableFungibleCryptoAsset,
-} from '@leather.io/models';
-import {
-  AccountRequest,
-  AccountSwapAsset,
-  MarketDataService,
-  SwapService,
-} from '@leather.io/services';
+import { CryptoAssetId, Money, SwapAsset, SwappableFungibleCryptoAsset } from '@leather.io/models';
+import { AccountRequest, MarketDataService, SwapService } from '@leather.io/services';
 import { delay } from '@leather.io/utils';
 
-type CustomQueryOptions<TQueryFnData, TError = Error, TData = TQueryFnData> = Omit<
-  UseQueryOptions<TQueryFnData, TError, TData>,
-  'queryKey' | 'queryFn'
->;
+const assetsRefetchInterval = 30_000;
+const marketDataRefetchInterval = 30_0000;
+const globalRefetchOptions = {
+  refetchIntervalInBackground: false,
+  refetchOnReconnect: 'always',
+  refetchOnWindowFocus: 'always',
+} as const;
 
 interface UseAccountBaseSwapAssetsQueryParams {
   swapService: SwapService;
   accountRequest: AccountRequest;
-  queryOptions?: CustomQueryOptions<AccountSwapAsset[]>;
 }
 
 export function useAccountBaseSwapAssetsQuery({
   swapService,
   accountRequest,
-  queryOptions,
 }: UseAccountBaseSwapAssetsQueryParams) {
   return useQuery({
     queryKey: ['account-base-swap-assets', { request: accountRequest }],
     queryFn: ({ signal }) => swapService.getAccountBaseSwapAssets(accountRequest, signal),
     select: createSwapAssetsSelector('base'),
-    ...queryOptions,
+    refetchInterval: assetsRefetchInterval,
+    ...globalRefetchOptions,
   });
 }
 
@@ -53,14 +40,12 @@ interface UseAccountTargetSwapAssetsQueryParams {
   swapService: SwapService;
   accountRequest: AccountRequest;
   baseId?: CryptoAssetId;
-  queryOptions?: CustomQueryOptions<AccountSwapAsset[]>;
 }
 
 export function useAccountTargetSwapAssetsQuery({
   swapService,
   accountRequest,
   baseId,
-  queryOptions,
 }: UseAccountTargetSwapAssetsQueryParams) {
   return useQuery({
     queryKey: ['account-target-swap-assets', { baseId, request: accountRequest }],
@@ -70,7 +55,8 @@ export function useAccountTargetSwapAssetsQuery({
     },
     enabled: isDefined(baseId),
     select: createSwapAssetsSelector('target'),
-    ...queryOptions,
+    refetchInterval: assetsRefetchInterval,
+    ...globalRefetchOptions,
   });
 }
 
@@ -82,7 +68,6 @@ interface UseSwapQuotesQueryParams {
   policy: SwapQuotePolicy;
   fairMarketRate: BigNumber | null;
   slippage: number;
-  queryOptions?: CustomQueryOptions<SwapQuote[], Error, SwapQuoteSelectionResult>;
 }
 
 export function useSwapQuotesQuery({
@@ -93,14 +78,18 @@ export function useSwapQuotesQuery({
   policy,
   fairMarketRate,
   slippage,
-  queryOptions,
 }: UseSwapQuotesQueryParams) {
   const minFetchDuration = 500;
   const debounceDelay = 350;
   const debouncedBaseAmount = useDebouncedValue(baseAmount, debounceDelay);
 
   return useQuery({
-    queryKey: ['swap-quotes', { baseSwapAsset, targetSwapAsset, debouncedBaseAmount }],
+    queryKey: [
+      'swap-quotes',
+      baseSwapAsset?.asset.symbol,
+      targetSwapAsset?.asset.symbol,
+      debouncedBaseAmount?.amount.toString(),
+    ],
     queryFn: async ({ signal }) => {
       if (!baseSwapAsset || !targetSwapAsset || !debouncedBaseAmount) return [];
 
@@ -111,7 +100,7 @@ export function useSwapQuotesQuery({
 
       return quotes;
     },
-    staleTime: Infinity,
+    staleTime: 0,
     select: data => swapQuoteSelector(data, policy, fairMarketRate, slippage),
     enabled: !!(
       baseSwapAsset &&
@@ -119,20 +108,20 @@ export function useSwapQuotesQuery({
       isNonNullish(debouncedBaseAmount) &&
       !debouncedBaseAmount.amount.isZero()
     ),
-    ...queryOptions,
+    // Quote refetching should be manually controlled in the usage site instead.
+    refetchInterval: false,
+    ...globalRefetchOptions,
   });
 }
 
 interface UseAssetMarketDataQueryParams {
   marketDataService: MarketDataService;
   asset?: SwappableFungibleCryptoAsset;
-  queryOptions?: CustomQueryOptions<MarketData>;
 }
 
 export function useAssetMarketDataQuery({
   marketDataService,
   asset,
-  queryOptions,
 }: UseAssetMarketDataQueryParams) {
   return useQuery({
     queryKey: ['asset-market-data', { asset }],
@@ -141,7 +130,7 @@ export function useAssetMarketDataQuery({
       return marketDataService.getMarketData(asset, signal);
     },
     enabled: isDefined(asset),
-    refetchInterval: 30000,
-    ...queryOptions,
+    refetchInterval: marketDataRefetchInterval,
+    ...globalRefetchOptions,
   });
 }
--- apps/mobile/src/features/swap/use-preload-swap-data.ts
+++ apps/mobile/src/features/swap/use-preload-swap-data.ts
@@ -1,32 +1,13 @@
-import { useEffect, useState } from 'react';
-
 import { useAccountBaseSwapAssetsQuery } from '@/features/swap/swap-state/swap.queries';
 import { useAccountRequest } from '@/hooks/use-account-request';
 
 import { getSwapService } from '@leather.io/services';
 
-const preloadDelay = 5000;
-
-function useDelayedFlag() {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    const timerId = setTimeout(() => setIsReady(true), preloadDelay);
-    return () => clearTimeout(timerId);
-  }, []);
-
-  return isReady;
-}
-
 export function usePreloadSwapData() {
-  const isReadyToPreload = useDelayedFlag();
   const accountRequest = useAccountRequest();
 
   useAccountBaseSwapAssetsQuery({
     swapService: getSwapService(),
     accountRequest,
-    queryOptions: {
-      enabled: isReadyToPreload,
-    },
   });
 }
