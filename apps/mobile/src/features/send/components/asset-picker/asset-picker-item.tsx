37dd62e2dd2b262959fdcf2856d3ab231b56a70a
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-11-26 13:43:19 +0400

    fix(mobile): fix and simplify asset item animation in Send

--- apps/mobile/src/features/send/components/asset-picker/asset-picker-item.tsx
+++ apps/mobile/src/features/send/components/asset-picker/asset-picker-item.tsx
@@ -1,40 +1,27 @@
-import { ReactElement, ReactNode, cloneElement, useRef, useState } from 'react';
+import { ReactElement, ReactNode, cloneElement, useRef } from 'react';
 import { View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { HEADER_HEIGHT } from '@/shared/constants';
-
-const parentTopPadding = 16;
-
-// Wrapper around asset items for measuring and reporting vertical offsets for animation.
-// This component really only exists to avoid converting BitcoinBalance and its dependencies to forwardRef.
-export function AssetPickerItem({
-  onPress,
-  children,
-}: {
+interface AssetPickerItemProps {
   onPress(top: number | null): void;
   children: ReactNode;
-}) {
-  const pressableRef = useRef<View>(null);
-  const { top: safeInsetTop } = useSafeAreaInsets();
-  const [offsetTopRelativeToParent, setOffsetTopRelativeToParent] = useState<number | null>(null);
-  // Calculate total Y distance from viewport using hardcoded values.
-  // This should in theory be done using pageY value from https://reactnative.dev/docs/0.74/direct-manipulation#measurecallback
-  // but lack of useLayoutEffect in the "old" architecture prevents us from reliably getting measurements on first render.
-  const totalOffsetTop =
-    offsetTopRelativeToParent === null
-      ? null
-      : safeInsetTop + HEADER_HEIGHT + parentTopPadding + offsetTopRelativeToParent;
+  canAnimate?: boolean;
+}
+
+export function AssetPickerItem({ onPress, children, canAnimate }: AssetPickerItemProps) {
+  const ref = useRef<View>(null);
 
   function handlePress() {
-    onPress(totalOffsetTop);
+    if (canAnimate) {
+      ref.current?.measureInWindow((...measurements) => {
+        onPress(Math.min(measurements[1], 200));
+      });
+    } else {
+      onPress(null);
+    }
   }
 
   return (
-    <View
-      ref={pressableRef}
-      onLayout={layoutEvent => setOffsetTopRelativeToParent(layoutEvent.nativeEvent.layout.y)}
-    >
+    <View ref={ref}>
       {cloneElement(children as ReactElement<Partial<{ onPress(): void }>>, {
         onPress: handlePress,
       })}
