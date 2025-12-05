4060ea57fcde337d6bf7c40854962cec98c62d31
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-12-05 10:15:53 +0000

    feat(queries): add Runes balance query configs
    Add shared query configurations for Runes balances:
    - createRunesAccountBalanceQueryConfig
    - createRunesAggregateBalanceQueryConfig
    - createRuneBalanceByRuneNameQueryConfig

--- packages/queries/index.ts
+++ packages/queries/index.ts
@@ -6,5 +6,6 @@ export * from './src/market-data/market-data.query-config';
 export * from './src/balances/btc-balances.query-config';
 export * from './src/balances/stx-balances.query-config';
 export * from './src/balances/sip10-balances.query-config';
+export * from './src/balances/runes-balances.query-config';
 export * from './src/activity/activity.query-config';
 export * from './src/activity/sip10-activity.query-config';
