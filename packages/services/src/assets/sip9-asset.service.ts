5972db50e09ef785ec920bc794365b479d3e1daf
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2025-12-16 14:04:07 +0000

    chore: try out rate limits for gamma

--- packages/services/src/assets/sip9-asset.service.ts
+++ packages/services/src/assets/sip9-asset.service.ts
@@ -2,8 +2,9 @@ import { injectable } from 'inversify';
 
 import { Sip9Asset } from '@leather.io/models';
 
-import { GammaApiClient } from '../infrastructure/api/gamma/gamma-api.client';
+import { GammaApiClient, type GammaNftMetadata } from '../infrastructure/api/gamma/gamma-api.client';
 import { HiroStacksApiClient } from '../infrastructure/api/hiro/hiro-stacks-api.client';
+import type { HiroMetadata } from '../infrastructure/api/hiro/hiro-stacks-api.types';
 import { createSip9Asset, getNonFungibleTokenId } from './sip9-asset.utils';
 import { getContractPrincipalFromAssetIdentifier } from './stacks-asset.utils';
 
@@ -24,10 +25,21 @@ export class Sip9AssetService {
   ): Promise<Sip9Asset> {
     const principal = getContractPrincipalFromAssetIdentifier(assetIdentifier);
     const tokenId = getNonFungibleTokenId(tokenHexValue);
-    const [hiroMetadata, gammaMetadata] = await Promise.all([
+
+    const [hiroResult, gammaResult] = await Promise.allSettled([
       this.stacksApiClient.getNftMetadata(principal, tokenId, { signal }),
       this.gammaApiClient.getStacksNft(principal, tokenId, { signal }),
     ]);
+
+    let hiroMetadata: HiroMetadata | undefined;
+    if (hiroResult.status === 'fulfilled') {
+      hiroMetadata = hiroResult.value;
+    }
+
+    let gammaMetadata: GammaNftMetadata | null = null;
+    if (gammaResult.status === 'fulfilled') {
+      gammaMetadata = gammaResult.value;
+    }
 
     return createSip9Asset(assetIdentifier, tokenId, hiroMetadata, gammaMetadata);
   }
