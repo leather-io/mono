ff52f9b5a24f201362361e2ed46279cf66c22bc9
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2025-12-18 23:44:01 +0400

    refactor(bitcoin): rework bitcoin send max calculation

--- packages/bitcoin/src/coin-selection/calculate-max-spend.spec.ts
+++ packages/bitcoin/src/coin-selection/calculate-max-spend.spec.ts
@@ -1,55 +1,50 @@
 import { createBitcoinAddress } from '../validation/bitcoin-address';
 import { calculateMaxSpend } from './calculate-max-spend';
-import { generateMockAverageFee, mockUtxos } from './coin-selection.mocks';
+import { mockUtxos } from './coin-selection.mocks';
 
 const recipient = createBitcoinAddress('');
 describe(calculateMaxSpend.name, () => {
   test('with 1 sat/vb fee', () => {
-    const fee = 1;
     const maxBitcoinSpend = calculateMaxSpend({
       recipient,
       utxos: mockUtxos,
-      feeRates: generateMockAverageFee(fee),
+      feeRate: 1,
     });
     expect(maxBitcoinSpend.amount.amount.toNumber()).toEqual(50087948);
   });
 
   test('with 5 sat/vb fee', () => {
-    const fee = 5;
     const maxBitcoinSpend = calculateMaxSpend({
       recipient,
       utxos: mockUtxos,
-      feeRates: generateMockAverageFee(fee),
+      feeRate: 5,
     });
     expect(maxBitcoinSpend.amount.amount.toNumber()).toEqual(50085342);
   });
 
   test('with 30 sat/vb fee', () => {
-    const fee = 30;
     const maxBitcoinSpend = calculateMaxSpend({
       recipient,
       utxos: mockUtxos,
-      feeRates: generateMockAverageFee(fee),
+      feeRate: 30,
     });
     expect(maxBitcoinSpend.amount.amount.toNumber()).toEqual(50073585);
   });
 
   test('with 100 sat/vb fee', () => {
-    const fee = 100;
     const maxBitcoinSpend = calculateMaxSpend({
       recipient,
       utxos: mockUtxos,
-      feeRates: generateMockAverageFee(fee),
+      feeRate: 100,
     });
     expect(maxBitcoinSpend.amount.amount.toNumber()).toEqual(50046950);
   });
 
   test('with 400 sat/vb fee', () => {
-    const fee = 400;
     const maxBitcoinSpend = calculateMaxSpend({
       recipient,
       utxos: mockUtxos,
-      feeRates: generateMockAverageFee(fee),
+      feeRate: 400,
     });
     expect(maxBitcoinSpend.amount.amount.toNumber()).toEqual(49969100);
   });
