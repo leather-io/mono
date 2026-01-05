24baa9bf3487d958460a1c666fac78815bfb0843
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2026-01-05 11:34:00 +0000

    fix: re-fetch fee when transaction length changes to account for memo/arg changes

--- apps/extension/src/app/query/stacks/fees/fees.hooks.ts
+++ apps/extension/src/app/query/stacks/fees/fees.hooks.ts
@@ -1,4 +1,4 @@
-import type { StacksTransactionWire } from '@stacks/transactions';
+import { type StacksTransactionWire, estimateTransactionByteLength } from '@stacks/transactions';
 import { useQuery } from '@tanstack/react-query';
 
 import {
@@ -8,6 +8,7 @@ import {
   type TransactionFeeQuote,
 } from '@leather.io/models';
 import { getStacksTransactionFeesService } from '@leather.io/services';
+import { getSerializedUnsignedStacksTxPayload } from '@leather.io/stacks';
 
 function assertIsStacksFeeRateQuote(
   quote: TransactionFeeQuote
@@ -17,15 +18,26 @@ function assertIsStacksFeeRateQuote(
   }
 }
 
+type StacksTxFeesQueryKey = readonly [
+  'stacks-tx-fees',
+  payload: string,
+  estimatedTransactionByteLength: number,
+  tx: StacksTransactionWire | undefined,
+];
+
 export function useCalculateStacksTxFees(unsignedTx?: StacksTransactionWire) {
-  return useQuery<Fees>({
+  const payload = unsignedTx ? getSerializedUnsignedStacksTxPayload(unsignedTx) : '';
+  const estimatedTransactionByteLength = unsignedTx ? estimateTransactionByteLength(unsignedTx) : 0;
+
+  return useQuery<Fees, Error, Fees, StacksTxFeesQueryKey>({
     enabled: !!unsignedTx,
-    queryKey: ['stacks-tx-fees', unsignedTx] as const,
-    queryFn: async ({ signal }) => {
-      if (!unsignedTx) throw new Error('Stacks tx fees query was called without unsigned tx');
+    queryKey: ['stacks-tx-fees', payload, estimatedTransactionByteLength, unsignedTx],
+    queryFn: async ({ signal, queryKey }) => {
+      const [, , , tx] = queryKey;
+      if (!tx) throw new Error('Stacks tx fees query was called without unsigned tx');
 
       const service = getStacksTransactionFeesService();
-      const res = await service.getStacksTransactionFees(unsignedTx, signal);
+      const res = await service.getStacksTransactionFees(tx, signal);
       const { low, standard, high } = res.options;
 
       assertIsStacksFeeRateQuote(low);
