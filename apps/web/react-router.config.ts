22190b8ee76cbb60ec4d5c64b46dec38d3d6a41a
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2026-01-09 12:28:10 +0400

    fix: react router audit

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -10,7 +10,7 @@ const buildEnd: Config['buildEnd'] = async ({ viteConfig, reactRouterConfig, bui
 export default {
   ssr: true,
   future: {
-    unstable_viteEnvironmentApi: true,
+    v8_viteEnvironmentApi: true,
   },
   // Hack to get around non-portable type export of `FutureConfig`
   buildEnd: buildEnd as unknown as Config['buildEnd'],
