f3996f64138b72c6aded047467a6e33479e039f7
kyranjamie <kyran.burraston@gmail.com>
2026-01-12 13:27:35 +0100

    fix(web): broken production build

--- apps/web/app/mocks/api/node.ts
+++
@@ -1,7 +0,0 @@
-import { setupServer } from 'msw/node';
-
-// With an SSR framework, mocks should also run in the node environment such
-// that initial responses also contain mocked response data. It hasn't been
-// possible to get this working, however for now it doesn't greatly impact our
-// ability to test the app. Ought to be `setupServer(...successHandlers);`
-export const server = setupServer();
