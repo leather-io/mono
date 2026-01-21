d5edeafe7a612f95647525514a9ea92053c2932b
Will Cameron <camerow@users.noreply.github.com>
2026-01-21 12:45:47 -0800

    fix(mobile): fix status bar color

--- apps/mobile/src/components/status-bar.tsx
+++ apps/mobile/src/components/status-bar.tsx
@@ -2,19 +2,16 @@ import { StatusBar as NativeStatusBar } from 'react-native';
 
 import { useSettings } from '@/store/settings/settings';
 
-import { useTheme } from '@leather.io/ui/native';
-
 export function StatusBar() {
   const { whenTheme } = useSettings();
-  const theme = useTheme();
 
   return (
     <NativeStatusBar
+      translucent
       barStyle={whenTheme({
         dark: 'light-content',
         light: 'dark-content',
       } as const)}
-      backgroundColor={theme.colors['ink.background-primary']}
     />
   );
 }
