e7970fa208f3ab485a31c0d484c0d0614485a25f
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2026-01-27 17:16:33 +0400

    fix(extension): move secret key files

--- apps/extension/src/app/pages/view-secret-key/unlocked-view-secret-key.tsx
+++ apps/extension/src/app/pages/view-secret-key/unlocked-view-secret-key.tsx
@@ -7,7 +7,7 @@ import {
   TwoColumnLayout,
 } from '@app/components/layout/layouts/two-column.layout';
 import { useDefaultWalletSecretKey } from '@app/store/in-memory-key/in-memory-key.selectors';
-import { SecretKey } from '@app/ui/components/secret-key-current/secret-key';
+import { SecretKey } from '@app/ui/components/secret-key/secret-key';
 
 export function UnlockedViewSecretKey() {
   const defaultWalletSecretKey = useDefaultWalletSecretKey();
--- apps/extension/src/app/ui/components/secret-key/secret-key-grid.tsx
+++ apps/extension/src/app/ui/components/secret-key/secret-key-grid.tsx
@@ -8,11 +8,11 @@ export function SecretKeyGrid({ children }: SecretKeyGridProps) {
     <Grid
       gridTemplateColumns={{
         base: 'repeat(1, minmax(160px, 1fr))',
-        md: 'repeat(2, minmax(170px, 1fr))',
-        lg: 'repeat(3, minmax(170px, 1fr))',
+        sm: 'repeat(2, minmax(170px, 1fr))',
+        md: 'repeat(3, minmax(170px, 1fr))',
       }}
-      rowGap="space.03"
-      columnGap="space.03"
+      rowGap="space.02"
+      columnGap="space.02"
     >
       {children}
     </Grid>
