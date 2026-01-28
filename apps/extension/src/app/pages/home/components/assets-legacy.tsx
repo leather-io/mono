105cd262a3be3f670f18be7b2edfdd96bd3454ec
Pete Watters <2938440+pete-watters@users.noreply.github.com>
2026-01-28 10:37:27 +0000

    feat(extension): remove homeTabsRevamp feature flag and go live

--- apps/extension/src/app/pages/home/components/assets-legacy.tsx
+++ apps/extension/src/app/pages/home/components/assets-legacy.tsx
@@ -5,14 +5,12 @@ import { Stack } from 'leather-styles/jsx';
 
 import { AssetList } from '@app/features/asset-list/asset-list';
 import { ManageTokens } from '@app/features/asset-list/manage-tokens/manage-tokens';
-import { Collectibles } from '@app/features/collectibles/collectibles';
 
 export function AssetsLegacy() {
   return (
     <Stack data-testid={HomePageSelectors.AssetList}>
       <AssetList filter="enabled" />
       <ManageTokens />
-      <Collectibles />
       <Outlet />
     </Stack>
   );
