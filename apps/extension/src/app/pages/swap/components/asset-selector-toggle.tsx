8acdb8a8b4f26f50659d947c8f3c630f45dd7e78
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2026-02-06 19:43:27 +0400

    feat(extension): add base asset section to swap form

---
+++ apps/extension/src/app/pages/swap/components/asset-selector-toggle.tsx
@@ -0,0 +1,53 @@
+import { Flex, styled } from 'leather-styles/jsx';
+
+import { SwappableFungibleCryptoAsset } from '@leather.io/models';
+import { AssetAvatarIcon, ChevronDownIcon, PlusIcon } from '@leather.io/ui';
+
+interface AssetSelectorToggleProps {
+  asset: SwappableFungibleCryptoAsset | undefined;
+  disabled?: boolean;
+  onPress(): void;
+}
+
+export function AssetSelectorToggle({ asset, disabled, onPress }: AssetSelectorToggleProps) {
+  return (
+    <styled.button
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+      gap="space.02"
+      pl="space.01"
+      pr="space.02"
+      bg="ink.component-background-hover"
+      height={32}
+      borderRadius="round"
+      opacity={disabled ? 0.5 : 1}
+      _active={{ opacity: 0.75 }}
+      onClick={onPress}
+    >
+      {renderAvatar(asset)}
+      <styled.span textStyle="label.03">{asset ? asset.symbol : `Select`}</styled.span>
+      <ChevronDownIcon variant="small" />
+    </styled.button>
+  );
+}
+
+function renderAvatar(asset: SwappableFungibleCryptoAsset | undefined) {
+  if (!asset) {
+    return (
+      <Flex
+        borderWidth={1}
+        borderColor="ink.border-transparent"
+        alignItems="center"
+        justifyContent="center"
+        width={24}
+        height={24}
+        borderRadius="round"
+      >
+        <PlusIcon color="ink.text-subdued" variant="small" />
+      </Flex>
+    );
+  }
+
+  return <AssetAvatarIcon asset={asset} size="sm" />;
+}
--- apps/extension/src/app/pages/swap/swap-form.tsx
+++ apps/extension/src/app/pages/swap/swap-form.tsx
@@ -1,17 +1,44 @@
-import { Box } from 'leather-styles/jsx/index';
+import { Box, Flex } from 'leather-styles/jsx';
+
+import { useSwapContext } from '@leather.io/state/swap';
 
 import { RouteUrls } from '@shared/route-urls';
 
 import { Card, Content, Page } from '@app/components/layout';
 import { PageHeader } from '@app/features/container/headers/page.header';
+import { AmountField } from '@app/pages/swap/components/amount-field';
+import { AssetBalance } from '@app/pages/swap/components/asset-balance';
+import { AssetSelectorToggle } from '@app/pages/swap/components/asset-selector-toggle';
 
 export function SwapForm() {
+  const { state, actions } = useSwapContext();
+
   return (
     <Box width="100%">
       <PageHeader title="Swap" onBackLocation={RouteUrls.Home} />
       <Content>
         <Page>
-          <Card>{null}</Card>
+          <Card>
+            <Flex justifyContent="space-between" alignItems="flex-start">
+              <AmountField
+                asset={state.baseSwapAsset?.asset}
+                value={state.baseAmount}
+                secondaryAmount={state.secondaryAmount}
+                inputCurrencyMode={state.inputCurrencyMode}
+              />
+
+              <Flex direction="column" gap="space.03" alignItems="flex-end">
+                <AssetSelectorToggle
+                  asset={state.baseSwapAsset?.asset}
+                  onPress={() => actions.openAssetSelector('base')}
+                />
+                <AssetBalance
+                  balance={state.baseSwapAsset?.balance}
+                  inputCurrencyMode={state.inputCurrencyMode}
+                />
+              </Flex>
+            </Flex>
+          </Card>
         </Page>
       </Content>
     </Box>
