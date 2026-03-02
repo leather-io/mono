a2835cf0541e901a8a5fc5fc25a43a9765e0a23c
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2026-03-02 10:11:02 +0400

    feat(extension): include Bitflow provider config into services setup

--- apps/extension/src/app/services/init-app-services.ts
+++ apps/extension/src/app/services/init-app-services.ts
@@ -1,6 +1,15 @@
 import { initServicesContainer } from '@leather.io/services';
 
-import { WALLET_ENVIRONMENT } from '@shared/environment';
+import {
+  BITFLOW_API_HOST,
+  BITFLOW_API_KEY,
+  BITFLOW_KEEPER_API_HOST,
+  BITFLOW_KEEPER_API_KEY,
+  BITFLOW_PROVIDER_ADDRESS,
+  BITFLOW_READONLY_CALL_API_HOST,
+  BITFLOW_READONLY_CALL_API_KEY,
+  WALLET_ENVIRONMENT,
+} from '@shared/environment';
 
 import { ExtensionHttpCacheService } from './extension-http-cache.service';
 import { ExtensionSettingsService } from './extension-settings.service';
@@ -9,6 +18,15 @@ export function initAppServices() {
   initServicesContainer({
     env: {
       environment: WALLET_ENVIRONMENT,
+      bitflow: {
+        bitflowApiHost: BITFLOW_API_HOST,
+        bitflowApiKey: BITFLOW_API_KEY,
+        bitflowProviderAddress: BITFLOW_PROVIDER_ADDRESS,
+        readonlyCallApiHost: BITFLOW_READONLY_CALL_API_HOST,
+        readonlyCallApiKey: BITFLOW_READONLY_CALL_API_KEY,
+        keeperApiKey: BITFLOW_KEEPER_API_KEY,
+        keeperApiHost: BITFLOW_KEEPER_API_HOST,
+      },
     },
     cacheService: ExtensionHttpCacheService,
     settingsService: ExtensionSettingsService,
