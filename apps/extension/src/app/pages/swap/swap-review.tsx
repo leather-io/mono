a98ddef09c62b59eb22477dbefee13d1f993cc6c
tigran <3755268+tigranpetrossian@users.noreply.github.com>
2026-03-03 22:48:52 +0400

    feat(extension): add routing and page header to swap review page

--- apps/extension/src/app/pages/swap/swap-review.tsx
+++ apps/extension/src/app/pages/swap/swap-review.tsx
@@ -1,5 +1,17 @@
-import { Card } from '@app/components/layout';
+import { Box } from 'leather-styles/jsx';
+
+import { Card, Content, Page } from '@app/components/layout';
+import { PageHeader } from '@app/features/container/headers/page.header';
 
 export function SwapReview() {
-  return <Card>{null}</Card>;
+  return (
+    <Box width="100%">
+      <PageHeader title="Swap" />
+      <Content>
+        <Page>
+          <Card>{null}</Card>
+        </Page>
+      </Content>
+    </Box>
+  );
 }
