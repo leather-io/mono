969682e66e4bff577fe53d079c471a4a607d710d
Leah McBeth <254699884+leah-stacks@users.noreply.github.com>
2026-06-10 09:45:08 -0600

    feat(ui): add WalletSparkle icon

---
+++ packages/ui/src/icons/wallet-sparkle-icon.native.tsx
@@ -0,0 +1,9 @@
+import WalletSparkle20 from '../assets/icons/wallet-sparkle-20-20.svg';
+import { createNativeIcon } from './icon/create-icon.native';
+
+export const WalletSparkleIcon = createNativeIcon({
+  icon: {
+    medium: WalletSparkle20,
+  },
+  displayName: 'WalletSparkle',
+});
---
+++ packages/ui/src/icons/wallet-sparkle-icon.web.tsx
@@ -0,0 +1,9 @@
+import WalletSparkle20 from '../assets/icons/wallet-sparkle-20-20.svg';
+import { createWebIcon } from './icon/create-icon.web';
+
+export const WalletSparkleIcon = createWebIcon({
+  icon: {
+    medium: WalletSparkle20,
+  },
+  displayName: 'WalletSparkle',
+});
