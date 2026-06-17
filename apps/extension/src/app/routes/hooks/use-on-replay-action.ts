d2262d0cbdd3c9948e32229e5cdf17804f523c5e
Edgar Khanzadian <edgar@stackslabs.com>
2026-06-17 18:04:34 +0400

    fix: run replay on clear account name

--- apps/extension/src/app/routes/hooks/use-on-replay-action.ts
+++ apps/extension/src/app/routes/hooks/use-on-replay-action.ts
@@ -1,28 +1,14 @@
-import { userAddsAccount } from '@leather.io/state/keychains';
-import { userRenamesWallet } from '@leather.io/state/wallet';
-
 import { addReplayActionListener } from '@shared/messages';
 
 import { useOnMount } from '@app/common/hooks/use-on-mount';
 import { useAppDispatch } from '@app/store';
-import { userRenamesAccount, userTogglesHideAccount } from '@app/store/accounts/accounts.slice';
-import { userSwitchesAccount } from '@app/store/active/active.slice';
-import { stxChainSlice } from '@app/store/chains/stx-chain.slice';
-
-const replayableActionTypes = new Set<string>([
-  userRenamesWallet.type,
-  userRenamesAccount.type,
-  userTogglesHideAccount.type,
-  userAddsAccount.type,
-  userSwitchesAccount.type,
-  stxChainSlice.actions.createNewAccount.type,
-]);
+import { isReplayableAction } from '@app/store/replay-actions';
 
 export function useOnReplayAction() {
   const dispatch = useAppDispatch();
   useOnMount(() =>
     addReplayActionListener(action => {
-      if (replayableActionTypes.has(action.type)) dispatch(action);
+      if (isReplayableAction(action)) dispatch(action);
     })
   );
 }
