d65c5da303cef30d222754bc30921390b9b2c44a
Alex Perry <278960221+aperry-stacks@users.noreply.github.com>
2026-06-22 11:50:08 +0200

    feat(web): connect vault account balance hook to services

--- packages/utils/src/accounts/account-addresses.ts
+++ packages/utils/src/accounts/account-addresses.ts
@@ -2,7 +2,6 @@ import {
   AccountAddresses,
   AccountId,
   BitcoinAddressInfo,
-  HdBitcoinAddressInfo,
   StacksAddressInfo,
 } from '@leather.io/models';
 
@@ -31,12 +30,6 @@ export function hasBitcoinAddress(
   account: AccountAddresses
 ): account is AccountAddresses & { bitcoin: BitcoinAddressInfo } {
   return account.bitcoin !== undefined;
-}
-
-export function hasHdBitcoinAddress(
-  account: AccountAddresses
-): account is AccountAddresses & { bitcoin: HdBitcoinAddressInfo } {
-  return account.bitcoin?.type === 'hd';
 }
 
 export function hasStacksAddress(
