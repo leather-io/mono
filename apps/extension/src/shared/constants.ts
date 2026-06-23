134b2e5e284162a80bb519ccc3578f127fc1328d
Edgar Khanzadian <edgar@stackslabs.com>
2026-06-23 14:58:56 +0400

    fix: constant

--- apps/extension/src/shared/constants.ts
+++ apps/extension/src/shared/constants.ts
@@ -2,5 +2,4 @@ export const GITHUB_ORG = 'leather-io';
 export const GITHUB_REPO = 'extension';
 
 // TODO: set the real multisig dApp origin before shipping the policy account methods
-export const policyAccountAllowedOrigin = 'http://localhost:5180';
-// export const policyAccountAllowedOrigin = 'https://app.leather.io';
+export const policyAccountAllowedOrigin = 'https://app.leather.io';
