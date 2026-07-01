dae37fc2aed9496bb3991640ba241db8d43c062f
Edgar Khanzadian <edgar@stackslabs.com>
2026-07-01 12:57:44 +0400

    fix: guard buy button for multisig

--- apps/extension/src/app/features/asset-list/utils.ts
+++ apps/extension/src/app/features/asset-list/utils.ts
@@ -11,14 +11,16 @@ import { openIndexPageInNewTab } from '@app/common/utils/open-in-new-tab';
 import { useFlags } from '@app/features/feature-flags';
 import { useActivityByAsset } from '@app/query/activity/activity.query';
 import { useCurrentAccountAddresses } from '@app/services/accounts/use-account-addresses';
+import { useCurrentPolicy } from '@app/store/policy/policy.selectors';
 
 export function useCryptoAssetBuy(asset: CryptoAsset) {
   const navigate = useNavigate();
   const account = useCurrentAccountAddresses();
   const activityQuery = useActivityByAsset(account, asset);
   const { releaseOnramperBuy } = useFlags();
+  const policy = useCurrentPolicy();
   const showBuyButton =
-    activityQuery.isSuccess && !activityQuery.data?.length && releaseOnramperBuy;
+    activityQuery.isSuccess && !activityQuery.data?.length && releaseOnramperBuy && !policy;
 
   const onBuy = useCallback(() => {
     const route = RouteUrls.Fund.replace(':chain?', asset.chain);
